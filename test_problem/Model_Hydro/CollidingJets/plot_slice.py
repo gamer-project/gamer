4fbf921109485cfa78a728d96db38cd3c2672229
Hsi-Yu Schive <hyschive@gmail.com>
2017-04-28 15:38:06 -0500

    Update the test problem "CollodingJets" to test a single jet by default

--- test_problem/Model_Hydro/CollidingJets/plot_slice.py
+++ test_problem/Model_Hydro/CollidingJets/plot_slice.py
@@ -3,7 +3,7 @@ import yt
 yt.enable_parallelism()
 
 idx_start = 0
-idx_end   = 2
+idx_end   = 50
 didx      = 1
 prefix    = 'Data_'
 
@@ -12,11 +12,9 @@ ts = yt.load( [ prefix+'%06d'%idx for idx in range(idx_start, idx_end+1, didx) ]
 
 for ds in ts.piter():
 
-   sz = yt.SlicePlot( ds, 'z', 'temperature', center='c' )
-
-#  set temperature unit to keV
+   sz = yt.SlicePlot( ds, 'z', 'temperature', center='c', width=[40,10] )
    sz.set_unit( 'temperature', 'keV', equivalency='thermal' )
    sz.set_zlim( 'temperature', 1.0e0, 2.0e2 )
    sz.set_cmap( 'temperature', 'afmhot' )
    sz.annotate_timestamp( time_unit='Myr', corner='upper_right' )
-   sz.save( mpl_kwargs={"dpi":150} )
+   sz.save( mpl_kwargs={"dpi":300} )
