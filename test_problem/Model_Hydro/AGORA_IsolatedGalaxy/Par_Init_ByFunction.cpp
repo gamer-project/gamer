7d586c210ac246cf7b863eb1a40cf8a248d7ea9c
Hsi-Yu Schive <hyschive@gmail.com>
2017-05-05 21:30:12 -0500

    AGORA: load the particle initial conditions

--- test_problem/Model_Hydro/AGORA_IsolatedGalaxy/Par_Init_ByFunction.cpp
+++ test_problem/Model_Hydro/AGORA_IsolatedGalaxy/Par_Init_ByFunction.cpp
@@ -3,42 +3,28 @@
 
 #ifdef PARTICLE
 
-/*
-extern char    ClusterMerger_File_Par1[1000];
-extern char    ClusterMerger_File_Par2[1000];
-extern bool    ClusterMerger_Coll;
-extern double  ClusterMerger_Coll_D;
-extern double  ClusterMerger_Coll_B;
-extern double  ClusterMerger_Coll_BulkVel1;
-extern double  ClusterMerger_Coll_BulkVel2;
-*/
+
+
+extern bool CheckEmptyString( const char *InputString );
+extern int  CountRow( const char *Filename );
+
+extern char AGORA_HaloPar_Filename[1000];
+extern char AGORA_DiskPar_Filename[1000];
+extern char AGORA_BulgePar_Filename[1000];
 
 
 
 
 //-------------------------------------------------------------------------------------------------------
 // Function    :  Par_Init_ByFunction
-// Description :  Initialize particle attributes for the merging cluster test
-//                --> Modified from "Par_Init_ByFile.cpp"
+// Description :  Initialize all particle attributes
 //
 // Note        :  1. Invoked by "Init_GAMER"
-//                2. Particles lying outside the active region will be removed by "Par_Aux_InitCheck"
-//                   if non-periodic B.C. is adopted
-//                3. Particles loaded here are only temporarily stored in this rank
-//                   --> They will be redistributed when calling "Par_LB_Init_RedistributeByRectangular
-//                       and LB_Init_LoadBalance"
-//                4. File format: plain C binary in the format [Number of particles][Particle attributes]
-//                   --> [Particle 0][Attribute 0], [Particle 0][Attribute 1], ...
-//                   --> Note that it's different from the internal data format in the particle repository,
-//                       which is [Particle attributes][Number of particles]
-//                   --> Currently it only loads particle mass, position x/y/z, and velocity x/y/z
-//                       (and exactly in this order)
-//                5. For LOAD_BALANCE, the total number of particles in each rank must be set in advance
-//                   --> Currently it's set by "Init_Parallelization"
+//                2. Assuming all particles are active initially
 //
 // Parameter   :  None
 //
-// Return      :  amr->Par->Time,Mass,PosX/Y/Z,VelX/Y/Z
+// Return      :  amr->Par->Mass/Pos(X/Y/Z)/Vel(X/Y/Z)/Time
 //-------------------------------------------------------------------------------------------------------
 void Par_Init_ByFunction()
 {
@@ -46,163 +32,198 @@ void Par_Init_ByFunction()
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "%s ...\n", __FUNCTION__ );
 
 
-   /*
-   const long NParVar = 7;   // mass, pos*3, vel*3
+   const int NParVar = 7;  // mass, pos*3, vel*3
+   real (*ParData_AllRank)[NParVar] = NULL;
 
-// check file existence
-   if ( !Aux_CheckFileExist(ClusterMerger_File_Par1) )
-      Aux_Error( ERROR_INFO, "file \"%s\" does not exist !!\n", ClusterMerger_File_Par1 );
+// load data --> for simplicity, currently only the root rank will load data from disk
+   if ( MPI_Rank == 0 )
+   {
+//    check file existence
+      if ( !Aux_CheckFileExist(AGORA_HaloPar_Filename) )
+         Aux_Error( ERROR_INFO, "file \"%s\" does not exist !!\n", AGORA_HaloPar_Filename );
 
-   if ( ClusterMerger_Coll  &&  !Aux_CheckFileExist(ClusterMerger_File_Par2) )
-      Aux_Error( ERROR_INFO, "file \"%s\" does not exist !!\n", ClusterMerger_File_Par2 );
+      if ( !Aux_CheckFileExist(AGORA_DiskPar_Filename) )
+         Aux_Error( ERROR_INFO, "file \"%s\" does not exist !!\n", AGORA_DiskPar_Filename );
+
+      if ( !Aux_CheckFileExist(AGORA_BulgePar_Filename) )
+         Aux_Error( ERROR_INFO, "file \"%s\" does not exist !!\n", AGORA_BulgePar_Filename );
 
 
-// check file size
-   FILE *File=NULL;
-   long  NPar[2]={0,0}, NParAll;
+//    check file size
+      long NPar_Halo, NPar_Disk, NPar_Bulge, NPar_All;
 
-   File  = fopen( ClusterMerger_File_Par1, "rb" );
-   fseek( File, 0, SEEK_END );
-   NPar[0] = ftell( File ) / ( NParVar*sizeof(real_par_in) );
-   fclose( File );
-   if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Number of particles in cluster 1 = %ld\n", NPar[0] );
+      NPar_Halo  = CountRow( AGORA_HaloPar_Filename );
+      NPar_Disk  = CountRow( AGORA_DiskPar_Filename );
+      NPar_Bulge = CountRow( AGORA_BulgePar_Filename );
+      NPar_All   = NPar_Halo + NPar_Disk + NPar_Bulge;
 
-   if ( ClusterMerger_Coll ) {
-   File  = fopen( ClusterMerger_File_Par2, "rb" );
-   fseek( File, 0, SEEK_END );
-   NPar[1] = ftell( File ) / ( NParVar*sizeof(real_par_in) );
-   fclose( File );
-   if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Number of particles in cluster 2 = %ld\n", NPar[1] );
-   }
+      Aux_Message( stdout, "   Number of halo  particles = %ld\n", NPar_Halo );
+      Aux_Message( stdout, "   Number of disk  particles = %ld\n", NPar_Disk );
+      Aux_Message( stdout, "   Number of bulge particles = %ld\n", NPar_Bulge );
+      Aux_Message( stdout, "   Number of all   particles = %ld\n", NPar_All );
 
-   NParAll = NPar[0] + NPar[1];
+      if ( NPar_All != amr->Par->NPar_Active_AllRank )
+         Aux_Error( ERROR_INFO, "total number of particles found in disk [%ld] != expect [%ld] !!\n",
+                    NPar_All, amr->Par->NPar_Active_AllRank );
 
-   if ( NParAll != amr->Par->NPar_Active_AllRank )
-      Aux_Error( ERROR_INFO, "total number of particles found in disk [%ld] != expect [%ld] !!\n",
-                 NParAll, amr->Par->NPar_Active_AllRank );
+
+//    allocate memory to store all particles loaded from disk
+      ParData_AllRank = new real [NPar_All][NParVar];
+
+
+//    load data
+      const char *Filename[3] = { AGORA_HaloPar_Filename, AGORA_DiskPar_Filename, AGORA_BulgePar_Filename };
+      const int   MaxLine     = 1024;  // maximum number of characters per line
+
+      char  *Line      = new char [MaxLine];
+      char  *FirstChar = NULL;
+      FILE  *File      = NULL;
+      long   NLoad     = 0;
+
+//    loop over all three particle tables
+      for (int t=0; t<3; t++)
+      {
+         Aux_Message( stdout, "   Loading particles from the file \"%s\" ... ", Filename[t] );
+
+         File = fopen( Filename[t], "r" );
+
+         while ( fgets( Line, MaxLine, File ) != NULL )
+         {
+//          skip empty lines
+            if (  !CheckEmptyString( Line )  )
+            {
+               FirstChar = Line;
+
+//             find the first non-empty character
+               while ( *FirstChar == ' '  ||  *FirstChar == '\t' )   FirstChar ++;
+
+//             skip lines starting with "#"
+               if ( *FirstChar != '#' )
+               {
+                  if ( NLoad >= NPar_All )   Aux_Error( ERROR_INFO, "NLoad (%ld) >= NPar_All (%ld) !!\n", NLoad, NPar_All );
+
+//                note that the on-disk data format is (x, y, z, vx, vy, vz, mass),
+//                while here we want to convert it to  (mass, x, y, z, vx, vy, vz)
+#                 ifdef FLOAT8
+                  sscanf( Line, "%lf%lf%lf%lf%lf%lf%lf",
+                          ParData_AllRank[NLoad]+1, ParData_AllRank[NLoad]+2, ParData_AllRank[NLoad]+3,
+                          ParData_AllRank[NLoad]+4, ParData_AllRank[NLoad]+5, ParData_AllRank[NLoad]+6,
+                          ParData_AllRank[NLoad]+0 );
+#                 else
+                  sscanf( Line, "%f%f%f%f%f%f%f",
+                          ParData_AllRank[NLoad]+1, ParData_AllRank[NLoad]+2, ParData_AllRank[NLoad]+3,
+                          ParData_AllRank[NLoad]+4, ParData_AllRank[NLoad]+5, ParData_AllRank[NLoad]+6,
+                          ParData_AllRank[NLoad]+0 );
+#                 endif
+
+                  NLoad ++;
+               }
+            } // if (  !CheckEmptyString( Line )  )
+         } // while ( fgets( Line, MaxLine, File ) != NULL )
+
+         fclose( File );
+
+         Aux_Message( stdout, "done\n" );
+
+      } //for (int t=0; t<3; t++)
+
+      delete [] Line;
+
+   } // if ( MPI_Rank == 0 )
 
    MPI_Barrier( MPI_COMM_WORLD );
 
 
-// prepare to load data
-   if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Preparing to load data ... " );
+// get the number of particles in each rank and set the corresponding offsets
+   if ( NParVar*amr->Par->NPar_Active_AllRank > (long)__INT_MAX__ )
+      Aux_Error( ERROR_INFO, "Total number of particle attributes to be sent (%ld) exceeds the maximum integer (%ld) !!\n",
+                 (long)NParVar*amr->Par->NPar_Active_AllRank, (long)__INT_MAX__ );
 
-   const int NCluster = ( ClusterMerger_Coll ) ? 2 : 1;
-   long NPar_ThisRank[2]={0,0}, FileOffset[2];  // [0/1] --> cluster 1/2
+   int NSend[MPI_NRank], SendDisp[MPI_NRank], NPar_MyRank=(int)amr->Par->NPar_AcPlusInac;
 
-   for (int c=0; c<NCluster; c++)
+   MPI_Gather( &NPar_MyRank, 1, MPI_INT, NSend, 1, MPI_INT, 0, MPI_COMM_WORLD );
+
+   if ( MPI_Rank == 0 )
    {
-//    get the number of particles loaded by each rank for each cluster
-      long NPar_EachRank[MPI_NRank];
+      for (int r=0; r<MPI_NRank; r++)  NSend[r] *= NParVar;
 
-      if ( c == 0 )  NPar_ThisRank[0] = NPar[0] / MPI_NRank + ( (MPI_Rank<NPar[0]%MPI_NRank)?1:0 );
-      else           NPar_ThisRank[1] = amr->Par->NPar_AcPlusInac - NPar_ThisRank[0];
-
-      MPI_Allgather( &NPar_ThisRank[c], 1, MPI_LONG, NPar_EachRank, 1, MPI_LONG, MPI_COMM_WORLD );
-
-//    check if the total number of particles is correct
-      long NPar_Check = 0;
-      for (int r=0; r<MPI_NRank; r++)  NPar_Check += NPar_EachRank[r];
-      if ( NPar_Check != NPar[c] )
-         Aux_Error( ERROR_INFO, "total number of particles in cluster %d: found (%ld) != expect (%ld) !!\n",
-                    c, NPar_Check, NPar[c] );
-
-//    set the file offset for this rank
-      FileOffset[c] = 0;
-      for (int r=0; r<MPI_Rank; r++)   FileOffset[c] = FileOffset[c] + NPar_EachRank[r]*NParVar*sizeof(real_par_in);
+      SendDisp[0] = 0;
+      for (int r=1; r<MPI_NRank; r++)  SendDisp[r] = SendDisp[r-1] + NSend[r-1];
    }
+
+
+// send particles from the root rank to all ranks
+   if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Sending particles from the root rank to all ranks ... " );
+   real (*ParData_MyRank)[NParVar] = new real [NPar_MyRank][NParVar];
+
+#  ifdef FLOAT8
+   MPI_Scatterv( ParData_AllRank[0], NSend, SendDisp, MPI_DOUBLE, ParData_MyRank[0], NPar_MyRank*NParVar, MPI_DOUBLE, 0, MPI_COMM_WORLD );
+#  else
+   MPI_Scatterv( ParData_AllRank[0], NSend, SendDisp, MPI_FLOAT,  ParData_MyRank[0], NPar_MyRank*NParVar, MPI_FLOAT,  0, MPI_COMM_WORLD );
+#  endif
+
+   delete [] ParData_AllRank;
 
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "done\n" );
 
 
-// load data to the particle repository
-   real_par_in (*ParData_ThisRank)[NParVar] = new real_par_in [ MAX(NPar_ThisRank[0], NPar_ThisRank[1]) ][NParVar];
+// store data in the particle repository
+   if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Storing particles in the particle repository ... " );
 
-   for (int c=0; c<NCluster; c++)
+   for (int p=0; p<NPar_MyRank; p++)
    {
-//    load data
-      if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Loading cluster %d ... ", c );
+      amr->Par->Mass[p] = ParData_MyRank[p][0];
+      amr->Par->PosX[p] = ParData_MyRank[p][1];
+      amr->Par->PosY[p] = ParData_MyRank[p][2];
+      amr->Par->PosZ[p] = ParData_MyRank[p][3];
+      amr->Par->VelX[p] = ParData_MyRank[p][4];
+      amr->Par->VelY[p] = ParData_MyRank[p][5];
+      amr->Par->VelZ[p] = ParData_MyRank[p][6];
 
-//    note that fread may fail for large files if sizeof(size_t) == 4 instead of 8
-      FILE *File = fopen( (c==0)?ClusterMerger_File_Par1:ClusterMerger_File_Par2, "rb" );
+//    synchronize all particles to the physical time at the base level
+      amr->Par->Time[p] = Time[0];
+   }
 
-      fseek( File, FileOffset[c], SEEK_SET );
-      fread( ParData_ThisRank, sizeof(real_par_in), NPar_ThisRank[c]*NParVar, File );
-      fclose( File );
+   delete [] ParData_MyRank;
 
-      if ( MPI_Rank == 0 )    Aux_Message( stdout, "done\n" );
+   if ( MPI_Rank == 0 )    Aux_Message( stdout, "done\n" );
 
 
-//    store data to the particle repository
-      if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Storing cluster %d to the particle repository ... ", c );
 
-      for (long p=0; p<NPar_ThisRank[c]; p++)
-      {
-//       particle index offset
-         const long pp = p + c*NPar_ThisRank[0];
+// convert to code units (assuming the loaded data have the units 1e9 Msun, kpc, and km/s)
+   if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Converting particle attributes to the code units ... " );
 
-//       we have assumed that particle data in the file are stored in the order [ID][Mass/PosX/Y/Z/VelX/Y/Z]
-//       --> convert to code unit before storing to the particle repository to avoid floating-point overflow
-//       --> we have assumed that the loaded data are in cgs
-         amr->Par->Mass[pp] = real( ParData_ThisRank[p][0] / UNIT_M );
+   const real UnitM = 1.0e9*Const_Msun/UNIT_M;
+   const real UnitL = 1.0e0*Const_kpc /UNIT_L;
+   const real UnitV = 1.0e0*Const_km  /UNIT_V;
 
-         amr->Par->PosX[pp] = real( ParData_ThisRank[p][1] / UNIT_L );
-         amr->Par->PosY[pp] = real( ParData_ThisRank[p][2] / UNIT_L );
-         amr->Par->PosZ[pp] = real( ParData_ThisRank[p][3] / UNIT_L );
+   for (int p=0; p<NPar_MyRank; p++)
+   {
+      amr->Par->Mass[p] *= UnitM;
+      amr->Par->PosX[p] *= UnitL;
+      amr->Par->PosY[p] *= UnitL;
+      amr->Par->PosZ[p] *= UnitL;
+      amr->Par->VelX[p] *= UnitV;
+      amr->Par->VelY[p] *= UnitV;
+      amr->Par->VelZ[p] *= UnitV;
+   }
 
-         amr->Par->VelX[pp] = real( ParData_ThisRank[p][4] / UNIT_V );
-         amr->Par->VelY[pp] = real( ParData_ThisRank[p][5] / UNIT_V );
-         amr->Par->VelZ[pp] = real( ParData_ThisRank[p][6] / UNIT_V );
-
-//       synchronize all particles to the physical time at the base level
-         amr->Par->Time[pp] = Time[0];
-      }
-
-      if ( MPI_Rank == 0 )    Aux_Message( stdout, "done\n" );
-   } // for (int c=0; c<NCluster; c++)
-
-   delete [] ParData_ThisRank;
+   if ( MPI_Rank == 0 )    Aux_Message( stdout, "done\n" );
 
 
 // shift center (assuming the center of loaded particles = [0,0,0])
-   if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Shifting particle center and adding bulk velocity ... " );
+   if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Shifting particle center ... " );
 
-   const double BoxCenter[3] = { 0.5*amr->BoxSize[0], 0.5*amr->BoxSize[1], 0.5*amr->BoxSize[2] };
-   real *Pos[3] = { amr->Par->PosX, amr->Par->PosY, amr->Par->PosZ };
+   const real BoxCenter[3] = { 0.5*amr->BoxSize[0], 0.5*amr->BoxSize[1], 0.5*amr->BoxSize[2] };
 
-   if ( ClusterMerger_Coll )
+   for (int p=0; p<NPar_MyRank; p++)
    {
-      const double ClusterCenter1[3]
-         = { BoxCenter[0]-0.5*ClusterMerger_Coll_D, BoxCenter[1]-0.5*ClusterMerger_Coll_B, BoxCenter[2] };
-      const double ClusterCenter2[3]
-         = { BoxCenter[0]+0.5*ClusterMerger_Coll_D, BoxCenter[1]+0.5*ClusterMerger_Coll_B, BoxCenter[2] };
-
-      for (long p=0; p<NPar_ThisRank[0]; p++)
-      for (int d=0; d<3; d++)
-         Pos[d][p] += ClusterCenter1[d];
-
-      for (long p=NPar_ThisRank[0]; p<amr->Par->NPar_AcPlusInac; p++)
-      for (int d=0; d<3; d++)
-         Pos[d][p] += ClusterCenter2[d];
-   }
-
-   else
-   {
-      for (long p=0; p<amr->Par->NPar_AcPlusInac; p++)
-      for (int d=0; d<3; d++)
-         Pos[d][p] += BoxCenter[d];
-   }
-
-
-// add the bulk velocity (to velocity-x only)
-   if ( ClusterMerger_Coll )
-   {
-      for (long p=0;                p<NPar_ThisRank[0];          p++)  amr->Par->VelX[p] += ClusterMerger_Coll_BulkVel1;
-      for (long p=NPar_ThisRank[0]; p<amr->Par->NPar_AcPlusInac; p++)  amr->Par->VelX[p] += ClusterMerger_Coll_BulkVel2;
+      amr->Par->PosX[p] += BoxCenter[0];
+      amr->Par->PosY[p] += BoxCenter[1];
+      amr->Par->PosZ[p] += BoxCenter[2];
    }
 
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "done\n" );
-   */
 
 
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "%s ... done\n", __FUNCTION__ );
@@ -212,8 +233,3 @@ void Par_Init_ByFunction()
 
 
 #endif // #ifdef PARTICLE
-
-
-
-
-
