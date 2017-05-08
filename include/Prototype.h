3e30017e647e44ab6ad42c925434244ecb37b08a
Hsi-Yu Schive <hyschive@gmail.com>
2017-05-08 14:47:20 -0500

    Support the new refinement flag on particle mass per cell "OPT__FLAG_PAR_MASS_CELL"
    Former-commit-id: b065d5d4bd20f86c9909afadfba33fa5d399d250

--- include/Prototype.h
+++ include/Prototype.h
@@ -237,10 +237,10 @@ void Output_BoundaryFlagList( const int option, const int lv, const char *commen
 // Refine
 void FindFather( const int lv, const int Mode );
 void Flag_Real( const int lv, const UseLBFunc_t UseLBFunc );
-bool Flag_Check( const int lv, const int PID, const int i, const int j, const int k,
+bool Flag_Check( const int lv, const int PID, const int i, const int j, const int k, const real dv,
                  const real Fluid[][PS1][PS1][PS1], const real Pot[][PS1][PS1], const real Pres[][PS1][PS1],
                  const real *Lohner_Var, const real *Lohner_Ave, const real *Lohner_Slope, const int Lohner_NVar,
-                 const real ParCount[][PS1][PS1] );
+                 const real ParCount[][PS1][PS1], const real ParDens[][PS1][PS1] );
 bool Flag_UserCriteria( const int i, const int j, const int k, const int lv, const int PID, const double Threshold);
 bool Flag_Region( const int i, const int j, const int k, const int lv, const int PID );
 bool Flag_Lohner( const int i, const int j, const int k, const OptLohnerForm_t Form, const real *Var1D, const real *Ave1D,
