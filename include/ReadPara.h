ae6cca6d485289e6a6dee2ffcc551ab0e8e45e66
Hsi-Yu Schive <hyschive@gmail.com>
2017-06-09 20:40:30 -0500

    Update ReadPara.h to check the legal range of each parameter

--- include/ReadPara.h
+++ include/ReadPara.h
@@ -9,7 +9,6 @@ bool Aux_CheckFileExist( const char *FileName );
 void Aux_Error( const char *File, const int Line, const char *Func, const char *Format, ... );
 
 #define NPARA_MAX    1000     // maximum number of parameters
-#define PARA_SET      '#'     // character to indicate that a given parameter has been set properly
 #define TYPE_INT        1     // different data types
 #define TYPE_LONG       2
 #define TYPE_UINT       3
@@ -19,19 +18,30 @@ void Aux_Error( const char *File, const int Line, const char *Func, const char *
 #define TYPE_DOUBLE     7
 #define TYPE_STRING     8
 
+#define GET_VOID( type, var )    ( *( (type*)(var) ) )   // helper function to converth the (void*) pointer
+
 
 
 
 //-------------------------------------------------------------------------------------------------------
 // Structure   :  ReadPara_t
-// Description :  Data structure ...
+// Description :  Data structure for loading runtime parameters
 //
-// Data Member :
+// Data Member :  NPara  : Total number of parameters
+//                Key    : Parameter names
+//                Ptr    : Pointer to the parameter variables
+//                Type   : Parameter data types
+//                Loaded : Record whether each parameter has been properly set or not (true --> set already)
+//                Def    : Default values
+//                Min    : Minimum allowed values
+//                Max    : Maximum allowed values
 //
 // Method      :  ReadPara_t : Constructor
 //               ~ReadPara_t : Destructor
 //                Add        : Add a new parameter
-//                Read       : Read parameters from the parameter file
+//                Read       : Read parameters from the given parameter file
+//                SetDefault : Set the default values
+//                Validate   : Check if all parameters are within their allowed range
 //-------------------------------------------------------------------------------------------------------
 struct ReadPara_t
 {
@@ -42,7 +52,10 @@ struct ReadPara_t
    char (*Key)[MAX_STRING];
    void **Ptr;
    int   *Type;
-   void **Default;
+   bool  *Loaded;
+   void **Def;
+   void **Min;
+   void **Max;
 
 
    //===================================================================================
@@ -54,11 +67,16 @@ struct ReadPara_t
    ReadPara_t()
    {
 
-      NPara   = 0;
-      Key     = new char  [NPARA_MAX][MAX_STRING];
-      Ptr     = new void* [NPARA_MAX];
-      Type    = new int   [NPARA_MAX];
-      Default = new void* [NPARA_MAX];
+      NPara  = 0;
+      Key    = new char  [NPARA_MAX][MAX_STRING];
+      Ptr    = new void* [NPARA_MAX];
+      Type   = new int   [NPARA_MAX];
+      Loaded = new bool  [NPARA_MAX];
+      Def    = new void* [NPARA_MAX];
+      Min    = new void* [NPARA_MAX];
+      Max    = new void* [NPARA_MAX];
+
+      for (int t=0; t<NPARA_MAX; t++)  Loaded[t] = false;
 
    } // METHOD : ReadPara_t
 
@@ -75,13 +93,18 @@ struct ReadPara_t
 
       for (int t=0; t<NPara; t++)
       {
-         delete Default[t];
+         delete Def[t];
+         delete Min[t];
+         delete Max[t];
       }
 
       delete [] Key;
       delete [] Ptr;
       delete [] Type;
-      delete [] Default;
+      delete [] Loaded;
+      delete [] Def;
+      delete [] Min;
+      delete [] Max;
 
    } // METHOD : ~ReadPara_t
 
@@ -93,10 +116,10 @@ struct ReadPara_t
    //
    // Note        :  1. This function stores the name, address, and data type of the new parameter
    //                2. Data type (e.g., integer, float, ...) is determined by the input pointer
-   //                3. NewPtr, NewDefault, NewMin, and NewMax must have the same data type
+   //                3. NewPtr, NewDef, NewMin, and NewMax must have the same data type
    //===================================================================================
    template <typename T>
-   void Add( const char NewKey[], T* NewPtr, T NewDefault )
+   void Add( const char NewKey[], T* NewPtr, T NewDef, T NewMin, T NewMax )
    {
 
       if ( NPara >= NPARA_MAX )  Aux_Error( ERROR_INFO, "exceed the maximum number of parameters (%d) !!\n", NPARA_MAX );
@@ -121,9 +144,14 @@ struct ReadPara_t
          Aux_Error( ERROR_INFO, "unsupported data type for \"%s\" (char*, float*, double*, int*, long*, unit*, ulong*, bool* only) !!\n",
                     NewKey );
 
-//    set the default values
-      Default[NPara] = new T;
-      *( (T*)Default[NPara] ) = NewDefault;
+//    set the default value and the allowed range of each variable
+      Def[NPara] = new T;
+      Min[NPara] = new T;
+      Max[NPara] = new T;
+
+      GET_VOID( T, Def[NPara] ) = NewDef;
+      GET_VOID( T, Min[NPara] ) = NewMin;
+      GET_VOID( T, Max[NPara] ) = NewMax;
 
       NPara ++;
 
@@ -177,7 +205,7 @@ struct ReadPara_t
             if (  strcmp( Key[k], LoadKey ) == 0  )
             {
                MatchIdx  = k;
-               Key[k][0] = PARA_SET;   // reset the key to any strange character to detect duplicate parameters
+               Loaded[k] = true;
                break;
             }
          }
@@ -186,13 +214,13 @@ struct ReadPara_t
          {
             switch ( Type[MatchIdx] )
             {
-               case TYPE_INT    :   *( (int*   )Ptr[MatchIdx] ) = (int   )atol( LoadValue );    break;
-               case TYPE_LONG   :   *( (long*  )Ptr[MatchIdx] ) = (long  )atol( LoadValue );    break;
-               case TYPE_UINT   :   *( (uint*  )Ptr[MatchIdx] ) = (uint  )atol( LoadValue );    break;
-               case TYPE_ULONG  :   *( (ulong* )Ptr[MatchIdx] ) = (ulong )atol( LoadValue );    break;
-               case TYPE_BOOL   :   *( (bool*  )Ptr[MatchIdx] ) = (bool  )atol( LoadValue );    break;
-               case TYPE_FLOAT  :   *( (float* )Ptr[MatchIdx] ) = (float )atof( LoadValue );    break;
-               case TYPE_DOUBLE :   *( (double*)Ptr[MatchIdx] ) = (double)atof( LoadValue );    break;
+               case TYPE_INT    :   GET_VOID( int,    Ptr[MatchIdx] ) = (int   )atol( LoadValue );    break;
+               case TYPE_LONG   :   GET_VOID( long,   Ptr[MatchIdx] ) = (long  )atol( LoadValue );    break;
+               case TYPE_UINT   :   GET_VOID( uint,   Ptr[MatchIdx] ) = (uint  )atol( LoadValue );    break;
+               case TYPE_ULONG  :   GET_VOID( ulong,  Ptr[MatchIdx] ) = (ulong )atol( LoadValue );    break;
+               case TYPE_BOOL   :   GET_VOID( bool,   Ptr[MatchIdx] ) = (bool  )atol( LoadValue );    break;
+               case TYPE_FLOAT  :   GET_VOID( float,  Ptr[MatchIdx] ) = (float )atof( LoadValue );    break;
+               case TYPE_DOUBLE :   GET_VOID( double, Ptr[MatchIdx] ) = (double)atof( LoadValue );    break;
                case TYPE_STRING :   strcpy( (char*)Ptr[MatchIdx], LoadValue );                  break;
 
                default: Aux_Error( ERROR_INFO, "unsupported data type (char*, float*, double*, int*, long*, unit*, ulong*, bool* only) !!\n" );
@@ -216,7 +244,7 @@ struct ReadPara_t
 
 
 //    validate the parameters
-
+      Validate();
 
    } // METHOD : Read
 
@@ -225,7 +253,7 @@ struct ReadPara_t
    // Constructor :  SetDefault
    // Description :  Set parameters missing in the runtime parameter file to their default values
    //
-   // Note        :  1. Parameters already set by the runtime parameter file have Key[0] == PARA_SET
+   // Note        :  1. Parameters already set by the runtime parameter file will have Loaded[] == true
    //                2. We do NOT set default values for strings. An error will be raised instead.
    //===================================================================================
    void SetDefault()
@@ -233,20 +261,20 @@ struct ReadPara_t
 
       for (int t=0; t<NPara; t++)
       {
-         if ( Key[t][0] != PARA_SET )
+         if ( !Loaded[t] )
          {
             long   def_int = NULL_INT;
             double def_flt = NULL_REAL;
 
             switch ( Type[t] )
             {
-               case TYPE_INT    :   def_int = *( (int*   )Ptr[t] ) = *( (int*   )Default[t] );  break;
-               case TYPE_LONG   :   def_int = *( (long*  )Ptr[t] ) = *( (long*  )Default[t] );  break;
-               case TYPE_UINT   :   def_int = *( (uint*  )Ptr[t] ) = *( (uint*  )Default[t] );  break;
-               case TYPE_ULONG  :   def_int = *( (ulong* )Ptr[t] ) = *( (ulong* )Default[t] );  break;
-               case TYPE_BOOL   :   def_int = *( (bool*  )Ptr[t] ) = *( (bool*  )Default[t] );  break;
-               case TYPE_FLOAT  :   def_flt = *( (float* )Ptr[t] ) = *( (float* )Default[t] );  break;
-               case TYPE_DOUBLE :   def_flt = *( (double*)Ptr[t] ) = *( (double*)Default[t] );  break;
+               case TYPE_INT    :   def_int = GET_VOID( int,    Ptr[t] ) = GET_VOID( int,    Def[t] );   break;
+               case TYPE_LONG   :   def_int = GET_VOID( long,   Ptr[t] ) = GET_VOID( long,   Def[t] );   break;
+               case TYPE_UINT   :   def_int = GET_VOID( uint,   Ptr[t] ) = GET_VOID( uint,   Def[t] );   break;
+               case TYPE_ULONG  :   def_int = GET_VOID( ulong,  Ptr[t] ) = GET_VOID( ulong,  Def[t] );   break;
+               case TYPE_BOOL   :   def_int = GET_VOID( bool,   Ptr[t] ) = GET_VOID( bool,   Def[t] );   break;
+               case TYPE_FLOAT  :   def_flt = GET_VOID( float,  Ptr[t] ) = GET_VOID( float,  Def[t] );   break;
+               case TYPE_DOUBLE :   def_flt = GET_VOID( double, Ptr[t] ) = GET_VOID( double, Def[t] );   break;
                case TYPE_STRING :   Aux_Error( ERROR_INFO, "string variable \"%s\" is not set !!\n", Key[t] ); break;
 
                default: Aux_Error( ERROR_INFO, "no default value for this data type (float*, double*, int*, long*, unit*, ulong*, bool* only) !!\n" );
@@ -267,13 +295,61 @@ struct ReadPara_t
 
    } // METHOD : SetDefault
 
+
+   //===================================================================================
+   // Constructor :  Validate
+   // Description :  Validate if all parameters are within their correct range
+   //
+   // Note        :  1. Correct range: Min <= Value <= Max
+   //                2. No check for bool and string variables
+   //===================================================================================
+   void Validate()
+   {
+
+//    helper macros for different data types
+//    ref: https://stackoverflow.com/questions/3419332/c-preprocessor-stringify-the-result-of-a-macro
+#     define QUOTE( str )  #str
+#     define EXPAND_AND_QUOTE( str )   QUOTE( str )
+#     define CHECK_RANGE( type, format )                                                                                   \
+      {                                                                                                                    \
+         if (  GET_VOID(type,Ptr[t]) < GET_VOID(type,Min[t])  ||  GET_VOID(type,Ptr[t]) > GET_VOID(type,Max[t])  )         \
+            Aux_Error( ERROR_INFO, "parameter \"%s\" = " EXPAND_AND_QUOTE(format) " is not within the correct range ["     \
+                       EXPAND_AND_QUOTE(format) " ... " EXPAND_AND_QUOTE(format) "] !!\n",                                 \
+                       Key[t], GET_VOID(type,Ptr[t]), GET_VOID(type,Min[t]), GET_VOID(type,Max[t]) );                      \
+      }
+
+
+      for (int t=0; t<NPara; t++)
+      {
+         if ( Type[t] != TYPE_BOOL  &&  Type[t] != TYPE_STRING )
+         {
+            switch ( Type[t] )
+            {
+               case TYPE_INT    :   CHECK_RANGE( int,    %d     );   break;
+               case TYPE_LONG   :   CHECK_RANGE( long,   %ld    );   break;
+               case TYPE_UINT   :   CHECK_RANGE( uint,   %u     );   break;
+               case TYPE_ULONG  :   CHECK_RANGE( ulong,  %lu    );   break;
+               case TYPE_FLOAT  :   CHECK_RANGE( float,  %14.7e );   break;
+               case TYPE_DOUBLE :   CHECK_RANGE( double, %14.7e );   break;
+
+               default: Aux_Error( ERROR_INFO, "no check for this data type (float*, double*, int*, long*, unit*, ulong* only) !!\n" );
+            }
+         }
+      } // for (int t=0; t<NPara; t++)
+
+#     undef QUOTE
+#     undef EXPAND_AND_QUOTE
+#     undef CHECK_RANGE
+
+   } // METHOD : Validate
+
+
 }; // struct ReadPara_t
 
 
 
 // remove symbolic constants since they are only used in this structure
 #undef NPARA_MAX
-#undef PARA_SET
 #undef TYPE_INT
 #undef TYPE_LONG
 #undef TYPE_UINT
@@ -282,6 +358,7 @@ struct ReadPara_t
 #undef TYPE_DOUBLE
 #undef TYPE_BOOL
 #undef TYPE_STRING
+#undef GET_VOID
 
 
 
