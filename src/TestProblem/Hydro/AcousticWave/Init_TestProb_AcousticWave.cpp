2af3b484c57f02eecc484c90af62126bad91816f
Hsi-Yu Schive <hyschive@gmail.com>
2017-06-15 15:19:25 -0500

    Add the test problem "AcousticWave" to the new test-problem infrastructure
    Former-commit-id: 09a7fe8e10c8c56a4eb7130f9fc93719a8678f25

--- src/TestProblem/Hydro/AcousticWave/Init_TestProb_AcousticWave.cpp
+++ src/TestProblem/Hydro/AcousticWave/Init_TestProb_AcousticWave.cpp
@@ -5,8 +5,13 @@
 
 // problem-specific global variables
 // =======================================================================================
-static double var_double;
-static int    var_int;
+static double Acoustic_RhoAmp;      // amplitude of the density perturbation (assuming background density = 1.0)
+static double Acoustic_Cs;          // sound speed
+static double Acoustic_v0;          // background velocity
+static double Acoustic_Sign;        // (+1/-1) --> (right/left-moving wave)
+static double Acoustic_Phase0;      // initial phase shift
+
+static double Acoustic_WaveLength;  // wavelength
 // =======================================================================================
 
 
@@ -26,16 +31,31 @@ void Validate()
 
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Validating test problem %d ...\n", TESTPROB_ID );
 
-
-// examples
-/*
 #  if ( MODEL != HYDRO )
    Aux_Error( ERROR_INFO, "MODEL != HYDRO !!\n" );
 #  endif
 
+#  ifdef GRAVITY
+   Aux_Error( ERROR_INFO, "GRAVITY must be disabled !!\n" );
+#  endif
+
+#  ifndef FLOAT8
+   Aux_Error( ERROR_INFO, "FLOAT8 must be enabled !!\n" );
+#  endif
+
+#  ifdef COMOVING
+   Aux_Error( ERROR_INFO, "COMOVING must be disabled !!\n" );
+#  endif
+
+#  ifdef PARTICLE
+   Aux_Error( ERROR_INFO, "PARTICLE must be disabled !!\n" );
+#  endif
+
    if ( amr->BoxSize[0] != amr->BoxSize[1]  ||  amr->BoxSize[0] != amr->BoxSize[2] )
-      Aux_Error( ERROR_INFO, "simulation domain must be CUBIC !!\n" );
-*/
+      Aux_Error( ERROR_INFO, "simulation domain must be cubic !!\n" );
+
+   if ( OPT__BC_FLU[0] != BC_FLU_PERIODIC )
+      Aux_Error( ERROR_INFO, "please set \"OPT__BC_FLU_* = 1\" (i.e., periodic BC) !!\n" );
 
 
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "   Validating test problem %d ... done\n", TESTPROB_ID );
@@ -74,23 +94,29 @@ void SetParameter()
 // ********************************************************************************************************************************
 // ReadPara->Add( "KEY_IN_THE_FILE",   &VARIABLE,              DEFAULT,       MIN,              MAX               );
 // ********************************************************************************************************************************
-   ReadPara->Add( "var_double",        &var_double,            -1.0,          NoMin_double,     NoMax_double      );
-   ReadPara->Add( "var_int",           &var_int,               -1,            NoMin_int,        NoMax_int         );
+   ReadPara->Add( "Acoustic_RhoAmp",   &Acoustic_RhoAmp,       -1.0,          NoZero_double,    NoMax_double      );
+   ReadPara->Add( "Acoustic_Cs",       &Acoustic_Cs,           -1.0,          NoZero_double,    NoMax_double      );
+   ReadPara->Add( "Acoustic_v0",       &Acoustic_v0,            0.0,          NoMin_double,     NoMax_double      );
+   ReadPara->Add( "Acoustic_Sign",     &Acoustic_Sign,          1.0,          NoMin_double,     NoMax_double      );
+   ReadPara->Add( "Acoustic_Phase0",   &Acoustic_Phase0,        0.0,          NoMin_double,     NoMax_double      );
 
    ReadPara->Read( FileName );
 
    delete ReadPara;
 
-// set the default values
+// force Acoustic_Sign to be +1.0/-1.0
+   if ( Acoustic_Sign >= 0.0 )   Acoustic_Sign = +1.0;
+   else                          Acoustic_Sign = -1.0;
 
 
 // (2) set the problem-specific derived parameters
+   Acoustic_WaveLength = amr->BoxSize[0] / sqrt(3.0);
 
 
 // (3) reset other general-purpose parameters
 //     --> a helper macro PRINT_WARNING is defined in TestProb.h
-   const double End_T_Default    = __FLT_MAX__;
-   const long   End_Step_Default = 10;
+   const double End_T_Default    = Acoustic_WaveLength / Acoustic_Cs;
+   const long   End_Step_Default = __INT_MAX__;
 
    if ( END_STEP < 0 ) {
       END_STEP = End_Step_Default;
@@ -107,9 +133,13 @@ void SetParameter()
    if ( MPI_Rank == 0 )
    {
       Aux_Message( stdout, "=============================================================================\n" );
-      Aux_Message( stdout, "  test problem ID           = %d\n",     TESTPROB_ID );
-      Aux_Message( stdout, "  var_double                = %13.7e\n", var_double );
-      Aux_Message( stdout, "  var_int                   = %d\n",     var_int );
+      Aux_Message( stdout, "  test problem ID     = %d\n",      TESTPROB_ID );
+      Aux_Message( stdout, "  density amplitude   = % 14.7e\n", Acoustic_RhoAmp );
+      Aux_Message( stdout, "  sound speed         = % 14.7e\n", Acoustic_Cs );
+      Aux_Message( stdout, "  background velocity = % 14.7e\n", Acoustic_v0 );
+      Aux_Message( stdout, "  sign (R/L)          = % 14.7e\n", Acoustic_Sign );
+      Aux_Message( stdout, "  initial phase shift = % 14.7e\n", Acoustic_Phase0 );
+      Aux_Message( stdout, "  wavelength          = % 14.7e\n", Acoustic_WaveLength );
       Aux_Message( stdout, "=============================================================================\n" );
    }
 
@@ -140,14 +170,26 @@ void SetParameter()
 void SetGridIC( real *fluid, const double x, const double y, const double z, const double Time )
 {
 
-// make sure that all model-dependent variables are enclosed within the corresponding symbolic constant check
-// so that the code can be compiled successfully even when these variables are not defined
 #  if ( MODEL == HYDRO )
-   fluid[DENS] = 1.0;
-   fluid[MOMX] = 0.0;
-   fluid[MOMY] = 0.0;
-   fluid[MOMZ] = 0.0;
-   fluid[ENGY] = 1.0;
+   const double r         = 1.0/sqrt(3.0)*( x + y + z ) - Acoustic_v0*Time;
+   const double _Gamma_m1 = 1.0/(GAMMA-1.0);
+
+   double v1, P0, P1, Phase, WaveK, WaveW;
+
+   v1    = Acoustic_Sign*Acoustic_Cs*Acoustic_RhoAmp;
+   P0    = SQR(Acoustic_Cs)/GAMMA;
+   P1    = SQR(Acoustic_Cs)*Acoustic_RhoAmp;
+
+   WaveK = 2.0*M_PI/Acoustic_WaveLength;
+   WaveW = 2.0*M_PI/(Acoustic_WaveLength/Acoustic_Cs);
+   Phase = WaveK*r - Acoustic_Sign*WaveW*Time + Acoustic_Phase0;
+
+   fluid[DENS] = 1.0 + Acoustic_RhoAmp*cos(Phase);
+   fluid[MOMX] = fluid[DENS]*( v1*cos(Phase) + Acoustic_v0 ) / sqrt(3.0);
+   fluid[MOMY] = fluid[MOMX];
+   fluid[MOMZ] = fluid[MOMX];
+   fluid[ENGY] = 0.5*( SQR(fluid[MOMX]) + SQR(fluid[MOMY]) + SQR(fluid[MOMZ]) )/fluid[DENS]
+                 + ( P0 + P1*cos(Phase) )*_Gamma_m1;
 #  endif
 
 } // FUNCTION : SetGridIC
@@ -155,7 +197,30 @@ void SetGridIC( real *fluid, const double x, const double y, const double z, con
 
 
 //-------------------------------------------------------------------------------------------------------
-// Function    :  Init_TestProb_Template
+// Function    :  OutputError
+// Description :  Output the L1 error
+//
+// Note        :  1. Invoke Output_L1Error()
+//                2. Use SetGridIC() to provide the analytical solution at any given time
+//
+// Parameter   :  None
+//
+// Return      :  None
+//-------------------------------------------------------------------------------------------------------
+void OutputError()
+{
+
+   const char Prefix[100]     = "AcousticWave";
+   const OptOutputPart_t Part = OUTPUT_DIAG;
+
+   Output_L1Error( SetGridIC, Prefix, Part, NULL_REAL, NULL_REAL, NULL_REAL );
+
+} // FUNCTION : OutputError
+
+
+
+//-------------------------------------------------------------------------------------------------------
+// Function    :  Init_TestProb_AcousticWave
 // Description :  Test problem initializer
 //
 // Note        :  None
@@ -164,7 +229,7 @@ void SetGridIC( real *fluid, const double x, const double y, const double z, con
 //
 // Return      :  None
 //-------------------------------------------------------------------------------------------------------
-void Init_TestProb_Template()
+void Init_TestProb_AcousticWave()
 {
 
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "%s ...\n", __FUNCTION__ );
@@ -180,7 +245,7 @@ void Init_TestProb_Template()
 
 // set the function pointers of various problem-specific routines
    Init_Function_User_Ptr   = SetGridIC;
-   Output_User_Ptr          = NULL;       // example: Hydro/AcousticWave/Init_TestProb_AcousticWave.cpp --> OutputError()
+   Output_User_Ptr          = OutputError;
    Flag_User_Ptr            = NULL;
    Mis_GetTimeStep_User_Ptr = NULL;
    Aux_Record_User_Ptr      = NULL;
@@ -191,4 +256,4 @@ void Init_TestProb_Template()
 
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "%s ... done\n", __FUNCTION__ );
 
-} // FUNCTION : Init_TestProb_Template
+} // FUNCTION : Init_TestProb_AcousticWave
