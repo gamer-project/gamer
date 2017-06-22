4e642cfe5ba34f6bac28b1852e761ad2cdc76abb
Hsi-Yu Schive <hyschive@gmail.com>
2017-06-22 17:19:38 -0500

    Grackle: add CPU_GrackleSolver_Original()

--- src/Grackle/CPU_Grackle/CPU_GrackleSolver_Original.cpp
+++ src/Grackle/CPU_Grackle/CPU_GrackleSolver_Original.cpp
@@ -14,13 +14,12 @@
 // Note        :  1. Work for "GRACKLE_MODE == GRACKLE_MODE_ORI"
 //                   --> No matter GPU is enabled or not
 //
-// Parameter   :  Che_FieldData :
-//                Che_Units     :
+// Parameter   :  Che_FieldData : Array of Grackle "grackle_field_data" objects
+//                Che_Units     : Grackle "code_units" object
 //                NPatchGroup   : Number of patch groups to be evaluated
 //                dt            : Time interval to advance solution
 //-----------------------------------------------------------------------------------------
-void CPU_GrackleSolver_Original( grackle_field_data Che_FieldData[], const code_units Che_Units,
-                                 const int NPatchGroup, const real dt )
+void CPU_GrackleSolver_Original( grackle_field_data Che_FieldData[], code_units Che_Units, const int NPatchGroup, const real dt )
 {
 
    const int NPatch = NPatchGroup*8;
@@ -28,10 +27,11 @@ void CPU_GrackleSolver_Original( grackle_field_data Che_FieldData[], const code_
 
 // loop over all patches
 #  pragma omp parallel for schedule( runtime )
-   for (int P=0; P<NPatch; P++)
+   for (int p=0; p<NPatch; p++)
    {
-
-   } // for (int P=0; P<NPatch; P++)
+      if (  solve_chemistry( &Che_Units, &Che_FieldData[p], dt ) == 0  )
+         Aux_Error( ERROR_INFO, "Grackle solve_chemistry() failed !!\n" );
+   }
 
 } // FUNCTION : CPU_GrackleSolver_Original
 
