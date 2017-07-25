7e0a3b14fd75ed5489d6e7d3baa3573d26cb2177
Hsi-Yu Schive <hyschive@gmail.com>
2017-07-25 16:55:33 -0500

    Grackle: add the minimum pressure estimated from the Jeans length --> JEANS_MIN_PRES

--- include/Global.h
+++ include/Global.h
@@ -89,8 +89,8 @@ extern WAF_Limiter_t    OPT__WAF_LIMITER;
 extern Opt1stFluxCorr_t OPT__1ST_FLUX_CORR;
 extern OptRSolver1st_t  OPT__1ST_FLUX_CORR_SCHEME;
 extern bool             OPT__FLAG_PRES_GRADIENT, OPT__FLAG_LOHNER_ENGY, OPT__FLAG_LOHNER_PRES, OPT__FLAG_LOHNER_TEMP;
-extern bool             OPT__FLAG_VORTICITY, OPT__FLAG_JEANS;
-extern int              OPT__CK_NEGATIVE;
+extern bool             OPT__FLAG_VORTICITY, OPT__FLAG_JEANS, JEANS_MIN_PRES;
+extern int              OPT__CK_NEGATIVE, JEANS_MIN_PRES_LEVEL, JEANS_MIN_PRES_NCELL;
 extern double           MIN_DENS, MIN_PRES;
 #ifdef DUAL_ENERGY
 extern double           DUAL_ENERGY_SWITCH;
