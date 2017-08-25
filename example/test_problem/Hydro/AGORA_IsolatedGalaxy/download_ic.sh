ff8c005eea7cef55b8ae3d56dac5723c9759274d
Hsi-Yu Schive <hyschive@gmail.com>
2017-08-25 11:15:05 -0500

    Update the AGORA isolated galaxy test
    Former-commit-id: 53e3d25871037cc44c66c1f4fefba2400dc80c2f

--- example/test_problem/Hydro/AGORA_IsolatedGalaxy/download_ic.sh
+++ example/test_problem/Hydro/AGORA_IsolatedGalaxy/download_ic.sh
@@ -1,5 +1,5 @@
 wget --no-check-certificate -O ./LOW.tar.gz https://www.dropbox.com/sh/1xzt1rysy9v3a9l/AAAMlJBQG1OQFW4cjhp11Ex6a/LOW.tar.gz?dl=1
-wget https://bitbucket.org/grackle/grackle/raw/default/input/CloudyData_noUVB.h5
+wget https://bitbucket.org/grackle/grackle/raw/default/input/CloudyData_UVB=HM2012.h5
 tar xzvf LOW.tar.gz
 mv LOW/*.dat ./
 rmdir LOW
