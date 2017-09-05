3e1a64854b37d58e26f3eddfb3b8438e2c9ccacd
Hsi-Yu Schive <hyschive@gmail.com>
2017-09-04 21:10:13 -0500

    pdate the plotting script of the AGORA isolated galaxy test
    Former-commit-id: 0944415307f98a3c21608e76ce04c992fe0edb87

--- example/test_problem/Hydro/AGORA_IsolatedGalaxy/yt_script/plot_gas_temp-vs-dens.py
+++ example/test_problem/Hydro/AGORA_IsolatedGalaxy/yt_script/plot_gas_temp-vs-dens.py
@@ -1,16 +1,40 @@
+import argparse
+import sys
 import yt
 
-yt.enable_parallelism()
+# load the command-line parameters
+parser = argparse.ArgumentParser( description='Plot the gas density-temperature phase diagram' )
 
-idx_start   = 0
-idx_end     = 50
-didx        = 1
+parser.add_argument( '-s', action='store', required=True,  type=int, dest='idx_start',
+                     help='first data index' )
+parser.add_argument( '-e', action='store', required=True,  type=int, dest='idx_end',
+                     help='last data index' )
+parser.add_argument( '-d', action='store', required=False, type=int, dest='didx',
+                     help='delta data index [%(default)d]', default=1 )
+
+args=parser.parse_args()
+
+# take note
+print( '\nCommand-line arguments:' )
+print( '-------------------------------------------------------------------' )
+for t in range( len(sys.argv) ):
+   print str(sys.argv[t]),
+print( '' )
+print( '-------------------------------------------------------------------\n' )
+
+
+idx_start   = args.idx_start
+idx_end     = args.idx_end
+didx        = args.didx
 prefix      = '../'
 
 colormap    = 'algae'
 width_kpc   = 30
 nbin        = 300
 dpi         = 150
+
+
+yt.enable_parallelism()
 
 ts = yt.load( [ prefix+'/Data_%06d'%idx for idx in range(idx_start, idx_end+1, didx) ] )
 #ts = yt.load( 'Data_??????' )
