a94e587839a78d1743d7de2f953e99ef776ab6e8
Hsi-Yu Schive <hyschive@gmail.com>
2017-10-10 16:40:54 -0500

    Remove the directory "Model_Template"

--- src/Init/Init_TestProb.cpp
+++ src/Init/Init_TestProb.cpp
@@ -12,6 +12,7 @@ void Init_TestProb_Hydro_ClusterMerger_vs_Flash();
 void Init_TestProb_Hydro_AGORA_IsolatedGalaxy();
 void Init_TestProb_Hydro_Caustic();
 void Init_TestProb_Hydro_SphericalCollapse();
+void Init_TestProb_Hydro_KelvinHelmholtzInstability();
 
 void Init_TestProb_ELBDM_ExtPot();
 
@@ -37,17 +38,18 @@ void Init_TestProb()
 // ******************************************************************
    switch ( TESTPROB_ID )
    {
-      case TESTPROB_NONE :                                                                            break;
+      case TESTPROB_NONE :                                                                                  break;
 
-      case TESTPROB_HYDRO_BLAST_WAVE :                Init_TestProb_Hydro_BlastWave();                break;
-      case TESTPROB_HYDRO_ACOUSTIC_WAVE :             Init_TestProb_Hydro_AcousticWave();             break;
-      case TESTPROB_HYDRO_BONDI :                     Init_TestProb_Hydro_Bondi();                    break;
-      case TESTPROB_HYDRO_CLUSTER_MERGER_VS_FLASH :   Init_TestProb_Hydro_ClusterMerger_vs_Flash();   break;
-      case TESTPROB_HYDRO_AGORA_ISOLATED_GALAXY :     Init_TestProb_Hydro_AGORA_IsolatedGalaxy();     break;
-      case TESTPROB_HYDRO_CAUSTIC :                   Init_TestProb_Hydro_Caustic();                  break;
-      case TESTPROB_HYDRO_SPHERICAL_COLLAPSE :        Init_TestProb_Hydro_SphericalCollapse();        break;
+      case TESTPROB_HYDRO_BLAST_WAVE :                   Init_TestProb_Hydro_BlastWave();                   break;
+      case TESTPROB_HYDRO_ACOUSTIC_WAVE :                Init_TestProb_Hydro_AcousticWave();                break;
+      case TESTPROB_HYDRO_BONDI :                        Init_TestProb_Hydro_Bondi();                       break;
+      case TESTPROB_HYDRO_CLUSTER_MERGER_VS_FLASH :      Init_TestProb_Hydro_ClusterMerger_vs_Flash();      break;
+      case TESTPROB_HYDRO_AGORA_ISOLATED_GALAXY :        Init_TestProb_Hydro_AGORA_IsolatedGalaxy();        break;
+      case TESTPROB_HYDRO_CAUSTIC :                      Init_TestProb_Hydro_Caustic();                     break;
+      case TESTPROB_HYDRO_SPHERICAL_COLLAPSE :           Init_TestProb_Hydro_SphericalCollapse();           break;
+      case TESTPROB_HYDRO_KELVIN_HELMHOLTZ_INSTABILITY : Init_TestProb_Hydro_KelvinHelmholtzInstability();  break;
 
-      case TESTPROB_ELBDM_EXTPOT :                    Init_TestProb_ELBDM_ExtPot();                   break;
+      case TESTPROB_ELBDM_EXTPOT :                       Init_TestProb_ELBDM_ExtPot();                      break;
 
       default: Aux_Error( ERROR_INFO, "unsupported TESTPROB_ID (%d) !!\n", TESTPROB_ID );
    } // switch( TESTPROB_ID )
