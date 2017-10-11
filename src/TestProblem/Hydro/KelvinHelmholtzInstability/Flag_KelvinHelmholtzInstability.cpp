faa27ce42cb30b1bf44783651aa3cee0aa72308b
Hsi-Yu Schive <hyschive@gmail.com>
2017-10-10 20:17:41 -0500

    Add "Kelvin-Helmholtz instability" to the new test problem infrastructure

--- src/TestProblem/Hydro/KelvinHelmholtzInstability/Flag_KelvinHelmholtzInstability.cpp
+++ src/TestProblem/Hydro/KelvinHelmholtzInstability/Flag_KelvinHelmholtzInstability.cpp
@@ -2,12 +2,14 @@
 #include "GAMER.h"
 
 extern bool KH_AllRankSame;
+extern int  KH_RefineShearMaxLv;
+extern int  KH_PeriodicZFactor;
 
 
 
 
 //-------------------------------------------------------------------------------------------------------
-// Function    :  Flag_UserCriteria
+// Function    :  Flag_KelvinHelmholtzInstability
 // Description :  Check if the element (i,j,k) of the input data satisfies the user-defined flag criteria
 //
 // Note        :  Users can put their favorite flag criteria in this function
@@ -21,18 +23,18 @@ extern bool KH_AllRankSame;
 // Return      :  "true"  if the flag criteria are satisfied
 //                "false" if the flag criteria are not satisfied
 //-------------------------------------------------------------------------------------------------------
-bool Flag_UserCriteria( const int i, const int j, const int k, const int lv, const int PID, const double Threshold )
+bool Flag_KelvinHelmholtzInstability( const int i, const int j, const int k, const int lv, const int PID, const double Threshold )
 {
 
    const double dh          = amr->dh[lv];
-   const double dz_periodic = ( KH_AllRankSame ) ? amr->BoxSize[2] / MPI_NRank_X[2] : amr->BoxSize[2];
+   const double dz_periodic = ( KH_AllRankSame ) ? amr->BoxSize[2] / KH_PeriodicZFactor / MPI_NRank_X[2]
+                                                 : amr->BoxSize[2] / KH_PeriodicZFactor;
    const double z_shear     = 0.5*dz_periodic;
    const double z           = amr->patch[0][lv][PID]->EdgeL[2] + (k+0.5)*dh;
    const double z_periodic  = fmod( z, dz_periodic );
 
-// flag all cells below level 2 and adjacent to the shear plane
-   const int MaxLv = 2;
-   return (  lv < MaxLv  &&  ( fabs(z_periodic-z_shear)<=dh || z_periodic<=dh || z_periodic>=dz_periodic-dh )  );
+// flag all cells below level KH_RefineShearMaxLv and adjacent to the shear plane
+   return (  lv < KH_RefineShearMaxLv  &&  ( fabs(z_periodic-z_shear)<=dh || z_periodic<=dh || z_periodic>=dz_periodic-dh )  );
 
-} // FUNCTION : Flag_UserCriteria
+} // FUNCTION : Flag_KelvinHelmholtzInstability
 
