0cc06c97dbac522355b36258530b04587730d848
Hsi-Yu Schive <hyschive@gmail.com>
2017-12-13 16:05:41 -0600

    Do not print message in Init_ExternalAcc_Bondi()

--- src/TestProblem/Hydro/Bondi/Init_ExternalAcc_Bondi.cpp
+++ src/TestProblem/Hydro/Bondi/Init_ExternalAcc_Bondi.cpp
@@ -24,18 +24,12 @@ extern double Bondi_Soften_R;
 void Init_ExternalAcc_Bondi()
 {
 
-   if ( MPI_Rank == 0 )    Aux_Message( stdout, "%s ...\n", __FUNCTION__ );
-
-
 // ExtAcc_AuxArray has the size of EXT_ACC_NAUX_MAX defined in CUPOT.h (default = 10)
    ExtAcc_AuxArray[0] = 0.5*amr->BoxSize[0];
    ExtAcc_AuxArray[1] = 0.5*amr->BoxSize[1];
    ExtAcc_AuxArray[2] = 0.5*amr->BoxSize[2];
    ExtAcc_AuxArray[3] = NEWTON_G*Bondi_MassBH;
    ExtAcc_AuxArray[4] = Bondi_Soften_R;
-
-
-   if ( MPI_Rank == 0 )    Aux_Message( stdout, "%s ... done\n", __FUNCTION__ );
 
 } // FUNCTION : Init_ExternalAcc_Bondi
 
