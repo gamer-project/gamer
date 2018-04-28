40ac8ffa9695b90273d1f83807c6635cc3c49a54
Hsi-Yu Schive <hyschive@gmail.com>
2018-04-28 17:20:01 +0800

    Rename Hydro_BoundaryCondition_Outflow.cpp as Flu_BoundaryCondition_Outflow.cpp to work for multiple models
    Former-commit-id: 19162aca0f3c18ec8a31716c0a94f08fb515d30e

--- src/LoadBalance/LB_Refine_GetNewRealPatchList.cpp
+++ src/LoadBalance/LB_Refine_GetNewRealPatchList.cpp
@@ -522,7 +522,7 @@ void PrepareCData( const int FaLv, const int FaPID, real *const FaData,
          switch ( OPT__BC_FLU[ BC_Face[BC_Sibling] ] )
          {
             case BC_FLU_OUTFLOW:
-               Hydro_BoundaryCondition_Outflow   ( FaData_Flu, BC_Face[BC_Sibling], NCOMP_TOTAL, FaGhost_Flu,
+               Flu_BoundaryCondition_Outflow     ( FaData_Flu, BC_Face[BC_Sibling], NCOMP_TOTAL, FaGhost_Flu,
                                                    FaSize_Flu, FaSize_Flu, FaSize_Flu, BC_Idx_Start, BC_Idx_End );
             break;
 
