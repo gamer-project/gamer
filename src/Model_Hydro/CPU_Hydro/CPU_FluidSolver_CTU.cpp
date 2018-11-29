e7bb43ceaba34fb716ac9793075a5408cbb7bc32
Hsi-Yu Schive <hyschive@gmail.com>
2018-11-29 16:42:47 +0800

    Revise CPU_Shared_FullStepUpdate.cpp

--- src/Model_Hydro/CPU_Hydro/CPU_FluidSolver_CTU.cpp
+++ src/Model_Hydro/CPU_Hydro/CPU_FluidSolver_CTU.cpp
@@ -20,8 +20,8 @@ extern void CPU_ComputeFlux( const real FC_Var [][6][ N_FC_VAR*N_FC_VAR*N_FC_VAR
                              const OptGravityType_t GravityType, const double ExtAcc_AuxArray[], const real MinPres,
                              const bool DumpFlux, real IntFlux[][NCOMP_TOTAL][ PS2*PS2 ] );;
 extern void CPU_FullStepUpdate( const real Input[][ FLU_NXT*FLU_NXT*FLU_NXT ], real Output[][ PS2*PS2*PS2 ], char DE_Status[],
-                                const real Flux[][3][NCOMP_TOTAL], const real dt, const real dh,
-                                const real Gamma, const real MinDens, const real MinPres, const real DualEnergySwitch,
+                                const real Flux[][NCOMP_TOTAL][ N_FC_FLUX*N_FC_FLUX*N_FC_FLUX ], const real dt, const real dh,
+                                const real Gamma_m1, const real _Gamma_m1, const real MinDens, const real MinPres, const real DualEnergySwitch,
                                 const bool NormPassive, const int NNorm, const int NormIdx[] );
 extern void CPU_StoreFlux( real Flux_Array[][NCOMP_TOTAL][ PS2*PS2 ], const real FC_Flux[][3][NCOMP_TOTAL] );
 extern real CPU_CheckMinPres( const real InPres, const real MinPres );
@@ -176,7 +176,7 @@ void CPU_FluidSolver_CTU( const real Flu_Array_In[][NCOMP_TOTAL][ FLU_NXT*FLU_NX
 
 //       7. full-step evolution
          CPU_FullStepUpdate( Flu_Array_In[P], Flu_Array_Out[P], DE_Array_Out[P],
-                             FC_Flux, dt, dh, Gamma, MinDens, MinPres, DualEnergySwitch,
+                             FC_Flux, dt, dh, Gamma_m1, _Gamma_m1, MinDens, MinPres, DualEnergySwitch,
                              NormPassive, NNorm, NormIdx );
 
 
