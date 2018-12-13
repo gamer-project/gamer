ca2af02a4ec16a57a55cb124f80a624225c970fd
Hsi-Yu Schive <hyschive@gmail.com>
2018-12-13 10:55:25 +0800

    Remove the extern function specifier

--- src/Model_Hydro/CPU_Hydro/CPU_Shared_RiemannSolver_Roe.cpp
+++ src/Model_Hydro/CPU_Hydro/CPU_Shared_RiemannSolver_Roe.cpp
@@ -24,22 +24,22 @@
 
 #else // #ifdef __CUDACC__
 
-extern void Hydro_Rotate3D( real InOut[], const int XYZ, const bool Forward );
-extern void Hydro_Con2Flux( const int XYZ, real Flux[], const real Input[], const real Gamma_m1, const real MinPres );
+void Hydro_Rotate3D( real InOut[], const int XYZ, const bool Forward );
+void Hydro_Con2Flux( const int XYZ, real Flux[], const real Input[], const real Gamma_m1, const real MinPres );
 #if   ( CHECK_INTERMEDIATE == EXACT )
-extern void Hydro_Con2Pri( const real In[], real Out[], const real Gamma_m1, const real MinPres,
-                           const bool NormPassive, const int NNorm, const int NormIdx[],
-                           const bool JeansMinPres, const real JeansMinPres_Coeff );
-extern void Hydro_RiemannSolver_Exact( const int XYZ, real eival_out[], real L_star_out[], real R_star_out[],
-                                       real Flux_Out[], const real L_In[], const real R_In[], const real Gamma );
+void Hydro_Con2Pri( const real In[], real Out[], const real Gamma_m1, const real MinPres,
+                    const bool NormPassive, const int NNorm, const int NormIdx[],
+                    const bool JeansMinPres, const real JeansMinPres_Coeff );
+void Hydro_RiemannSolver_Exact( const int XYZ, real eival_out[], real L_star_out[], real R_star_out[],
+                                real Flux_Out[], const real L_In[], const real R_In[], const real Gamma );
 #elif ( CHECK_INTERMEDIATE == HLLE )
-extern void Hydro_RiemannSolver_HLLE( const int XYZ, real Flux_Out[], const real L_In[], const real R_In[],
-                                      const real Gamma, const real MinPres );
+void Hydro_RiemannSolver_HLLE( const int XYZ, real Flux_Out[], const real L_In[], const real R_In[],
+                               const real Gamma, const real MinPres );
 #elif ( CHECK_INTERMEDIATE == HLLC )
-extern void Hydro_RiemannSolver_HLLC( const int XYZ, real Flux_Out[], const real L_In[], const real R_In[],
-                                      const real Gamma, const real MinPres );
+void Hydro_RiemannSolver_HLLC( const int XYZ, real Flux_Out[], const real L_In[], const real R_In[],
+                               const real Gamma, const real MinPres );
 #endif
-extern real Hydro_CheckMinPres( const real InPres, const real MinPres );
+real Hydro_CheckMinPres( const real InPres, const real MinPres );
 
 #endif // #ifdef __CUDACC__ ... else ...
 
