3fa7800e6e15523d68b4916343ea8728a46718f3
Hsi-Yu Schive <hyschive@gmail.com>
2018-12-19 20:40:27 +0800

    Move external acceleration and potential routines to CPU_Gravity/ and CPU_Poisson/
    Former-commit-id: 92b2c9b45d429f3512651a922e0e24084a3df5a2

--- src/SelfGravity/CPU_Gravity/CPU_ExternalAcc.cpp
+++ src/SelfGravity/CPU_Gravity/CPU_ExternalAcc.cpp
@@ -1,1 +1,81 @@
-../GPU_Gravity/CUPOT_ExternalAcc.cu
+#ifndef __EXTERNALACC__
+#define __EXTERNALACC__
+
+
+
+#include "CUPOT.h"
+
+#ifdef GRAVITY
+
+
+
+// soften length implementation
+#  define SOFTEN_PLUMMER
+//#  define SOFTEN_RUFFERT
+
+
+
+
+//-----------------------------------------------------------------------------------------
+// Function    :  ExternalAcc
+// Description :  Calculate the external acceleration at the given coordinates and time
+//
+// Note        :  1. This function is shared by CPU and GPU
+//                3. Axiliary array UserArray[] is set by "Init_ExternalAcc_Ptr", which
+//                   points to Init_ExternalAcc() by default but may be overwritten by various
+//                   test problem initializers
+//                4. By default we assume
+//                     UserArray[0] = x coordinate of the external acceleration center
+//                     UserArray[1] = y ...
+//                     UserArray[2] = z ..
+//                     UserArray[3] = gravitational_constant*point_source_mass
+//                     UserArray[4] = soften_length (<=0.0 --> disable)
+//                   --> But one can easily modify this file to change the default behavior
+//                5. Two different soften length implementations are supported
+//                   --> SOFTEN_PLUMMER & SOFTEN_RUFFERT
+//
+// Parameter   :  Acc       : Array to store the output external acceleration
+//                x/y/z     : Target spatial coordinates
+//                Time      : Current physical time
+//                UserArray : User-provided auxiliary array (set by "Init_ExternalAcc_Ptr")
+//
+// Return      :  Acc
+//-----------------------------------------------------------------------------------------
+GPU_DEVICE
+void ExternalAcc( real Acc[], const double x, const double y, const double z, const double Time, const double UserArray[] )
+{
+
+   const double Cen[3] = { UserArray[0], UserArray[1], UserArray[2] };
+   const real GM       = (real)UserArray[3];
+   const real eps      = (real)UserArray[4];
+   const real dx       = (real)(x - Cen[0]);
+   const real dy       = (real)(y - Cen[1]);
+   const real dz       = (real)(z - Cen[2]);
+   const real r        = SQRT( dx*dx + dy*dy + dz*dz );
+
+// Plummer
+#  if   ( defined SOFTEN_PLUMMER )
+   const real _r3 = ( eps <= (real)0.0 ) ? (real)1.0/CUBE(r) : POW( SQR(r)+SQR(eps), (real)-1.5 );
+
+// Ruffert 1994
+#  elif ( defined SOFTEN_RUFFERT )
+   const real tmp = EXP( -SQR(r)/SQR(eps) );
+   const real _r3 = ( eps <= (real)0.0 ) ? (real)1.0/CUBE(r) : POW( SQR(r)+SQR(eps)*tmp, (real)-1.5 )*( (real)1.0 - tmp );
+
+#  else
+   const real _r3 = (real)1.0/CUBE(r);
+#  endif
+
+   Acc[0] = -GM*_r3*dx;
+   Acc[1] = -GM*_r3*dy;
+   Acc[2] = -GM*_r3*dz;
+
+} // FUNCTION : ExternalAcc
+
+
+
+#endif // #ifdef GRAVITY
+
+
+
+#endif // #ifndef __EXTERNALACC__
