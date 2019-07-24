18ca83068ba98bccad3afd576064edd9bdf60880
Tseng, Po-Hsun <zengbs@gmail.com>
2019-07-24 17:13:02 +0800

    set DT_SPEED_OF_LIGHT as global variabl (Main.cpp)

--- src/Main/Main.cpp
+++ src/Main/Main.cpp
@@ -107,6 +107,7 @@ OptRSolver1st_t      OPT__1ST_FLUX_CORR_SCHEME;
 bool                 OPT__FLAG_PRES_GRADIENT, OPT__FLAG_LOHNER_ENGY, OPT__FLAG_LOHNER_PRES, OPT__FLAG_LOHNER_TEMP;
 bool                 OPT__FLAG_VORTICITY, OPT__FLAG_JEANS, JEANS_MIN_PRES;
 int                  OPT__CK_NEGATIVE, JEANS_MIN_PRES_LEVEL, JEANS_MIN_PRES_NCELL;
+int                  DT_SPEED_OF_LIGHT;
 double               MIN_DENS, MIN_TEMP, MIN_PRES;
 #elif ( MODEL == ELBDM )
 double               DT__PHASE, FlagTable_EngyDensity[NLEVEL-1][2];
