f5a3c61e11c136dd4f548344cfff8c8b38ffb05d
Tseng, Po-Hsun <zengbs@gmail.com>
2019-08-02 09:22:19 +0800

    set Lorentz factor as refinement criterion

--- src/TestProblem/SRHydro/Jet/Init_TestProb_SRHydro_Jets.cpp
+++ src/TestProblem/SRHydro/Jet/Init_TestProb_SRHydro_Jets.cpp
@@ -419,6 +419,65 @@ bool Flu_ResetByUser_Jets( real fluid[], const double x, const double y, const d
 } // FUNCTION : Flu_ResetByUser_Jets
 #endif // #if ( MODEL == SR_HYDRO )
 
+bool Flag_User( const int i, const int j, const int k, const int lv, const int PID, const double Threshold )
+{
+
+   const double dh     = amr->dh[lv];                                                  // grid size
+   const double Pos[3] = { amr->patch[0][lv][PID]->EdgeL[0] + (i+0.5)*dh,              // x,y,z position
+                           amr->patch[0][lv][PID]->EdgeL[1] + (j+0.5)*dh,
+                           amr->patch[0][lv][PID]->EdgeL[2] + (k+0.5)*dh  };
+
+   const real (*Dens)[PS1][PS1] = amr->patch[ amr->FluSg[lv] ][lv][PID]->fluid[DENS];  // density
+   const real (*MomX)[PS1][PS1] = amr->patch[ amr->FluSg[lv] ][lv][PID]->fluid[MOMX];  // momentum x
+   const real (*MomY)[PS1][PS1] = amr->patch[ amr->FluSg[lv] ][lv][PID]->fluid[MOMY];  // momentum y
+   const real (*MomZ)[PS1][PS1] = amr->patch[ amr->FluSg[lv] ][lv][PID]->fluid[MOMZ];  // momentum z
+   const real (*Engy)[PS1][PS1] = amr->patch[ amr->FluSg[lv] ][lv][PID]->fluid[ENGY];  // total energy
+#  ifdef GRAVITY
+   const real (*Pot )[PS1][PS1] = amr->patch[ amr->PotSg[lv] ][lv][PID]->pot;          // potential
+#  endif
+
+   bool Flag = false;
+
+
+// put your favorite flag criteria here
+// ##########################################################################################################
+
+   /*
+// Example 1 : flag if the velocity exceeds the given threshold
+   const real Vel = sqrt(  ( MomX[k][j][i]*MomX[k][j][i] + MomY[k][j][i]*MomY[k][j][i] +
+                             MomZ[k][j][i]*MomZ[k][j][i] )  ) / Dens[k][j][i];
+   Flag = Vel > Threshold;
+   */
+
+
+   /*
+// Example 2 : flag if the grid is within a sphere with the radius eqaul to the input "Threshold" and the origin
+//             in the center of the simulation box
+   const double Center[3] = { 0.5*amr->BoxSize[0], 0.5*amr->BoxSize[1], 0.5*amr->BoxSize[2] };
+   const double dr[3]     = { Pos[0]-Center[0], Pos[1]-Center[1], Pos[2]-Center[2] };
+   const double Radius    = sqrt( dr[0]*dr[0] + dr[1]*dr[1] + dr[2]*dr[2] );
+
+   Flag = Radius < Threshold;
+   */
+// ##########################################################################################################
+
+   real Cons[NCOMP_FLUID], Prim[NCOMP_FLUID], LorentzFactor;
+
+  
+   Cons[DENS] = Dens[k][j][i];
+   Cons[MOMX] = MomX[k][j][i];
+   Cons[MOMY] = MomY[k][j][i];
+   Cons[MOMZ] = MomZ[k][j][i];
+   Cons[ENGY] = Engy[k][j][i];
+
+   LorentzFactor = SRHydro_Con2Pri( Cons, Prim, GAMMA, MIN_TEMP );
+
+   Flag = LorentzFactor >= Threshold;
+
+ 
+   return Flag;
+
+} // FUNCTION : Flag_User
 
 
 //-------------------------------------------------------------------------------------------------------
@@ -447,7 +506,7 @@ void Init_TestProb_SRHydro_Jets()
 
 
    Init_Function_User_Ptr   = SetGridIC;
-   Flag_User_Ptr            = NULL;
+   Flag_User_Ptr            = Flag_User;
    Mis_GetTimeStep_User_Ptr = NULL;
    BC_User_Ptr              = NULL;
    Flu_ResetByUser_Func_Ptr = Flu_ResetByUser_Jets;
