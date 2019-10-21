afafe7e2c72f315a5bf88f85ebce4e7bc32d4852
Hsi-Yu Schive <hyschive@gmail.com>
2019-10-21 21:51:52 +0800

    Filter data in get_center_of_mass.py
    Former-commit-id: 08aa9cb4fd17fd0d98c42246ff30862dc65899b7

--- example/yt/get_center_of_mass.py
+++ example/yt/get_center_of_mass.py
@@ -7,8 +7,12 @@ file = 'Data_000000'
 ds = yt.load( file )
 ad = ds.all_data()
 
+# filter data if required
+# ref: https://yt-project.org/docs/dev/analyzing/filtering.html#cut-regions
+dense = ad.cut_region( ['obj["Dens"]>1.0e2'] )
+
 # calculate center-of-mass
-cm = ad.quantities.weighted_average_quantity( ['x','y','z'], weight='cell_mass' )
+cm = dense.quantities.weighted_average_quantity( ['x','y','z'], weight='cell_mass' )
 
 print( 'CM = (%13.7e, %13.7e, %13.7e) in code units' % (cm[0].in_units('code_length'),
                                                         cm[1].in_units('code_length'),
