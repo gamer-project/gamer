8e107c1bac5724fda56f4c9f0ee5e5a5ce24b6f2
Hsi-Yu Schive <hyschive@gmail.com>
2019-10-21 20:05:45 +0800

    Add the yt example "get_center_of_mass.py"

---
+++ example/yt/get_center_of_mass.py
@@ -0,0 +1,15 @@
+import yt
+
+# parameters
+file = 'Data_000000'
+
+# load data
+ds = yt.load( file )
+ad = ds.all_data()
+
+# calculate center-of-mass
+cm = ad.quantities.weighted_average_quantity( ['x','y','z'], weight='cell_mass' )
+
+print( 'CM = (%13.7e, %13.7e, %13.7e) in code units' % (cm[0].in_units('code_length'),
+                                                        cm[1].in_units('code_length'),
+                                                        cm[2].in_units('code_length')) )
