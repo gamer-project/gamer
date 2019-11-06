67a8bd1a9f6b9d7a68441469a2ec76dc9da0cd7e
Tseng, Po-Hsun <zengbs@gmail.com>
2019-11-06 09:06:48 +0800

    relative error obtained by recover scheme is still under machine epsilon in non-relativistic regime

--- src/Model_SRHydro/CPU_SRHydro/CPU_Shared_FluUtility.cpp
+++ src/Model_SRHydro/CPU_SRHydro/CPU_Shared_FluUtility.cpp
@@ -4,18 +4,21 @@
 #include "CUFLU.h"
 #include <stdio.h>
 
-struct Fun_params
+struct Arguments
 {
-  real M_Dsqr;
+  real M_D;
   real E_D;
 };
+
+
 
 GPU_DEVICE 
 real VectorDotProduct( real V1, real V2, real V3 );
 GPU_DEVICE 
-static void Fun_DFun (real Q, void *ptr, real * f, real * df, real Gamma);
+void SRHydro_HTilde_Function (real HTilde, real M_Dsqr, real Constant, real *Fun, real *DiffFun, real Gamma);
 GPU_DEVICE 
-static void NewtonRaphsonSolver(void *ptr, real *root, const real guess, const real epsabs, const real epsrel, const real Gamma);
+static
+void NewtonRaphsonSolver(void (*FunPtr)(real, real, real, real*, real*, real), real M_D, real E_D, real *root, const real guess, const real epsabs, const real epsrel, const real Gamma);
 
 //-------------------------------------------------------------------------------------------------------
 // Function    :  SRHydro_Rotate3D
@@ -63,6 +66,61 @@ void SRHydro_Rotate3D (real InOut[], const int XYZ, const bool Forward)
     }
 }				// FUNCTION : SRHydro_Rotate3D
 
+GPU_DEVICE
+void SRHydro_HTilde2Temperature (const real HTilde, real *Temp, real *DiffTemp )
+{
+
+   real Factor0 = (real)2.0*SQR(HTilde) + (real)4.0*HTilde;
+   real Factor1 = SQRT( (real)9.0*SQR(HTilde) + (real)18.0*HTilde + (real)25.0 );
+   real Factor2 = (real)5.0*HTilde + (real)5.0 + Factor1;
+
+
+   if ( Temp != NULL )
+   {
+     *Temp = Factor0 / Factor2;
+   }
+
+   if ( DiffTemp != NULL )
+   {
+     *DiffTemp = ( (real)4.0*HTilde + (real)4.0 ) / Factor2
+			   -  Factor0 * ( ( (real)9.0*HTilde + (real)9.0 ) /  Factor1 + (real)5.0 ) / SQR(Factor2);
+   }
+
+}// FUNCTION : SRHydro_GetTemperature
+
+GPU_DEVICE
+real SRHydro_GetHTilde( const real Con[], real Gamma )
+{
+  real HTilde, guess;
+
+  real Msqr = VectorDotProduct(Con[1], Con[2], Con[3]);
+  real Dsqr = SQR(Con[0]);
+  real abc = (real)1.0 / Dsqr;
+  real E_D = Con[4] / Con[0];
+  real E_Dsqr = abc * SQR(Con[4]);
+  real M_Dsqr = abc * Msqr;
+  real M_D = SQRT( M_Dsqr );
+  
+# if ( EOS == APPROXIMATED_GENERAL )
+# if   ( CONSERVED_ENERGY == 1 )
+# elif ( CONSERVED_ENERGY == 2 )
+  real Constant = E_Dsqr - M_Dsqr + (real)2.0 * E_D;
+
+  guess = (real)5.0*Constant/(real)6.0;
+# else
+# error: CONSERVED_ENERGY must be 1 or 2!
+# endif
+# elif ( EOS == CONSTANT_GAMMA )
+# endif
+ 
+  void (*FunPtr)( real HTilde, real M_Dsqr, real Constant, real *Fun, real *DiffFun, real Gamma ) = &SRHydro_HTilde_Function;
+
+  NewtonRaphsonSolver(FunPtr, M_Dsqr, -Constant, &HTilde, guess, (real) TINY_NUMBER, (real) EPSILON, Gamma);
+  
+  return HTilde;
+}
+
+
 
 //-------------------------------------------------------------------------------------------------------
 // Function    :  SRHydro_GetTemperature
@@ -80,114 +138,35 @@ void SRHydro_Rotate3D (real InOut[], const int XYZ, const bool Forward)
 // Return      :  kB*T/mc**2
 //-------------------------------------------------------------------------------------------------------
 GPU_DEVICE
-real SRHydro_GetTemperature (const real Dens, const real MomX, const real MomY, const real MomZ, const real Engy,
+real SRHydro_GetTemperature( const real Dens, const real MomX, const real MomY, const real MomZ, const real Engy,
                              const real Gamma, const real MinTemp )
 {
-  real In[5] = {Dens, MomX, MomY, MomZ, Engy};
+   real Temp, Cons[NCOMP_FLUID], HTilde;
+   
+   Cons[DENS] = Dens;
+   Cons[MOMX] = MomX;
+   Cons[MOMY] = MomY;
+   Cons[MOMZ] = MomZ;
+   Cons[ENGY] = Engy;
 
-  real guess, root;
-  real Msqr = VectorDotProduct(In[1], In[2], In[3]);
-  real M = SQRT (Msqr); // magnitude of momentum
-  real Dsqr = SQR(In[0]);
-  real abc = (real)1.0 / Dsqr;
-  real E_D = In[4] / In[0];
-  real E_Dsqr = abc * SQR(In[4]);
-  real M_Dsqr = abc * Msqr;
+   HTilde = SRHydro_GetHTilde( Cons, Gamma );
 
-# if ( EOS == APPROXIMATED_GENERAL )
-# if   ( CONSERVED_ENERGY == 1 )
-/* initial guess  */
-  real Constant = E_Dsqr - M_Dsqr;
-  if ( Constant > (real)1.0 )
-  {
-   if ( Dsqr - (real)0.0625 * Msqr >= (real)0 )
-   {
-     if ( Constant > (real)2.5 ) 
-          guess = SQRT( FMA( (real)1.0, E_Dsqr, - FMA ( (real)0.93750, M_Dsqr, (real)2.0 ) ) ) / (real)3.0;
-     else guess = (Constant - (real)1.0) / (real)3.0;
-   }
-   else // 1 - (M/D)**2 < 0
-   {
-     if ( Constant >  (real)1.5 + SQRT( FMA ((real)0.0625, M_Dsqr, (real) -0.75 ))) 
-          guess = SQRT( FMA( (real)1.0, E_Dsqr, - FMA ( (real)0.93750, M_Dsqr, (real)2.0 ) ) ) / (real)3.0;
-     else guess = (Constant - (real)1.0) / (real)3.0;
-   }
-  } else return MinTemp;
-# elif ( CONSERVED_ENERGY == 2 )
-/* initial guess  */
-  real Constant = E_Dsqr - M_Dsqr + (real)2.0 * E_D;
-  if ( Constant > (real)0.0 )
-  {
-      if ( Dsqr - (real)0.0625 * Msqr >= (real)0 )
-      {
-        if ( Constant > (real)1.5 ) 
-             guess = SQRT( E_Dsqr + (real)2.0 * E_D - (real)0.93750 * M_Dsqr - (real)1.0 ) / (real)3.0;
-        else guess = Constant / (real)3.0;
-      }
-      else // 1 - (M/D)**2 < 0
-      {
-        if ( Constant >  (real)0.5 + SQRT( (real)0.0625 * M_Dsqr - (real)0.75 )) 
-             guess = SQRT( E_Dsqr + (real)2.0 * E_D - (real)0.93750 * M_Dsqr - (real)1.0 ) / (real)3.0;
-        else guess = Constant / (real)3.0;
-      }
-  } else return MinTemp;
-# else
-# error: CONSERVED_ENERGY must be 1 or 2!
-# endif
-# elif ( EOS == CONSTANT_GAMMA )
-  real Gamma_m1 = Gamma - (real) 1.0;
-/* initial guess */
-# if   ( CONSERVED_ENERGY == 1 )
-  real Constant = E_Dsqr - M_Dsqr;
+   SRHydro_HTilde2Temperature( HTilde, &Temp, NULL ); 
 
-  if ( Constant > (real)1.0 )
-  {
-     if ( Constant > (real)1.0 + (real)2* (Gamma_m1 / Gamma ) * (M / In[0]) )
-     {
-       real A = (real)1.0 / SQR(Gamma_m1);
-       real B = (real)2.0 /Gamma_m1;
-       real C = (((real)2*Gamma-(real)1.0)/(Gamma*Gamma)) * M_Dsqr - E_Dsqr;
-       real delta = SQRT( B * B - (real)4 * A * C );
-       guess = -(real)2.0 *  C / ( B + delta);
-     }
-     else guess = (real)0.5*Gamma_m1 * ( Constant - (real)1.0);
-  } else return MinTemp;
-# elif ( CONSERVED_ENERGY == 2 )
-  real Constant = E_Dsqr - M_Dsqr + (real)2 * E_D;
+   return Temp;
+}
 
-  if ( Constant > (real)0.0 )
-  {
-    if ( Constant > (real)2* (Gamma_m1 / Gamma ) * (M / In[0]) )
-    {
-      real A = (real)1.0 / SQR(Gamma_m1);
-      real B = (real)2.0 /Gamma_m1;
-      real C = (((real)2*Gamma-(real)1.0)/(Gamma*Gamma)) * M_Dsqr - E_Dsqr - (real)2 * E_D;
-      real delta = SQRT( B * B - (real)4 * A * C );
-      guess = -(real)2.0 *  C / ( B + delta);
-    }
-    else guess = (real)0.5*Gamma_m1 * Constant;
-  } else return MinTemp;
-# else
-# error: CONSERVED_ENERGY must be 1 or 2!
-# endif
-# else
-# error: unsupported EoS!
-# endif
 
-     struct Fun_params params = { M_Dsqr, E_D };
 
-# ifdef CHECK_FAILED_CELL_IN_FLUID
-  if ( guess != guess || guess >= HUGE_NUMBER || guess <= TINY_NUMBER )
-  { 
-    printf ("guess root = %14.7e\n", guess);
-    printf ("D=%14.7e, Mx=%14.7e, My=%14.7e, Mz=%14.7e, E=%14.7e\n", In[0], In[1], In[2], In[3], In[4]);
-  }
-# endif 
+GPU_DEVICE
+real SRHydro_Temperature2HTilde (const real Temperature )
+{
+    real HTilde;
 
-  NewtonRaphsonSolver(&params ,&root, guess, (real) TINY_NUMBER, (real) EPSILON, Gamma);
-  //root = Constant / (real)3.0;
-  return root;
-}				// FUNCTION : SRHydro_GetTemperature
+	HTilde = (real)2.5*Temperature + 2.25*SQR(Temperature)/(SQRT( (real)2.25*SQR(Temperature) + (real)1.0 ) + (real)1.0);
+
+	return HTilde;
+}
 
 //-------------------------------------------------------------------------------------------------------
 // Function    :  SpecificEnthalpy
@@ -203,46 +182,7 @@ real SRHydro_GetTemperature (const real Dens, const real MomX, const real MomY, 
 GPU_DEVICE
 real SpecificEnthalpy( const real Con[], real Temp, real Gamma )
 {
-   real h;
-
-#  if ( EOS == APPROXIMATED_GENERAL )
-   h = FMA( (real)2.5, Temp, SQRT( FMA( (real)2.25, Temp*Temp, (real)1.0 ) ) );
-
-// if there is an overfolw due to ultra-high temperature, we simply set Gamma = 4/3.
-   if ( h != h && Con != NULL) 
-   {    
-		Gamma = (real)1.3333333;
-        real Gamma_m1 = Gamma - (real)1.0;
-
-#       if ( CONSERVED_ENERGY == 1  )
-        real E_Dsqr = SQR( Con[ENGY] / Con[DENS] );
-#       else
-        real E_Dsqr = SQR( ( Con[ENGY] + Con[DENS] ) / Con[DENS] );
-#       endif
-
-        real M_Dsqr = VectorDotProduct(Con[MOMX], Con[MOMY], Con[MOMZ]) / SQR(Con[DENS]);
-
-        real A = (real)1.0 / SQR(Gamma_m1);
-        real B = (real)2.0 /Gamma_m1;
-        real C = (((real)2.0*Gamma-(real)1.0)/(Gamma*Gamma)) * M_Dsqr - E_Dsqr;
-
-        real delta = SQRT( B * B - (real)4.0 * A * C );
-
-        Temp = -(real)2.0 *  C / ( B + delta);
-
-        h = (real)4.0 * Temp;
-   }
-   else if ( h!= h && Con == NULL )
-   {
-        printf("Conservative variables should be provided! function: %s: %d\n", __FUNCTION__, __LINE__);
-   }
-
-#  elif ( EOS == CONSTANT_GAMMA ) 
-   h = (real)1.0 + Temp * Gamma / (Gamma-(real)1.0);
-#  else
-#  error: unsupported EoS!
-#  endif
-
+   real h = 0.0;
    return h;
 }
 
@@ -260,45 +200,64 @@ real SpecificEnthalpy( const real Con[], real Temp, real Gamma )
 GPU_DEVICE
 real SRHydro_Con2Pri (const real In[], real Out[], const real Gamma, const real MinTemp)
 {
-   real Temp = SRHydro_GetTemperature (In[DENS], In[MOMX], In[MOMY], In[MOMZ], In[ENGY], Gamma, MinTemp );
+   real LorentzFactor, HTilde, Temperature, Factor0;
 
-   real h = SpecificEnthalpy(In, Temp, Gamma);
+   HTilde = SRHydro_GetHTilde( In, Gamma );
 
-   real factor = In[0]*h;
-   Out[1] = In[1]/factor;
-   Out[2] = In[2]/factor;
-   Out[3] = In[3]/factor;
+   SRHydro_HTilde2Temperature( HTilde, &Temperature, NULL );
 
-   real Lorentz = SQRT((real)1.0 + VectorDotProduct(Out[1], Out[2], Out[3]));
+   Factor0 = In[DENS] * HTilde + In[DENS];
 
-   Out[0] = In[0]/Lorentz;
-   Out[4] = Out[0] * Temp; // P = rho*kB*T*c**2
+   Out[1] = In[MOMX] / Factor0;
+   Out[2] = In[MOMY] / Factor0;
+   Out[3] = In[MOMZ] / Factor0;
 
-   return Lorentz;
+
+   LorentzFactor = SQRT( (real)1.0 + SQR( Out[1] )+ SQR( Out[2] )+ SQR( Out[3] ) );
+
+   Out[0] = In[DENS] / LorentzFactor;
+
+   Out[4] = Temperature*In[DENS] / LorentzFactor;
+
+   return LorentzFactor;
 }// FUNCTION : SRHydro_Con2Pri
 
 
 GPU_DEVICE
 void SRHydro_Pri2Con (const real In[], real Out[], const real Gamma)
 {
-  real h = SpecificEnthalpy( NULL, In[4]/In[0], Gamma );
-  real nh   = In[0] * h;
+  real LorentzFactor, Temperature, HTilde, H;
+  real Factor0, A, B, C;
 
-  real Factor0 = (real)1.0 + VectorDotProduct(In[1], In[2], In[3]);
-  real Factor1 = SQRT(Factor0); // Lorentz factor
-  
-  Out[0] = In[0] * Factor1; // mass density in inertial frame
-  Out[1] = Out[0]*h*In[1]; // MomX
-  Out[2] = Out[0]*h*In[2]; // MomX
-  Out[3] = Out[0]*h*In[3]; // MomX
-# if   ( CONSERVED_ENERGY == 1 )
-  Out[4] = FMA( nh, Factor0, - In[4] ); // total_energy
-# elif ( CONSERVED_ENERGY == 2 )
-  Out[4] = nh * Factor0 - In[4] - Out[0]; // ( total_energy ) - ( rest_mass_energy )
-# else
-# error: CONSERVED_ENERGY must be 1 or 2!
-# endif
+  LorentzFactor = SQRT( (real)1.0 + SQR(In[1]) + SQR(In[2]) + SQR(In[3]) );
+
+  Temperature = In[4]/In[0];
+
+  HTilde = SRHydro_Temperature2HTilde( Temperature );
+
+  Out[DENS] = In[0] * LorentzFactor;
+  Factor0 = Out[DENS]*H;
+
+
+  Out[MOMX] = Factor0*In[1];
+  Out[MOMY] = Factor0*In[2];
+  Out[MOMZ] = Factor0*In[3];
+
+
+  real Msqr = VectorDotProduct(Out[MOMX], Out[MOMY], Out[MOMZ]);
+  real M_Dsqr = Msqr / SQR( Out[DENS] );
+  real Constant = M_Dsqr;
+  real Fun;
+
+
+  SRHydro_HTilde_Function( HTilde, M_Dsqr, Constant, &Fun, NULL, Gamma );
+
+  Out[ENGY] = Fun / ( SQRT((real)1.0 + Fun ) + (real)1.0 );
+  Out[ENGY] *= Out[DENS];
+
+
 }				// FUNCTION : SRHydro_Pri2Con
+
 
 //-------------------------------------------------------------------------------------------------------
 // Function    :  SRHydro_4Velto3Vel
@@ -732,12 +691,12 @@ real SRHydro_PoissonSource( real Con[], real Gamma, real MinTemp )
 
 GPU_DEVICE
 static void 
-NewtonRaphsonSolver(void *ptr, real *root, const real guess, const real epsabs, const real epsrel, const real Gamma)
+NewtonRaphsonSolver(void (*FunPtr)(real, real, real, real*, real*, real), real M_D, real E_D, real *root, const real guess, const real epsabs, const real epsrel, const real Gamma)
 {
  int iter = 0;
  int max_iter = 20;
 
- real f, df;
+ real Fun, DiffFun;
  real delta;
  real tolerance;
  *root = guess;
@@ -745,15 +704,15 @@ NewtonRaphsonSolver(void *ptr, real *root, const real guess, const real epsabs, 
  do
    {
      iter++;
-     Fun_DFun(*root, ptr, &f, &df, Gamma);
+     FunPtr(*root, M_D, E_D, &Fun, &DiffFun, Gamma);
 
 #    ifdef CHECK_FAILED_CELL_IN_FLUID
-     if ( df == (real)0.0 )                                                  printf("derivative is zero\n");
-     if (  f != f  ||(real) -HUGE_NUMBER >= f  || f  >= (real)HUGE_NUMBER )  printf("function value is not finite\n");
-     if ( df != df ||(real) -HUGE_NUMBER >= df || df >= (real)HUGE_NUMBER )  printf("derivative value is not finite\n");
+     if ( DiffFun == (real)0.0 )                                                  printf("derivative is zero\n");
+     if (  Fun != Fun  ||(real) -HUGE_NUMBER >= Fun  || Fun  >= (real)HUGE_NUMBER )  printf("function value is not finite\n");
+     if ( DiffFun != DiffFun ||(real) -HUGE_NUMBER >= DiffFun || DiffFun >= (real)HUGE_NUMBER )  printf("derivative value is not finite\n");
 #    endif     
 
-      delta = f/df;
+      delta = Fun/DiffFun;
       *root = *root - delta;
 
       tolerance =  FMA( epsrel, FABS(*root), epsabs );
@@ -769,48 +728,37 @@ NewtonRaphsonSolver(void *ptr, real *root, const real guess, const real epsabs, 
 //-------------------------------------------------------------------------------------------------------
 
 GPU_DEVICE
-static void
-Fun_DFun (real Temp, void *ptr, real * f, real * df, real Gamma)
+void SRHydro_HTilde_Function (real HTilde, real M_Dsqr, real Constant, real *Fun, real *DiffFun, real Gamma)
 {
-  struct Fun_params *params = (struct Fun_params *) ptr;
+  real Temp, DiffTemp;
 
-  real M_Dsqr = (params->M_Dsqr);
-  real E_D    = (params->E_D);
-  real Tsqr = Temp * Temp;
 
 # if ( EOS == APPROXIMATED_GENERAL )
-  real abc = SQRT(FMA( (real)9.0, Tsqr, (real)4.0 ));
-  real h = FMA( (real)2.5, Temp, SQRT(FMA( (real)2.25, Tsqr, (real)1.0 ))); // approximate enthalpy
-  real dh = FMA( (real)9.0, Temp / SQRT(FMA( (real)36.0, Tsqr, (real)16.0 )), (real)2.5 );
-  real hsqr = SQR(h);
+  SRHydro_HTilde2Temperature ( HTilde, &Temp, &DiffTemp );
+
+
 # if   (CONSERVED_ENERGY == 1)
-  real Constant = FMA( E_D, E_D, - M_Dsqr );
-  *f = (real)3.5 * Tsqr + (real)1.5 * Temp * abc + hsqr * Tsqr / (hsqr + M_Dsqr) + (real)1.0 - Constant;
 # elif (CONSERVED_ENERGY == 2)
-  real Constant = SQR(E_D) + (real)2*(E_D) - M_Dsqr;
-  *f = (real)3.5 * Tsqr + (real)1.5 * Temp * abc + hsqr * Tsqr / (hsqr + M_Dsqr) - Constant;
+  real H =  HTilde + (real)1.0;
+  real Factor0 = SQR( H ) + M_Dsqr;
+
+  if ( Fun != NULL )
+
+  *Fun = SQR( HTilde ) + (real)2.0*HTilde - (real)2.0*Temp - (real)2.0*Temp*HTilde
+		  + SQR( Temp * H ) / Factor0 + Constant;
+
+  if ( DiffFun != NULL )
+
+  *DiffFun = (real)2.0*H - (real)2.0*Temp - (real)2.0*H*DiffTemp +
+		  ( (real)2.0*Temp*DiffTemp*H*H - (real)2.0*Temp*Temp*H ) / SQR( Factor0 );
 # else
 # error: CONSERVED_ENERGY must be 1 or 2!
 # endif
-  *df = FMA( (real)7.0, Temp, FMA( (real)1.5, abc, (real)13.5 * Tsqr / abc )) + (real)2*h*Temp*((h*hsqr + M_Dsqr*h + Temp*dh*M_Dsqr) / SQR( hsqr + M_Dsqr) );
 
 # elif ( EOS == CONSTANT_GAMMA )
-  real zeta = (real)1.0 / ( Gamma - (real)1.0 );
-  real alpha = Gamma * zeta;
-  real h = (real)1 + alpha * Temp;
-  real hsqr = SQR(h);
-  real beta = ((real)2 - Gamma) * zeta * alpha;
-  real theta = (real)2 * zeta;
-
 # if   (CONSERVED_ENERGY == 1)
-  real Constant = SQR(E_D) - M_Dsqr;
-  *f = (real)1.0 + beta * Tsqr + theta * Temp + hsqr * Tsqr / (hsqr + M_Dsqr) - Constant; 
 # elif (CONSERVED_ENERGY == 2)
-  real Constant = SQR(E_D) + (real)2*(E_D) - M_Dsqr;
-  *f = beta * Tsqr + theta * Temp + hsqr * Tsqr / (hsqr + M_Dsqr) - Constant; 
 # endif
-  real dh = alpha;
-  *df = (real)2 * beta * Temp + theta + (real)2*h*Temp*((h*hsqr + M_Dsqr*h + Temp*dh*M_Dsqr) / SQR( hsqr + M_Dsqr) );
 # else
 # error: unsupported EoS!
 # endif // #if ( EOS == APPROXIMATED_GENERAL )
