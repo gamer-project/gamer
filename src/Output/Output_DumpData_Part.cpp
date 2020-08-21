abea222fb52364dc6d8c0ad0daa51ea7aaf76a6e
Hsi-Yu Schive <hyschive@gmail.com>
2020-08-21 13:47:59 +0800

    Do not apply pressure floor in Output_DumpData_Part()

--- src/Output/Output_DumpData_Part.cpp
+++ src/Output/Output_DumpData_Part.cpp
@@ -255,9 +255,9 @@ void WriteFile( FILE *File, const int lv, const int PID, const int i, const int 
 // output other derived fields
 #  if   ( MODEL == HYDRO )
 #  if ( MODEL == HYDRO )
-   const bool CheckMinPres_Yes = true;
+   const bool CheckMinPres_No = false;
    fprintf( File, " %13.6e", Hydro_Con2Pres(u[DENS],u[MOMX],u[MOMY],u[MOMZ],u[ENGY],u+NCOMP_FLUID,
-                                            CheckMinPres_Yes,MIN_PRES,Emag,
+                                            CheckMinPres_No,NULL_REAL,Emag,
                                             EoS_DensEint2Pres_CPUPtr,EoS_AuxArray,NULL) );
 #  endif
 #  endif // MODEL
