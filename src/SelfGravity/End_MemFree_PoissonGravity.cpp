c5e39f34727099dac51f61488a132840745ad03f
Hsi-Yu Schive <hyschive@gmail.com>
2020-09-22 21:50:54 +0800

    Rename d_Corner_Array_G as d_Corner_Array_PGT

--- src/SelfGravity/End_MemFree_PoissonGravity.cpp
+++ src/SelfGravity/End_MemFree_PoissonGravity.cpp
@@ -16,22 +16,22 @@ void End_MemFree_PoissonGravity()
 
    for (int t=0; t<2; t++)
    {
-      delete [] h_Rho_Array_P    [t];  h_Rho_Array_P    [t] = NULL;
-      delete [] h_Pot_Array_P_In [t];  h_Pot_Array_P_In [t] = NULL;
-      delete [] h_Pot_Array_P_Out[t];  h_Pot_Array_P_Out[t] = NULL;
+      delete [] h_Rho_Array_P     [t];  h_Rho_Array_P     [t] = NULL;
+      delete [] h_Pot_Array_P_In  [t];  h_Pot_Array_P_In  [t] = NULL;
+      delete [] h_Pot_Array_P_Out [t];  h_Pot_Array_P_Out [t] = NULL;
 #     ifdef UNSPLIT_GRAVITY
-      delete [] h_Pot_Array_USG_G[t];  h_Pot_Array_USG_G[t] = NULL;
-      delete [] h_Flu_Array_USG_G[t];  h_Flu_Array_USG_G[t] = NULL;
+      delete [] h_Pot_Array_USG_G [t];  h_Pot_Array_USG_G [t] = NULL;
+      delete [] h_Flu_Array_USG_G [t];  h_Flu_Array_USG_G [t] = NULL;
 #     endif
-      delete [] h_Flu_Array_G    [t];  h_Flu_Array_G    [t] = NULL;
-      delete [] h_Corner_Array_G [t];  h_Corner_Array_G [t] = NULL;
+      delete [] h_Flu_Array_G     [t];  h_Flu_Array_G     [t] = NULL;
+      delete [] h_Corner_Array_PGT[t];  h_Corner_Array_PGT[t] = NULL;
 #     ifdef DUAL_ENERGY
-      delete [] h_DE_Array_G     [t];  h_DE_Array_G     [t] = NULL;
+      delete [] h_DE_Array_G      [t];  h_DE_Array_G      [t] = NULL;
 #     endif
 #     ifdef MHD
-      delete [] h_Emag_Array_G   [t];  h_Emag_Array_G   [t] = NULL;
+      delete [] h_Emag_Array_G    [t];  h_Emag_Array_G    [t] = NULL;
 #     endif
-      delete [] h_Pot_Array_T    [t];  h_Pot_Array_T    [t] = NULL;
+      delete [] h_Pot_Array_T     [t];  h_Pot_Array_T     [t] = NULL;
    }
 
    delete [] GreenFuncK;   GreenFuncK = NULL;
