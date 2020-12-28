b5094286e0b7c54f6133394f96c4ca8589191cf5
Hsi-Yu Schive <hyschive@gmail.com>
2020-12-28 16:18:40 +0800

    Raise error if SRC_GHOST_SIZE != 0

--- src/Auxiliary/Aux_Check_Parameter.cpp
+++ src/Auxiliary/Aux_Check_Parameter.cpp
@@ -1347,6 +1347,10 @@ void Aux_Check_Parameter()
 
 // errors
 // ------------------------------
+#  if ( SRC_GHOST_SIZE != 0 )
+#     error : ERROR : SRC_GHOST_SIZE must be zero for now !!
+#  endif
+
 #  if ( MODEL != HYDRO )
    if ( SRC_TERMS.Deleptonization )
       Aux_Error( ERROR_INFO, "SRC_DELEPTONIZATION is only supported in HYDRO !!\n" );
