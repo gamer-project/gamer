b392f0058cd7ec7ec9123cf96ebab5f5e9489b46
Hsi-Yu Schive <hyschive@gmail.com>
2021-01-28 21:40:17 +0800

    Use synchronous transfer in CUAPI_SendExtPotTable2GPU()

--- src/GPU_API/CUAPI_SendExtPotTable2GPU.cu
+++ src/GPU_API/CUAPI_SendExtPotTable2GPU.cu
@@ -15,6 +15,7 @@ extern real *d_ExtPotTable;
 // Description :  Send the external potential table to GPU
 //
 // Note        :  1. Invoked by Init_LoadExtPotTable()
+//                2. Use synchronous transfer
 //
 // Parameter   :  h_Table : Host array storing the input table
 //
@@ -25,7 +26,8 @@ void CUAPI_SendExtPotTable2GPU( const real *h_Table )
 
    const long MemSize = (long)sizeof(real)*EXT_POT_TABLE_NPOINT[0]*EXT_POT_TABLE_NPOINT[1]*EXT_POT_TABLE_NPOINT[2];
 
-   CUDA_CHECK_ERROR(  cudaMemcpyAsync( d_ExtPotTable, h_Table, MemSize, cudaMemcpyHostToDevice )  );
+// use synchronous transfer
+   CUDA_CHECK_ERROR(  cudaMemcpy( d_ExtPotTable, h_Table, MemSize, cudaMemcpyHostToDevice )  );
 
 } // FUNCTION : CUAPI_SendExtPotTable2GPU
 
