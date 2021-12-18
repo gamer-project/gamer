95876d24aa87404b79362b8c64ff5b7423218e9f
Po-Hsun Tseng <zengbs@gmail.com>
2021-12-18 10:30:51 +0800

    update

--- example/test_problem/Hydro/ClusterMerger/gamer_cluster_ics.py
+++ example/test_problem/Hydro/ClusterMerger/gamer_cluster_ics.py
@@ -1,7 +1,7 @@
 import cluster_generator as cg
 import unyt as u
 from numpy.random import RandomState
-import numpy as np  
+import numpy as np
 
 # Note that cluster_generator does not use unyt units for speed and simplicity,
 # so mass units are Msun, length units are kpc, and time units are Myr
@@ -23,8 +23,8 @@ r200_2 = cg.find_overdensity_radius(M200_2, 200.0, z=z)
 a1 = r200_1/conc
 a2 = r200_2/conc
 
-# For the total mass density profile, we will use a "super-NFW" profile, which 
-# is very similar to the NFW profile but falls off slightly faster (Lilley, E. J., 
+# For the total mass density profile, we will use a "super-NFW" profile, which
+# is very similar to the NFW profile but falls off slightly faster (Lilley, E. J.,
 # Wyn Evans, N., & Sanders, J.L. 2018, MNRAS)
 
 # Determine the total mass for each sNFW profile
@@ -32,8 +32,8 @@ M1 = cg.snfw_total_mass(M200_1, r200_1, a1)
 M2 = cg.snfw_total_mass(M200_2, r200_2, a2)
 
 # Use this total mass to construct total mass profiles for each cluster
-Mt1 = cg.snfw_mass_profile(M1, a1) 
-Mt2 = cg.snfw_mass_profile(M2, a2) 
+Mt1 = cg.snfw_mass_profile(M1, a1)
+Mt2 = cg.snfw_mass_profile(M2, a2)
 
 # Use the total mass profiles to determine r500/M500 and r2500/M2500 for
 # each cluster
@@ -55,9 +55,9 @@ rhos2 = 0.02*rhot2
 f_g1 = cg.f_gas(M500_1)
 f_g2 = cg.f_gas(M500_2)
 
-# This sets the gas density profile using the functional form from Vikhlinin, A., 
-# Kravtsov, A., Forman, W., et al. 2006, ApJ, 640, 691 for the first cluster. We 
-# set the scale density to 1.0 first and will rescale it in the next line by the 
+# This sets the gas density profile using the functional form from Vikhlinin, A.,
+# Kravtsov, A., Forman, W., et al. 2006, ApJ, 640, 691 for the first cluster. We
+# set the scale density to 1.0 first and will rescale it in the next line by the
 # gas mass within r500
 rhog1 = cg.vikhlinin_density_profile(1.0, 0.2*r2500_1, 0.67*r200_1, 1.0, 0.67, 3.0)
 rhog1 = cg.rescale_profile_by_mass(rhog1, f_g1*M500_1, r500_1)
@@ -103,12 +103,12 @@ b = 0.0 # in kpc
 center1, center2 = cg.compute_centers_for_binary(center, d, b)
 
 # This sets up a 3D magnetic vector potential which GAMER will take the curl
-# of on the AMR grid to get the initial B-field. It is a tangled field which 
-# uses a Kolmogorov spectrum with a large-scale cutoff of 500 kpc, a 
+# of on the AMR grid to get the initial B-field. It is a tangled field which
+# uses a Kolmogorov spectrum with a large-scale cutoff of 500 kpc, a
 # small-scale cutoff of 10 kpc, and is proportional on average to the pressure
 # everywhere (given by the magnetic field profile of the clusters from above).
 # Outside of r_max = 5000.0 kpc from each cluster center the average B-field
-# is constant 
+# is constant
 left_edge = center-0.5*w
 right_edge = center+0.5*w
 dims = (256,)*3
@@ -117,19 +117,19 @@ bfield = cg.RadialRandomMagneticVectorPotential(left_edge, right_edge, dims,
                                                 "profile1.h5", ctr2=center2,
                                                 profile2="profile2.h5", r_max=5000.0)
 
-# Write the 3D vector potential to the B_IC file                                                                                                             
+# Write the 3D vector potential to the B_IC file
 bfield.write_to_h5("B_IC", overwrite=True, length_unit="Mpc",
                    field_unit="sqrt(1e14*Msun/Mpc**3)*Mpc/(10*Gyr)")
 
 # We now set up the velocities of the two clusters. Assume 1500 km/s
-# relative velocity, and then use the M200 of the two clusters to 
+# relative velocity, and then use the M200 of the two clusters to
 # set velocity vectors in roughly the CM frame. The velocity is in
 # the x-direction only
 velocity = (1500.0*u.km/u.s).to_value("kpc/Myr")
 velocity1 = np.array([velocity*M200_2/(M200_1+M200_2), 0.0, 0.0])
 velocity2 = np.array([-velocity*M200_1/(M200_1+M200_2), 0.0, 0.0])
 
-# Now we set up the cluster initial conditions. use 2e6 DM particles, 
+# Now we set up the cluster initial conditions. use 2e6 DM particles,
 # 4e4 star particles. At r_max = 5000.0 kpc, the profiles of each cluster
 # are constant
 num_particles = {"dm": 2_000_000, "star": 40_000}
