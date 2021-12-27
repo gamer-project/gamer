4c2725e75582a16a89f37a86b3a36eec12610630
koarakawaii <arahaguojnestore@gmail.com>
2021-12-27 14:05:57 +0800

    fix bug for Par_Output_BinaryFile.cpp

--- src/Particle/Par_Output_BinaryFile.cpp
+++ src/Particle/Par_Output_BinaryFile.cpp
@@ -12,26 +12,28 @@
 void Par_Output_BinaryFile( const char *FileName )
 {
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "%s (DumpID = %d) ...\n", __FUNCTION__, DumpID );
-
+//   int N_att = PAR_NATT_TOTAL;
+   FILE *File;
 
 // check
    if ( MPI_Rank == 0  &&  Aux_CheckFileExist(FileName) )
+   {
       Aux_Message( stderr, "WARNING : file \"%s\" already exists and will be overwritten !!\n", FileName );
+      File = fopen( FileName, "w" );
+      fclose(File);
+   }
+   MPI_Barrier( MPI_COMM_WORLD );
 
-   FILE *File;
    real *attribute_buff = (real*)malloc(sizeof(real)*amr->Par->NPar_AcPlusInac);
 // data
-   for (int TargetMPIRank=0; TargetMPIRank<MPI_NRank; TargetMPIRank++)
+   for (int v=0; v<PAR_NATT_TOTAL; v++)
    {
-      if ( MPI_Rank == TargetMPIRank )
+      for (int TargetMPIRank=0; TargetMPIRank<MPI_NRank; TargetMPIRank++)
       {
-          if ( MPI_Rank==0 )
-              File = fopen( FileName, "w" );
-          else
-              File = fopen( FileName, "a" );
-         for (int v=0; v<PAR_NATT_TOTAL; v++)
+         if ( MPI_Rank == TargetMPIRank )
          {
-            int counter = 0;
+            File = fopen( FileName, "a" );
+            long counter = 0;
             for (long p=0; p<amr->Par->NPar_AcPlusInac; p++)
             {
 //             skip inactive particles
@@ -43,14 +45,27 @@ void Par_Output_BinaryFile( const char *FileName )
                   counter ++;
                }
             }
+////          record number of activate particle and number of attribute
+//            if (v==0)
+//            {
+//                MPI_Gather(&counter, 1, MPI_LONG, &counter_gather, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+//                counter_gather += counter;
+//                if (MPI_Rank==0) 
+//                {
+//                    fwrite(&counter_gather, sizeof(long), 1, File);
+//                    printf("%ld\n", counter_gather);
+//                    fwrite(&N_att, sizeof(int), 1, File);
+//                }
+//            }
+////
+//          record particle data
             fwrite(attribute_buff, sizeof(real), counter, File);
-
-         }
-         fclose( File );
+            fclose( File );
+         } // if ( MPI_Rank == TargetMPIRank )
          MPI_Barrier( MPI_COMM_WORLD );
-      } // if ( MPI_Rank == TargetMPIRank )
+      } // for (int TargetMPIRank=0; TargetMPIRank<MPI_NRank; TargetMPIRank++)
+   } // for (int v=0; v<PAR_NATT_TOTAL; v++)
 
-   } // for (int TargetMPIRank=0; TargetMPIRank<MPI_NRank; TargetMPIRank++)
    free(attribute_buff);
 } // FUNCTION : Par_Output_BinaryFile
 #endif // end of ifdef PARTICLE
