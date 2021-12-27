ccddfccca5102abaa00342df4a515dd88ff095a1
John ZuHone <jzuhone@gmail.com>
2021-12-26 21:51:34 -0500

    Trying to package the interpolation into one separate function. This probably doesn't work yet

---
+++ src/Particle/Par_MapMesh2Particles.cpp
@@ -0,0 +1,204 @@
+#include "GAMER.h"
+
+#ifdef PARTICLE
+
+void Par_MapMesh2Particles ( const int lv, const double EdgeL[3], const double EdgeR[3],
+                             const int AttrSize3D, const real Attr3D[AttrSize3D][AttrSize3D][AttrSize3D],
+                             const int NPar, const real InterpParPos[3][NPar], const real ParType[NPar],
+                             const long ParList[NPar], bool useTracers, real ParAttr[NPar] )
+{
+
+   const ParInterp_t IntScheme    = amr->Par->Interp;
+
+   const double _dh               = 1.0/amr->dh[lv];
+
+   long   ParID;
+
+   for (int p=0; p<NPar; p++)
+   {
+      ParID = ParList[p];
+
+      if ( useTracers )
+      {
+//       skip massive particles
+         if ( ParType[ParID] != PTYPE_TRACER )
+            continue;
+      }
+      else
+      {
+//       skip tracer particles
+         if ( ParType[ParID] != PTYPE_TRACER )
+            continue;
+      }
+
+      switch ( IntScheme ) {
+
+//    1 NGP
+      case ( PAR_INTERP_NGP )
+      {
+         int idx[3];
+
+//       calculate the nearest grid index
+         for (int d=0; d<3; d++)
+         {
+            idx[d] = int( ( InterpParPos[d][ParID] - EdgeL[d] )*_dh );
+
+//          prevent from round-off errors (especially for NGP and TSC)
+            if ( idx[d] < 0 )
+            {
+#              ifdef DEBUG_PARTICLE
+               if (  ! Mis_CompareRealValue( InterpParPos[d][ParID], (real)EdgeL[d], NULL, false )  )
+                  Aux_Error( ERROR_INFO, "index outside the attr array (pos[%d] %14.7e, EdgeL %14.7e, idx %d) !!\n",
+                             d, InterpParPos[d][ParID], EdgeL[d], idx[d] );
+#              endif
+
+               idx[d] = 0;
+            }
+            else if ( idx[d] >= AttrSize3D )
+            {
+#              ifdef DEBUG_PARTICLE
+               if (  ! Mis_CompareRealValue( InterpParPos[d][ParID], (real)EdgeR[d], NULL, false )  )
+                  Aux_Error( ERROR_INFO, "index outside the attr array (pos[%d] %14.7e, EdgeR %14.7e, idx %d) !!\n",
+                             d, InterpParPos[d][ParID], EdgeR[d], idx[d] );
+#              endif
+
+               idx[d] = AttrSize3D - 1;
+            }
+         } // for (int d=0; d<3; d++)
+
+//       calculate new particle attribute
+         ParAttr[ParID] = Attr3D[ idx[2] ][ idx[1] ][ idx[0] ];
+
+      } // PAR_INTERP_NGP
+      break;
+
+//    2 CIC
+      case ( PAR_INTERP_CIC ):
+      {
+         int    idxLR[2][3];     // array index of the left (idxLR[0][d]) and right (idxLR[1][d]) cells
+         double dr      [3];     // distance to the center of the left cell
+         double Frac [2][3];     // weighting of the left (Frac[0][d]) and right (Frac[1][d]) cells
+
+         for (int d=0; d<3; d++)
+         {
+//          calculate the array index of the left and right cells
+            dr      [d] = ( InterpParPos[d[ParID] - EdgeL[d] )*_dh + ParGhost - 0.5;
+            idxLR[0][d] = int( dr[d] );
+            idxLR[1][d] = idxLR[0][d] + 1;
+
+//          prevent from round-off errors
+//          (CIC should be clear of this issue unless round-off erros are comparable to dh)
+            if ( idxLR[0][d] < 0 )
+            {
+#              ifdef DEBUG_PARTICLE
+               if (  ! Mis_CompareRealValue( InterpParPos[d][ParID], (real)EdgeL[d], NULL, false )  )
+                  Aux_Error( ERROR_INFO, "index outside the attr array (pos[%d] %14.7e, EdgeL %14.7e, idxL %d, idxR %d) !!\n",
+                             d, InterpParPos[d][ParID], EdgeL[d], idxLR[0][d], idxLR[1][d] );
+#              endif
+
+               idxLR[0][d] = 0;
+               idxLR[1][d] = 1;
+            }
+            else if ( idxLR[1][d] >= AttrSize3D )
+            {
+#              ifdef DEBUG_PARTICLE
+               if (  ! Mis_CompareRealValue( InterpParPos[d][ParID], (real)EdgeR[d], NULL, false )  )
+                  Aux_Error( ERROR_INFO, "index outside the attr array (pos[%d] %14.7e, EdgeR %14.7e, idxL %d, idxR %d) !!\n",
+                             d, InterpParPos[d][ParID], EdgeR[d], idxLR[0][d], idxLR[1][d] );
+#              endif
+
+               idxLR[0][d] = AttrSize3D - 2;
+               idxLR[1][d] = AttrSize3D - 1;
+            }
+
+//          get the weighting of the nearby 8 cells
+            dr     [d] -= (double)idxLR[0][d];
+            Frac[0][d]  = 1.0 - dr[d];
+            Frac[1][d]  =       dr[d];
+
+         } // for (int d=0; d<3; d++)
+
+//       calculate attribute
+         ParAttr[ParID] = (real)0.0;
+
+         for (int k=0; k<2; k++)
+         for (int j=0; j<2; j++)
+         for (int i=0; i<2; i++) {
+            ParAttr[ParID] += Attr3D[ idxLR[k][2] ][ idxLR[j][1] ][ idxLR[i][0] ]
+               *Frac[i][0]*Frac[j][1]*Frac[k][2];
+         }
+
+      } // PAR_INTERP_CIC
+      break;
+
+//    3 TSC
+      case ( PAR_INTERP_TSC ):
+      {
+         int    idxLCR[3][3];    // array index of the left/central/right cells (idxLCR[0/1/2][d])
+         double dr       [3];    // distance to the left edge of the central cell
+         double Frac  [3][3];    // weighting of the left/central/right cells (Frac[0/1/2][d])
+
+         for (int d=0; d<3; d++)
+         {
+//          calculate the array index of the left, central, and right cells
+            dr       [d] = ( InterpParPos[d][ParID] - EdgeL[d] )*_dh + ParGhost;
+            idxLCR[1][d] = int( dr[d] );
+            idxLCR[0][d] = idxLCR[1][d] - 1;
+            idxLCR[2][d] = idxLCR[1][d] + 1;
+
+//          prevent from round-off errors (especially for NGP and TSC)
+            if ( idxLCR[0][d] < 0 )
+            {
+#              ifdef DEBUG_PARTICLE
+               if (  ! Mis_CompareRealValue( InterpParPos[d][ParID], (real)EdgeL[d], NULL, false )  )
+                  Aux_Error( ERROR_INFO, "index outside the attr array (pos[%d] %14.7e, EdgeL %14.7e, idxL %d, idxR %d) !!\n",
+                             d, InterpParPos[d][ParID], EdgeL[d], idxLCR[0][d], idxLCR[2][d] );
+#              endif
+
+               idxLCR[0][d] = 0;
+               idxLCR[1][d] = 1;
+               idxLCR[2][d] = 2;
+            }
+            else if ( idxLCR[2][d] >= AttrSize3D )
+            {
+#              ifdef DEBUG_PARTICLE
+               if (  ! Mis_CompareRealValue( InterpParPos[d][ParID], (real)EdgeR[d], NULL, false )  )
+                  Aux_Error( ERROR_INFO, "index outside the attr array (pos[%d] %14.7e, EdgeR %14.7e, idxL %d, idxR %d) !!\n",
+                             d, InterpParPos[d][ParID], EdgeR[d], idxLCR[0][d], idxLCR[2][d] );
+#              endif
+
+               idxLCR[0][d] = AttrSize3D - 3;
+               idxLCR[1][d] = AttrSize3D - 2;
+               idxLCR[2][d] = AttrSize3D - 1;
+            }
+
+//          get the weighting of the nearby 27 cells
+            dr     [d] -= (double)idxLCR[1][d];
+            Frac[0][d]  = 0.5*SQR( 1.0 - dr[d] );
+            Frac[1][d]  = 0.5*( 1.0 + 2.0*dr[d] - 2.0*SQR(dr[d]) );
+            Frac[2][d]  = 0.5*SQR( dr[d] );
+         } // for (int d=0; d<3; d++)
+
+//       calculate attribute
+         ParAttr[ParID] = (real)0.0;
+
+         for (int k=0; k<3; k++)
+         for (int j=0; j<3; j++)
+         for (int i=0; i<3; i++) {
+
+            ParAttr[ParID] += Attr3D[ idxLCR[k][2] ][ idxLCR[j][1] ][ idxLCR[i][0] ]
+               *Frac[i][0]*Frac[j][1]*Frac[k][2];
+
+         }
+
+      } // PAR_INTERP_TSC
+      break;
+
+      default: Aux_Error( ERROR_INFO, "unsupported particle interpolation scheme !!\n" );
+      } // switch ( IntScheme )
+
+   } //
+
+} // FUNCTION : Par_MapMesh2Particles
+
+#endif // #ifdef PARTICLE
--- src/Particle/Par_UpdateTracerParticle.cpp
+++ src/Particle/Par_UpdateTracerParticle.cpp
@@ -2,7 +2,10 @@
 
 #ifdef PARTICLE
 
-
+void Par_MapMesh2Particles ( const int lv, const double EdgeL[3], const double EdgeR[3],
+                             const int AttrSize3D, const real Attr3D[AttrSize3D][AttrSize3D][AttrSize3D],
+                             const int NPar, const real InterpParPos[3][NPar], const real ParType[NPar],
+                             const long ParList[NPar], bool useTracers, real ParAttr[NPar] );
 
 
 //-------------------------------------------------------------------------------------------------------
@@ -45,14 +48,10 @@ void Par_UpdateTracerParticle( const int lv, const double TimeNew, const double 
    const int  ParGhost            = amr->Par->GhostSize;
    const int  VelSize             = PS1 + 2*ParGhost;
 
-   real InterpParPos[3];
-
    real *ParPos[3] = { amr->Par->PosX, amr->Par->PosY, amr->Par->PosZ };
    real *ParVel[3] = { amr->Par->VelX, amr->Par->VelY, amr->Par->VelZ };
    real *ParTime   = amr->Par->Time;
    real *ParType   = amr->Par->Type;
-
-
 
 // check
 #  ifdef COMOVING
@@ -64,9 +63,9 @@ void Par_UpdateTracerParticle( const int lv, const double TimeNew, const double 
    {
 
 // per-thread variables
-   real *VelX = new real [ 8*CUBE(VelSize)  ];    // 8: number of patches per patch group
-   real *VelY = new real [ 8*CUBE(VelSize)  ];    // 8: number of patches per patch group
-   real *VelZ = new real [ 8*CUBE(VelSize)  ];    // 8: number of patches per patch group
+   real *VelX = new real [ 8*CUBE(VelSize) ];    // 8: number of patches per patch group
+   real *VelY = new real [ 8*CUBE(VelSize) ];    // 8: number of patches per patch group
+   real *VelZ = new real [ 8*CUBE(VelSize) ];    // 8: number of patches per patch group
 
    real (*VelX3D)[VelSize][VelSize][VelSize]    = ( real (*)[VelSize][VelSize][VelSize] )VelX;
    real (*VelY3D)[VelSize][VelSize][VelSize]    = ( real (*)[VelSize][VelSize][VelSize] )VelY;
@@ -74,7 +73,7 @@ void Par_UpdateTracerParticle( const int lv, const double TimeNew, const double 
 
    bool   GotYou;
    long   ParID;
-   real   Vel_Temp[3], dt, dt_half;
+   real   dt, dt_half;
    double x, y, z;
 
 
@@ -116,8 +115,43 @@ void Par_UpdateTracerParticle( const int lv, const double TimeNew, const double 
 
       for (int PID=PID0, P=0; PID<PID0+8; PID++, P++)
       {
-          // skip patches with no tracer particles
-         if ( amr->patch[0][lv][PID]->NParType[(long)PTYPE_TRACER] == 0 )  continue; 
+//       skip patches with no tracer particles
+         if ( amr->patch[0][lv][PID]->NParType[(long)PTYPE_TRACER] == 0 ) continue;
+
+         real** Temp_Vel = new real*[3];
+         real** InterpParPos = new real*[3];
+         for (int d=0; d<3; d++)
+            Temp_Vel[i] = new real[amr->patch[0][lv][PID]->NPar];
+            InterpParPos[i] = new real[amr->patch[0][lv][PID]->NPar];
+
+         for (int p=0; p<amr->patch[0][lv][PID]->NPar; p++)
+         {
+            ParID = amr->patch[0][lv][PID]->ParList[p];
+
+//          skip massive particles
+            if ( ParType[ParID] != PTYPE_TRACER )
+               continue;
+
+//          determine time-step and skip particles with zero or negative time-step
+            dt      = (real)TimeNew - ParTime[ParID];
+            if ( dt <= (real)0.0 )  continue;
+
+            for (int d=0; d<3; d++)
+               InterpParPos[d][ParID] = ParPos[d][ParID]+dt*ParVel[d][ParID];
+
+         } // for (int p=0; p<amr->patch[0][lv][PID]->NPar; p++)
+
+         Par_MapMesh2Particles( lv, amr->patch[0][lv][PID]->EdgeL, amr->patch[0][lv][PID]->EdgeR,
+                                VelSize, VelX3D[P], amr->patch[0][lv][PID]->NPar, InterpParPos,
+                                ParType, ParList, true, Vel_Temp[0] );
+         Par_MapMesh2Particles( lv, amr->patch[0][lv][PID]->EdgeL, amr->patch[0][lv][PID]->EdgeR,
+                                VelSize, VelY3D[P], amr->patch[0][lv][PID]->NPar, InterpParPos,
+                                ParType, ParList, true, Vel_Temp[1] );
+         Par_MapMesh2Particles( lv, amr->patch[0][lv][PID]->EdgeL, amr->patch[0][lv][PID]->EdgeR,
+                                VelSize, VelY3D[P], amr->patch[0][lv][PID]->NPar, InterpParPos,
+                                ParType, ParList, true, Vel_Temp[2] );
+
+//       5. update particles
 
          for (int p=0; p<amr->patch[0][lv][PID]->NPar; p++)
          {
@@ -132,213 +166,15 @@ void Par_UpdateTracerParticle( const int lv, const double TimeNew, const double 
             if ( dt <= (real)0.0 )  continue;
             dt_half = (real)0.5*dt;
 
-            int npass = amr->Par->IntegTracer == TRACER_INTEG_RK2 ? 2:1;
-
-            for (int ipass=0; ipass<npass; ipass++) {
-
-            for (int d=0; d<3; d++) {
-               if ( ipass == 0 ) 
-                  InterpParPos[d] = ParPos[d][ParID]+dt*ParVel[d][ParID];
-               else
-                  InterpParPos[d] = ParPos[d][ParID];
-            }
-
-//          4. calculate gas velocity at the particle position
-            switch ( IntScheme ) {
-
-//          4.1 NGP
-            case ( PAR_INTERP_NGP ):
-            {
-               int idx[3];
-
-//             calculate the nearest grid index
-               for (int d=0; d<3; d++)
-               {
-                  idx[d] = int( ( InterpParPos[d] - amr->patch[0][lv][PID]->EdgeL[d] )*_dh );
-
-//                prevent from round-off errors (especially for NGP and TSC)
-                  if ( idx[d] < 0 )
-                  {
-#                    ifdef DEBUG_PARTICLE
-                     if (  ! Mis_CompareRealValue( InterpParPos[d], (real)amr->patch[0][lv][PID]->EdgeL[d], NULL, false )  )
-                     Aux_Error( ERROR_INFO, "index outside the vel array (pos[%d] %14.7e, EdgeL %14.7e, idx %d) !!\n",
-                                d, InterpParPos[d], amr->patch[0][lv][PID]->EdgeL[d], idx[d] );
-#                    endif
-
-                     idx[d] = 0;
-                  }
-
-                  else if ( idx[d] >= VelSize )
-                  {
-#                    ifdef DEBUG_PARTICLE
-                     if (  ! Mis_CompareRealValue( InterpParPos[d], (real)amr->patch[0][lv][PID]->EdgeR[d], NULL, false )  )
-                        Aux_Error( ERROR_INFO, "index outside the vel array (pos[%d] %14.7e, EdgeR %14.7e, idx %d) !!\n",
-                                   d, InterpParPos[d], amr->patch[0][lv][PID]->EdgeR[d], idx[d] );
-#                    endif
-
-                     idx[d] = VelSize - 1;
-                  }
-               } // for (int d=0; d<3; d++)
-
-//             calculate new particle velocity
-               Vel_Temp[0] = VelX3D[P][ idx[2] ][ idx[1] ][ idx[0] ];
-               Vel_Temp[1] = VelY3D[P][ idx[2] ][ idx[1] ][ idx[0] ];
-               Vel_Temp[2] = VelZ3D[P][ idx[2] ][ idx[1] ][ idx[0] ];
-
-            } // PAR_INTERP_NGP
-            break;
-
-
-//          4.2 CIC
-            case ( PAR_INTERP_CIC ):
-            {
-               int    idxLR[2][3];     // array index of the left (idxLR[0][d]) and right (idxLR[1][d]) cells
-               double dr      [3];     // distance to the center of the left cell
-               double Frac [2][3];     // weighting of the left (Frac[0][d]) and right (Frac[1][d]) cells
-
-               for (int d=0; d<3; d++)
-               {
-//                calculate the array index of the left and right cells
-                  dr      [d] = ( InterpParPos[d] - amr->patch[0][lv][PID]->EdgeL[d] )*_dh + ParGhost - 0.5;
-                  idxLR[0][d] = int( dr[d] );
-                  idxLR[1][d] = idxLR[0][d] + 1;
-
-//                prevent from round-off errors
-//                (CIC should be clear off this issue unless round-off erros are comparable to dh)
-                  if ( idxLR[0][d] < 0 )
-                  {
-#                    ifdef DEBUG_PARTICLE
-                     if (  ! Mis_CompareRealValue( InterpParPos[d], (real)amr->patch[0][lv][PID]->EdgeL[d], NULL, false )  )
-                     Aux_Error( ERROR_INFO, "index outside the vel array (pos[%d] %14.7e, EdgeL %14.7e, idxL %d, idxR %d) !!\n",
-                                d, InterpParPos[d], amr->patch[0][lv][PID]->EdgeL[d], idxLR[0][d], idxLR[1][d] );
-#                    endif
-
-                     idxLR[0][d] = 0;
-                     idxLR[1][d] = 1;
-                  }
-
-                  else if ( idxLR[1][d] >= VelSize )
-                  {
-#                    ifdef DEBUG_PARTICLE
-                     if (  ! Mis_CompareRealValue( InterpParPos[d], (real)amr->patch[0][lv][PID]->EdgeR[d], NULL, false )  )
-                     Aux_Error( ERROR_INFO, "index outside the vel array (pos[%d] %14.7e, EdgeR %14.7e, idxL %d, idxR %d) !!\n",
-                                d, InterpParPos[d], amr->patch[0][lv][PID]->EdgeR[d], idxLR[0][d], idxLR[1][d] );
-#                    endif
-
-                     idxLR[0][d] = VelSize - 2;
-                     idxLR[1][d] = VelSize - 1;
-                  }
-
-//                get the weighting of the nearby 8 cells
-                  dr     [d] -= (double)idxLR[0][d];
-                  Frac[0][d]  = 1.0 - dr[d];
-                  Frac[1][d]  =       dr[d];
-               } // for (int d=0; d<3; d++)
-
-//             calculate velocity
-               for (int d=0; d<3; d++) Vel_Temp[d] = (real)0.0;
-
-               for (int k=0; k<2; k++) {
-                  for (int j=0; j<2; j++) {
-                     for (int i=0; i<2; i++) {
-                        Vel_Temp[0] += VelX3D[P][ idxLR[k][2] ][ idxLR[j][1] ][ idxLR[i][0] ]
-                                   *Frac[i][0]*Frac[j][1]*Frac[k][2];
-                        Vel_Temp[1] += VelY3D[P][ idxLR[k][2] ][ idxLR[j][1] ][ idxLR[i][0] ]
-                                   *Frac[i][0]*Frac[j][1]*Frac[k][2];
-                        Vel_Temp[2] += VelZ3D[P][ idxLR[k][2] ][ idxLR[j][1] ][ idxLR[i][0] ]
-                                   *Frac[i][0]*Frac[j][1]*Frac[k][2];
-                     }
-                  }
-               }
-
-            } // PAR_INTERP_CIC
-            break;
-
-
-//          4.3 TSC
-            case ( PAR_INTERP_TSC ):
-            {
-               int    idxLCR[3][3];    // array index of the left/central/right cells (idxLCR[0/1/2][d])
-               double dr       [3];    // distance to the left edge of the central cell
-               double Frac  [3][3];    // weighting of the left/central/right cells (Frac[0/1/2][d])
-
-               for (int d=0; d<3; d++)
-               {
-//                calculate the array index of the left, central, and right cells
-                  dr       [d] = ( InterpParPos[d] - amr->patch[0][lv][PID]->EdgeL[d] )*_dh + ParGhost;
-                  idxLCR[1][d] = int( dr[d] );
-                  idxLCR[0][d] = idxLCR[1][d] - 1;
-                  idxLCR[2][d] = idxLCR[1][d] + 1;
-
-//                prevent from round-off errors (especially for NGP and TSC)
-                  if ( idxLCR[0][d] < 0 )
-                  {
-#                    ifdef DEBUG_PARTICLE
-                     if (  ! Mis_CompareRealValue( InterpParPos[d], (real)amr->patch[0][lv][PID]->EdgeL[d], NULL, false )  )
-                     Aux_Error( ERROR_INFO, "index outside the vel array (pos[%d] %14.7e, EdgeL %14.7e, idxL %d, idxR %d) !!\n",
-                                d, InterpParPos[d], amr->patch[0][lv][PID]->EdgeL[d], idxLCR[0][d], idxLCR[2][d] );
-#                    endif
-
-                     idxLCR[0][d] = 0;
-                     idxLCR[1][d] = 1;
-                     idxLCR[2][d] = 2;
-                  }
-
-                  else if ( idxLCR[2][d] >= VelSize )
-                  {
-#                    ifdef DEBUG_PARTICLE
-                     if (  ! Mis_CompareRealValue( InterpParPos[d], (real)amr->patch[0][lv][PID]->EdgeR[d], NULL, false )  )
-                     Aux_Error( ERROR_INFO, "index outside the vel array (pos[%d] %14.7e, EdgeR %14.7e, idxL %d, idxR %d) !!\n",
-                                d, InterpParPos[d], amr->patch[0][lv][PID]->EdgeR[d], idxLCR[0][d], idxLCR[2][d] );
-#                    endif
-
-                     idxLCR[0][d] = VelSize - 3;
-                     idxLCR[1][d] = VelSize - 2;
-                     idxLCR[2][d] = VelSize - 1;
-                  }
-
-//                get the weighting of the nearby 27 cells
-                  dr     [d] -= (double)idxLCR[1][d];
-                  Frac[0][d]  = 0.5*SQR( 1.0 - dr[d] );
-                  Frac[1][d]  = 0.5*( 1.0 + 2.0*dr[d] - 2.0*SQR(dr[d]) );
-                  Frac[2][d]  = 0.5*SQR( dr[d] );
-               } // for (int d=0; d<3; d++)
-
-//             calculate velocity
-               for (int d=0; d<3; d++) Vel_Temp[d] = (real)0.0;
-
-               for (int k=0; k<3; k++) {
-                  for (int j=0; j<3; j++) {
-                     for (int i=0; i<3; i++) {
-                        Vel_Temp[0] += VelX3D[P][ idxLCR[k][2] ][ idxLCR[j][1] ][ idxLCR[i][0] ]
-                                   *Frac[i][0]*Frac[j][1]*Frac[k][2];
-                        Vel_Temp[1] += VelY3D[P][ idxLCR[k][2] ][ idxLCR[j][1] ][ idxLCR[i][0] ]
-                                   *Frac[i][0]*Frac[j][1]*Frac[k][2];
-                        Vel_Temp[2] += VelZ3D[P][ idxLCR[k][2] ][ idxLCR[j][1] ][ idxLCR[i][0] ]
-                                   *Frac[i][0]*Frac[j][1]*Frac[k][2];
-                     }
-                  }
-               }
-
-            } // PAR_INTERP_TSC
-            break;
-
-
-            default: Aux_Error( ERROR_INFO, "unsupported particle interpolation scheme !!\n" );
-            } // switch ( IntScheme )
-
-
-//          5. update particles
-
-
 //          5.0 Euler method
             if ( amr->Par->IntegTracer == TRACER_INTEG_EULER )
             {
-               for (int d=0; d<3; d++)
-               {
-                  ParPos[d][ParID] = InterpParPos[d];
-                  ParVel[d][ParID] = Vel_Temp[d];
+
+               for (int d=0; d<3; d++) {
+                  ParPos[d][ParID] = InterpParPos[d][ParID];
+                  ParVel[d][ParID] = Vel_Temp[d][ParID];
                }
+               ParTime[ParID] = TimeNew;
 
             }
 
@@ -347,21 +183,52 @@ void Par_UpdateTracerParticle( const int lv, const double TimeNew, const double 
             {
 
                for (int d=0; d<3; d++)
-               {
-                  if ( ipass == 0 ) {
-                     ParPos[d][ParID] = InterpParPos[d] + dt_half*(Vel_Temp[d]-ParVel[d][ParID]);
-                  } else {
-                     ParVel[d][ParID] = Vel_Temp[d];
-                  } 
-               }
+                  ParPos[d][ParID] = InterpParPos[d] + dt_half*(Vel_Temp[d]-ParVel[d][ParID]);
 
             } // amr->Par->IntegTracer
 
-            } // for (int ipass=0; i<npass; ipass++)
-
-            ParTime[ParID] = TimeNew;
-
          } // for (int p=0; p<amr->patch[0][lv][PID]->NPar; p++)`
+
+         if ( amr->Par->IntegTracer == TRACER_INTEG_RK2 )
+         {
+
+            Par_MapMesh2Particles( lv, amr->patch[0][lv][PID]->EdgeL, amr->patch[0][lv][PID]->EdgeR,
+                                   VelSize, VelX3D[P], amr->patch[0][lv][PID]->NPar, InterpParPos,
+                                   ParType, ParList, true, Vel_Temp[0] );
+            Par_MapMesh2Particles( lv, amr->patch[0][lv][PID]->EdgeL, amr->patch[0][lv][PID]->EdgeR,
+                                   VelSize, VelY3D[P], amr->patch[0][lv][PID]->NPar, InterpParPos,
+                                   ParType, ParList, true, Vel_Temp[1] );
+            Par_MapMesh2Particles( lv, amr->patch[0][lv][PID]->EdgeL, amr->patch[0][lv][PID]->EdgeR,
+                                   VelSize, VelY3D[P], amr->patch[0][lv][PID]->NPar, InterpParPos,
+                                   ParType, ParList, true, Vel_Temp[2] );
+
+            for (int p=0; p<amr->patch[0][lv][PID]->NPar; p++)
+
+               ParID = amr->patch[0][lv][PID]->ParList[p];
+
+//             skip massive particles
+               if ( ParType[ParID] != PTYPE_TRACER )
+                  continue;
+
+//             determine time-step and skip particles with zero or negative time-step
+               dt      = (real)TimeNew - ParTime[ParID];
+               if ( dt <= (real)0.0 )  continue;
+               dt_half = (real)0.5*dt;
+
+               for (int d=0; d<3; d++)
+                  ParVel[d][ParID] = Vel_Temp[d][ParID];
+               ParTime[ParID] = TimeNew;
+
+            } // for (int p=0; p<amr->patch[0][lv][PID]->NPar; p++)`
+
+         } // if ( amr->Par->IntegTracer == TRACER_INTEG_RK2 )
+
+         for (int d=0; d<3; d++)
+            delete [] Temp_Vel[i];
+            delete [] InterpParPos[i];
+         delete [] Temp_Vel;
+         delete [] InterpParPos;
+
       } // for (int PID=PID0, P=0; PID<PID0+8; PID++, P++)
    } // for (int PID0=0; PID0<amr->NPatchComma[lv][1]; PID0+=8)
 
