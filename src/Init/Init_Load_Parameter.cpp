e446445992d4fa58bccc0ef7deebbde98c8a2199
Hsi-Yu Schive <hyschive@gmail.com>
2022-01-09 22:13:14 +0800

    Disable ELBDM_TAYLOR3_AUTO by default

--- src/Init/Init_Load_Parameter.cpp
+++ src/Init/Init_Load_Parameter.cpp
@@ -251,7 +251,7 @@ void Init_Load_Parameter()
    ReadPara->Add( "ELBDM_LAMBDA",               &ELBDM_LAMBDA,                    1.0,             NoMin_double,  NoMax_double   );
 #  endif
    ReadPara->Add( "ELBDM_TAYLOR3_COEFF",        &ELBDM_TAYLOR3_COEFF,             1.0/6.0,         NoMin_double,  NoMax_double   );
-   ReadPara->Add( "ELBDM_TAYLOR3_AUTO",         &ELBDM_TAYLOR3_AUTO,              true,            Useless_bool,  Useless_bool   );
+   ReadPara->Add( "ELBDM_TAYLOR3_AUTO",         &ELBDM_TAYLOR3_AUTO,              false,           Useless_bool,  Useless_bool   );
    ReadPara->Add( "ELBDM_REMOVE_MOTION_CM",     &ELBDM_REMOVE_MOTION_CM,          ELBDM_REMOVE_MOTION_CM_NONE, 0, 2              );
 #  endif // #if ( MODEL == ELBDM )
 
