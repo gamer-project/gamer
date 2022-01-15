fc9bd1900d653eeff50c78a250ff69634a5ff444
Po-Hsun Tseng <zengbs@gmail.com>
2022-01-15 23:31:54 +0800

    update

--- src/Model_Hydro/CPU_Hydro/CPU_Shared_FullStepUpdate.cpp
+++ src/Model_Hydro/CPU_Hydro/CPU_Shared_FullStepUpdate.cpp
@@ -189,8 +189,8 @@ void Hydro_FullStepUpdate( const real g_Input[][ CUBE(FLU_NXT) ], real g_Output[
 
 
 //      5-2. return all threads within a block when any cell in the block is unphysical
-//           --> do not return when Iteration == MaxIteration, ensuring that the rest of
-//               cells in the patch group are stored properly
+//           --> return only when Iteration < MaxIteration, ensuring
+//               that the rest of cells in the patch group are stored properly
         if ( *FullStepFailure == 1 && Iteration < MaxIteration )     return;
 
 
