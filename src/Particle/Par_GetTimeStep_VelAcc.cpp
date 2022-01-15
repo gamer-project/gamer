77155d195d608d1197a4e832519558643bde33ea
John ZuHone <jzuhone@gmail.com>
2022-01-15 11:33:20 -0500

    don't check acceleration for tracer particles

--- src/Particle/Par_GetTimeStep_VelAcc.cpp
+++ src/Particle/Par_GetTimeStep_VelAcc.cpp
@@ -53,6 +53,7 @@ void Par_GetTimeStep_VelAcc( double &dt_vel, double &dt_acc, const int lv )
 #  else
    const int   NT         = 1;
 #  endif
+   const real *ParType   = amr->Par->Type;
 
 #  ifndef STORE_PAR_ACC
    if ( UseAcc )
@@ -68,8 +69,9 @@ void Par_GetTimeStep_VelAcc( double &dt_vel, double &dt_acc, const int lv )
    const bool TimingSendPar_No = false;
 
    if ( IncNonleaf )
-      Par_CollectParticle2OneLevel( lv, _PAR_VEL|((UseAcc)?_PAR_ACC:0), PredictPos_No, NULL_REAL,
-                                    SibBufPatch_No, FaSibBufPatch_No, JustCountNPar_No, TimingSendPar_No );
+      Par_CollectParticle2OneLevel( lv, _PAR_VEL|((UseAcc)?_PAR_ACC:0)|_PAR_TYPE, PredictPos_No,
+                                    NULL_REAL, SibBufPatch_No, FaSibBufPatch_No, JustCountNPar_No,
+                                    TimingSendPar_No );
 
 
 // get the maximum particle velocity and acceleration on the target level
@@ -138,18 +140,25 @@ void Par_GetTimeStep_VelAcc( double &dt_vel, double &dt_acc, const int lv )
 //          ParAtt_Copy[] is only defined in LOAD_BALANCE
             real *Vel_Copy[3] = { NULL, NULL, NULL };
             real *Acc_Copy[3] = { NULL, NULL, NULL };
+            real *Typ_Copy = NULL;
 #           ifdef LOAD_BALANCE
             for (int d=0; d<3; d++)
             {
                Vel_Copy[d] = amr->patch[0][lv][PID]->ParAtt_Copy[ PAR_VELX + d ];
                Acc_Copy[d] = amr->patch[0][lv][PID]->ParAtt_Copy[ PAR_ACCX + d ];
             }
+            Typ_Copy    = amr->patch[0][lv][PID]->ParAtt_Copy[ PAR_TYPE ];
 #           endif
 
             for (int p=0; p<NParThisPatch; p++)
             {
+
                for (int d=0; d<3; d++)
                MaxVel_OMP[TID] = MAX( MaxVel_OMP[TID], FABS(Vel_Copy[d][p]) );
+
+//             don't check acceleration for tracer particles
+               if ( Typ_Copy[p] == PTYPE_TRACER )
+                  continue;
 
                if ( UseAcc )
                for (int d=0; d<3; d++)
@@ -165,6 +174,10 @@ void Par_GetTimeStep_VelAcc( double &dt_vel, double &dt_acc, const int lv )
 
                for (int d=0; d<3; d++)
                MaxVel_OMP[TID] = MAX( MaxVel_OMP[TID], FABS(Vel[d][ParID]) );
+
+//             don't check acceleration for tracer particles
+               if ( ParType[ParID] == PTYPE_TRACER )
+                  continue;
 
                if ( UseAcc )
                for (int d=0; d<3; d++)
