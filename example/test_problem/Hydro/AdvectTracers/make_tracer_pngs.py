44995a7e719ad8cf768c86021837cdae7c0955bf
John ZuHone <jzuhone@gmail.com>
2022-01-22 13:47:54 -0500

    Updates for the AdvectTracers example

--- example/test_problem/Hydro/AdvectTracers/make_tracer_pngs.py
+++ example/test_problem/Hydro/AdvectTracers/make_tracer_pngs.py
@@ -9,7 +9,7 @@ fns.sort()
 ts = yt.DatasetSeries(fns)
 
 for ds in ts:
-    slc = yt.SlicePlot(ds, "z", ["velocity_magnitude"])
+    slc = yt.SlicePlot(ds, "z", ["velocity_magnitude","particle_density_on_grid"])
     slc.annotate_particles(1.0, p_size=10)
     slc.annotate_velocity(plot_args={"color": "red"})
     slc.annotate_grids()
