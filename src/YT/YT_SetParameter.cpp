2ee5382a48185c18f61a31828b5ad941db3a5299
cindytsai <6188366+cindytsai@users.noreply.github.com>
2022-04-09 22:57:21 +0800

    Update LIBYT_USE_PATCH_GROUP for Makefile and YT_SetParameter.cpp

--- src/YT/YT_SetParameter.cpp
+++ src/YT/YT_SetParameter.cpp
@@ -42,10 +42,16 @@ void YT_SetParameter( const int NPatchAllLv, const int NField, const int NPatchL
    param_yt.current_time            = Time[0];
    param_yt.dimensionality          = 3;
    param_yt.refine_by               = 2;
-   param_yt.num_grids               = NPatchAllLv;
-
    param_yt.num_fields              = NField;
+
+#  ifdef LIBYT_USE_PATCH_GROUP
+   if ( NPatchAllLv % 8 != 0 || NPatchLocalLv % 8 != 0 ) Aux_Error( ERROR_INFO, "Using patch group in libyt failed !!\n" );
+   param_yt.num_grids               = NPatchAllLv / 8;
+   param_yt.num_grids_local         = NPatchLocalLv / 8;
+#  else
+   param_yt.num_grids               = NPatchAllLv;
    param_yt.num_grids_local         = NPatchLocalLv;
+#  endif
 
 #  ifdef PARTICLE
    yt_species *species_list         = new yt_species [1];
@@ -98,7 +104,7 @@ void YT_SetParameter( const int NPatchAllLv, const int NField, const int NPatchL
 #  if ( MODEL == HYDRO )
    const double gamma = (double) GAMMA;
    const double mu = (double) MOLECULAR_WEIGHT;
-   const int srhd = 0; // TODO: No SRHydrodynamics found inside gamer.
+   const int srhd = 0;
    if (yt_add_user_parameter_double("gamma", 1, &gamma) != YT_SUCCESS )  Aux_Error( ERROR_INFO, "yt_add_user_parameter() add GAMMA failed !!\n" );
    if (yt_add_user_parameter_double("mu", 1, &mu) != YT_SUCCESS )  Aux_Error( ERROR_INFO, "yt_add_user_parameter() add MOLECULAR_WEIGHT failed !!\n" );
    if (yt_add_user_parameter_int("srhd", 1, &srhd) != YT_SUCCESS ) Aux_Error( ERROR_INFO, "yt_add_user_parameter() add srhd failed !!\n" );
