1d3744fb6aec3c023932359d69d769ed55f0326d
cindytsai <6188366+cindytsai@users.noreply.github.com>
2022-04-09 22:59:23 +0800

    LIBYT_USE_PATCH_GROUP update in YT_AddLocalGrid.cpp

--- src/YT/YT_AddLocalGrid.cpp
+++ src/YT/YT_AddLocalGrid.cpp
@@ -1,13 +1,178 @@
 #include "GAMER.h"
 
 #ifdef SUPPORT_LIBYT
-
-
-
+#ifdef LIBYT_USE_PATCH_GROUP
 
 //-------------------------------------------------------------------------------------------------------
 // Function    :  YT_AddLocalGrid
-// Description :  Send the hierarchy information and data of local patches to libyt
+// Description :  Send the hierarchy information and data of local patches to libyt in patch.
+//
+// Note        :  1. One must call YT_SetParameter() before invoking this function
+//                2. Invoked by YT_Inline()
+//                3. FieldList is used by MHD field, since it needs to load dimensions to yt_field.
+//
+// Parameter   :  GID_Offset    : Global patch index offset at each refinement level for this rank
+//                GID_LvStart   : Glocal patch index that this level starts at
+//                NPatchAllRank : Number of patches in [MPI rank][level]
+//                NField        : Number of fields loaded to YT.
+//                FieldList     : List of field_name, field_define_type.
+//
+// Return      :  None
+//-------------------------------------------------------------------------------------------------------
+void YT_AddLocalGrid( const int *GID_Offset, const int *GID_LvStart, const int (*NPatchAllRank)[NLEVEL], int NField, yt_field *FieldList)
+{
+
+   if ( OPT__VERBOSE  &&  MPI_Rank == 0 )    Aux_Message( stdout, "%s ...\n", __FUNCTION__ );
+
+// get the libyt local grids array pointer
+   yt_grid *YT_Grids;
+   yt_get_gridsPtr( &YT_Grids );
+
+// record local grids index and patched grids index
+   int LID = 0;
+   int LID_Patch = 0;
+
+// get the search table, needed by parent_id
+   int   RecvCount_LBIdx[MPI_NRank], RecvDisp_LBIdx[MPI_NRank];
+   long *LBIdxList_Sort[NLEVEL], *LBIdxList_Local[NLEVEL];
+   int  *LBIdxList_Sort_IdxTable[NLEVEL];
+
+   for (int lv=0; lv<NLEVEL; lv++)
+   {
+      LBIdxList_Local        [lv] = new long [ amr->NPatchComma[lv][1] ];
+      LBIdxList_Sort         [lv] = new long [ NPatchTotal[lv] ];
+      LBIdxList_Sort_IdxTable[lv] = new int  [ NPatchTotal[lv] ];
+   }
+
+   for (int lv=0; lv<NLEVEL; lv++)
+   {
+      for (int r=0; r<MPI_NRank; r++)
+      {
+         RecvCount_LBIdx[r] = NPatchAllRank[r][lv];
+         RecvDisp_LBIdx [r] = ( r == 0 ) ? 0 : RecvDisp_LBIdx[r-1] + RecvCount_LBIdx[r-1];
+      }
+
+      for (int PID=0; PID<amr->NPatchComma[lv][1]; PID++)
+         LBIdxList_Local[lv][PID] = amr->patch[0][lv][PID]->LB_Idx;
+
+      MPI_Allgatherv( LBIdxList_Local[lv], amr->NPatchComma[lv][1], MPI_LONG,
+                      LBIdxList_Sort[lv], RecvCount_LBIdx, RecvDisp_LBIdx, MPI_LONG,
+                      MPI_COMM_WORLD );
+   }
+
+   for (int lv=0; lv<NLEVEL; lv++)
+      Mis_Heapsort( NPatchTotal[lv], LBIdxList_Sort[lv], LBIdxList_Sort_IdxTable[lv] );
+
+// loop over local patches at all levels
+   for (int lv=0; lv<NLEVEL; lv++)
+   {
+      const int FaLv  = lv - 1;
+      const int FluSg = amr->FluSg[lv];
+#     ifdef GRAVITY
+      const int PotSg = amr->PotSg[lv];
+#     endif
+
+#     ifdef MHD
+      const int MagSg = amr->MagSg[lv];
+#     endif
+
+      for (int PID=0; PID<(amr->NPatchComma[lv][1]); PID++)
+      {
+         const int GID = PID + GID_Offset[lv];
+         LID_Patch = LID / 8;
+
+         if ( LID % 8 == 0 ){
+             for (int d=0; d<3; d++)
+             {
+                YT_Grids[LID_Patch].left_edge [d] = (amr->patch[0][lv][PID]->EdgeL[d]) * 2;
+                YT_Grids[LID_Patch].right_edge[d] = (amr->patch[0][lv][PID]->EdgeR[d]) * 2;
+                YT_Grids[LID_Patch].grid_dimensions[d] = PATCH_SIZE * 2;
+             }
+
+#            ifdef PARTICLE
+             // counting particle number inside this grid patch
+             long particle_count = 0;
+             for(int i=PID; i<PID+8; i++){
+                 particle_count += (long) amr->patch[0][lv][i]->NPar;
+             }
+             // input particle num in this grid patch
+             YT_Grids[LID_Patch].particle_count_list[0] = particle_count;
+#            endif
+
+             YT_Grids[LID_Patch].id             = GID / 8;
+             YT_Grids[LID_Patch].level          = lv;
+
+             // getting parent's id
+             int FaPID = amr->patch[0][lv][PID]->father;
+             int FaLv = lv - 1;
+
+             if ( FaPID < 0 ){
+                // no father (only possible for the root patches)
+#            ifdef DEBUG_HDF5
+                if ( lv != 0 )       Aux_Error( ERROR_INFO, "Lv %d, PID %d, FaPID %d < 0 !!\n", lv, PID, FaPID );
+                if ( FaPID != -1 )   Aux_Error( ERROR_INFO, "Lv %d, PID %d, FaPID %d < 0 but != -1 !!\n", lv, PID, FaPID );
+#            endif
+                YT_Grids[LID_Patch].parent_id = -1;
+             }
+             else if ( FaPID < (amr->NPatchComma[FaLv][1]) ){
+                // father patch is a real patch
+                YT_Grids[LID_Patch].parent_id = FaPID + GID_Offset[FaLv];
+             }
+             else{
+                // father patch is a buffer patch (only possible in LOAD_BALANCE)
+#            ifdef DEBUG_HDF5
+#            ifndef LOAD_BALANCE
+                Aux_Error( ERROR_INFO, "Lv %d, PID %d, FaPID %d >= NRealFaPatch %d (only possible in LOAD_BALANCE) !!\n",
+                           lv, PID, FaPID, amr->NPatchComma[FaLv][1] );
+#            endif
+                if ( FaPID >= (amr->num[FaLv]) ){
+                    Aux_Error( ERROR_INFO, "Lv %d, PID %d, FaPID %d >= total number of patches %d !!\n",
+                               lv, PID, FaPID, amr->num[FaLv] );
+                }
+#            endif // DEBUG_HDF5
+
+                long FaLBIdx = amr->patch[0][FaLv][FaPID]->LB_Idx;
+                int MatchIdx;
+                Mis_Matching_int( NPatchTotal[FaLv], LBIdxList_Sort[FaLv], 1, &FaLBIdx, &MatchIdx );
+
+#            ifdef DEBUG_HDF5
+                if ( MatchIdx < 0 ){
+                    Aux_Error( ERROR_INFO, "Lv %d, PID %d, FaPID %d, FaLBIdx %ld, couldn't find a matching patch !!\n",
+                               lv, PID, FaPID, FaLBIdx );
+                }
+#            endif
+
+                YT_Grids[LID_Patch].parent_id = LBIdxList_Sort_IdxTable[FaLv][MatchIdx] + GID_LvStart[FaLv];
+             }
+         }
+
+#        ifdef PARTICLE
+         // store this grid patches's gid, for later searching for particle data
+         amr->patch[0][lv][PID]->libyt_GID    = (long) GID / 8;
+#        endif
+         
+         LID = LID + 1;
+      }
+   }
+
+   // free resources
+   for (int lv=0; lv<NLEVEL; lv++)
+   {
+      delete [] LBIdxList_Local        [lv];
+      delete [] LBIdxList_Sort         [lv];
+      delete [] LBIdxList_Sort_IdxTable[lv];
+   }
+
+   if ( yt_commit_grids( ) != YT_SUCCESS )  Aux_Error( ERROR_INFO, "yt_commit_grids() failed !!\n" );
+
+   if ( OPT__VERBOSE  &&  MPI_Rank == 0 )    Aux_Message( stdout, "%s ... done\n", __FUNCTION__ );
+
+} // FUNCTION : YT_AddLocalGrid
+
+#else // #ifdef LIBYT_USE_PATCH_GROUP
+//-------------------------------------------------------------------------------------------------------
+// Function    :  YT_AddLocalGrid
+// Description :  Send the hierarchy information and data of local patches to libyt.
 //
 // Note        :  1. One must call YT_SetParameter() before invoking this function
 //                2. Invoked by YT_Inline()
@@ -206,8 +371,8 @@ void YT_AddLocalGrid( const int *GID_Offset, const int *GID_LvStart, const int (
 
    if ( OPT__VERBOSE  &&  MPI_Rank == 0 )    Aux_Message( stdout, "%s ... done\n", __FUNCTION__ );
 
-} // FUNCTION : YT_AddAllGrid
+} // FUNCTION : YT_AddLocalGrid
 
 
-
+#endif // #ifdef LIBYT_USE_PATCH_GROUP
 #endif // #ifdef SUPPORT_LIBYT
