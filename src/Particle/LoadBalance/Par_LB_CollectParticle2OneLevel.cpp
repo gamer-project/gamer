95f0df9b7b210c4720b839f62dd9ae7869af7152
Hsi-Yu Schive <hyschive@gmail.com>
2022-05-14 11:05:25 +0800

    No need to specify the target attributes in Par_CollectParticle2OneLevel() when adopting JustCountNPar_Yes

--- src/Particle/LoadBalance/Par_LB_CollectParticle2OneLevel.cpp
+++ src/Particle/LoadBalance/Par_LB_CollectParticle2OneLevel.cpp
@@ -82,6 +82,7 @@ void Par_LB_CollectParticle2OneLevel( const int FaLv, const long AttBitIdx, cons
 // --> PosSendIdx[] is used by Par_PredictPos()
    int NAtt=0, AttIntIdx[PAR_NATT_TOTAL], PosSendIdx[3]={-1, -1, -1};
 
+   if ( !JustCountNPar )
    for (int v=0; v<PAR_NATT_TOTAL; v++)
       if ( AttBitIdx & (1L<<v) )    AttIntIdx[ NAtt ++ ] = v;
 
