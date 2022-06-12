c6ad4094f3d383216407e70003db935b4631236e
Hsi-Yu Schive <hyschive@gmail.com>
2022-06-12 16:05:37 +0800

    Generalize the default SOR_MAX/MIN_ITER for an arbitrary PATCH_SIZE

--- src/SelfGravity/Init_Set_Default_SOR_Parameter.cpp
+++ src/SelfGravity/Init_Set_Default_SOR_Parameter.cpp
@@ -33,13 +33,14 @@ void Init_Set_Default_SOR_Parameter( double &SOR_Omega, int &SOR_Max_Iter, int &
 // reference to the optimum relaxation parameter: Yang & Gobbert, Appl. Math. Lett. 22, 325 (2009)
 // --> Eq. [1.3] and the last paragraph in Sec. 3
 // --> further confirmed with the test problem "Hydro/Gravity" using different PATCH_SIZE and POT_GHOST_SIZE
-   const double Default_Omega   = 2.0 / (  1.0 + sin( M_PI/(PS1+2*POT_GHOST_SIZE) )  );
+   const int    NCell           = PS1 + 2*POT_GHOST_SIZE;
+   const double Default_Omega   = 2.0 / ( 1.0 + sin(M_PI/NCell) );
 #  ifdef FLOAT8
-   const int    Default_MaxIter = 110;
+   const int    Default_MaxIter = int( 13 + 10*NCell );  // add a factor of 2 buffer to the empirically determined maximum iteration
 #  else
-   const int    Default_MaxIter = 60;
+   const int    Default_MaxIter = int( 40 + 3*NCell );   // add a factor of 2 buffer to the empirically determined maximum iteration
 #  endif
-   const int    Default_MinIter = 10;
+   const int    Default_MinIter = Default_MaxIter / 5;   // 20% of the maximum iteration (determined empirically)
 
    if ( SOR_Omega < 0.0 )
    {
