abb8d98b084ba474a8d0c5e0147c864876a466ed
Xerox16 <kunkel.alexander@protonmail.com>
2022-10-12 16:24:53 +0800

    Fix computation of velocity for hybrid scheme

--- src/Model_ELBDM/ELBDM_GetTimeStep_Velocity.cpp
+++ src/Model_ELBDM/ELBDM_GetTimeStep_Velocity.cpp
@@ -27,12 +27,10 @@ double ELBDM_GetTimeStep_Velocity( const int lv )
    real   MaxdS_dx;
    double dt;
 
-   double softenVelocity = 1e-6;
-
 
 // get the velocity dS/dx / dx as first derivative of phase
-   MaxdS_dx  = GetMaxVelocity( lv ) + softenVelocity;
-   //MaxdS_dx += (MaxdS_dx == 0) ? 1e-8 : 0;
+   MaxdS_dx  = GetMaxVelocity( lv );
+   MaxdS_dx += (MaxdS_dx == 0) ? 1e-8 : 0;
 
 // get the time-step
    dt = 1 / MaxdS_dx * 0.5 * ELBDM_ETA * DT__VELOCITY;
@@ -72,6 +70,7 @@ real GetMaxVelocity( const int lv )
    const int  NPG               = 1;               // number of patch groups (must be ONE here)
    const int  NCOMP1            = 1;               // we only need to retrieve phase for velocity criterion
    real (*Flu_Array)[NCOMP1][Size_Flu][Size_Flu][Size_Flu] = NULL;
+   real (*Wave_Patch_Array)[NCOMP1][Size_Flu][Size_Flu][Size_Flu] = NULL;
 
    real dS_dx, MaxdS_dx, _dh, _dh2, GradS[3];
    int im, ip, jm, jp, km, kp, I, J, K;
@@ -102,12 +101,6 @@ real GetMaxVelocity( const int lv )
 #     pragma omp for reduction( max:MaxdS_dx ) schedule( runtime )
       for (int PID0=0; PID0<amr->NPatchComma[lv][1]; PID0+=NPG*8)
       {
-         //skipPatchgroup = false;
-         //for (int LocalID=0; LocalID<8; LocalID++)
-         //{
-         //   skipPatchgroup |= ( amr->patch[0][lv][PID0 + LocalID]->use_wave_flag );
-         //}
-         //if (skipPatchgroup) continue;
          
 //       prepare phase with NGhost ghost zone on each side (any interpolation scheme can be used)
          Prepare_PatchData( lv, Time[lv], &Flu_Array[0][0][0][0][0], NULL, NGhost, NPG, &PID0, _PHAS, _NONE,
@@ -115,16 +108,63 @@ real GetMaxVelocity( const int lv )
                             MinDens_No, MinPres_No, MinTemp_No, MinEntr_No, DE_Consistency_No );
 
 
+
 //       evaluate dS_dt
          for (int k=NGhost; k<Size_Flu-NGhost; k++)    {  km = k - 1;    kp = k + 1;   K = k - NGhost;
          for (int j=NGhost; j<Size_Flu-NGhost; j++)    {  jm = j - 1;    jp = j + 1;   J = j - NGhost;
          for (int i=NGhost; i<Size_Flu-NGhost; i++)    {  im = i - 1;    ip = i + 1;   I = i - NGhost;
+
+//       skip velocities of cells that have a wave counterpart on the refined levels
+
+//       convert coordinates of cell to global integer coordinate system 
+         int coordinates[3] = {I, J, K};
+
+         for ( int l = 0; l < 3; ++l ) {
+            coordinates[l] *= amr->scale[lv];
+            coordinates[l] += amr->patch[0][lv][PID0]->corner[l];
+         }
+
+         int curLv  = lv - 1, curPID = PID0, sonPID = PID0; 
+         bool inside = true; 
+
+//       traverse the tree until we get to leave nodes
+         while ( sonPID != -1 ) {
+            curPID = sonPID; 
+            curLv += 1;
+
+//          loop over patches in patch block or sons and check which patch the cell {K, J, I} belongs to
+            for (int PID=curPID; PID<curPID+8; PID++)
+            {
+               inside = true; 
+
+//             check whether cell {I, J, K} is within given patch
+               for ( int l = 0; l < 3; ++l ) {
+                  if (coordinates[l] < amr->patch[0][curLv][PID]->corner[l] || coordinates[l] >=  amr->patch[0][curLv][PID]->corner[l] + PS1 * amr->scale[curLv])
+                     inside = false;
+               }
+
+//             if it is within the given patch, go to the patch's son
+               if ( inside ) { 
+                  curPID = PID;
+                  sonPID = amr->patch[0][curLv][curPID]->son;
+                  break;
+               }
+            }
+         }
+
+//       check whether leave node corresponding to cell uses phase scheme
+         if ( !amr->use_wave_flag[curLv] ) {
+
             GradS[0]   = _dh2 * ( Flu_Array[0][0][k ][j ][ip] - Flu_Array[0][0][k ][j ][im] );
             GradS[1]   = _dh2 * ( Flu_Array[0][0][k ][jp][i ] - Flu_Array[0][0][k ][jm][i ] );
             GradS[2]   = _dh2 * ( Flu_Array[0][0][kp][j ][i ] - Flu_Array[0][0][km][j ][i ] );
 
             dS_dx      = _dh * (FABS(GradS[0]) + FABS(GradS[1]) + FABS(GradS[2]));
             MaxdS_dx   = MAX( MaxdS_dx, dS_dx );
+
+         } else {
+            printf("No computation of phase velocity for K %d J %d I %d\n", coordinates[0], coordinates[1], coordinates[2]);
+         }
 
          }}} // i,j,k
       } // for (int PID0=0; PID0<amr->NPatchComma[lv][1]; PID0+=NPG*8)
