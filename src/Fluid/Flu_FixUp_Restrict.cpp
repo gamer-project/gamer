2381c904941f3d3116cebbf7124020ec7cbb6ad4
Xerox16 <kunkel.alexander@protonmail.com>
2022-12-20 14:06:32 +0800

    Use previous level phase field for backward matching

--- src/Fluid/Flu_FixUp_Restrict.cpp
+++ src/Fluid/Flu_FixUp_Restrict.cpp
@@ -186,8 +186,8 @@ void Flu_FixUp_Restrict( const int FaLv, const int SonFluSg, const int FaFluSg, 
                   real (*IFaPtr) [PS1][PS1] = amr->patch[ FaFluSg][ FaLv][ FaPID]->fluid[IMAG];
 
 #           if ( ELBDM_SCHEME == HYBRID )
-                  real (*PFaPtr)[PS1][PS1]  = amr->patch[  FaFluSg][ FaLv][ FaPID]->fluid[PHAS];
-                  real (*OldPFaPtr)[PS1][PS1]  = amr->patch[ FaFluSg][ FaLv][ FaPID]->fluid[PHAS];
+                  real (*PFaPtr)   [PS1][PS1]  = amr->patch[  FaFluSg][ FaLv][ FaPID]->fluid[PHAS];
+                  real (*OldPFaPtr)[PS1][PS1]  = amr->patch[1-FaFluSg][ FaLv][ FaPID]->fluid[PHAS];
 //                handle that we do not have data of previous time step during initialisation corresponding to a negative time
                   if ( amr->FluSgTime[FaLv][1-FaFluSg ] < 0 ) {
                      OldPFaPtr = PFaPtr;
