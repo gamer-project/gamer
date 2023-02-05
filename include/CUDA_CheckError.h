901e675847ded60884c071720ec393efed3bacd7
Hsi-Yu Schive <hyschive@gmail.com>
2023-02-05 21:44:02 +0800

    Reduce GPU_NSTREAM automatically when running out of GPU global memory

--- include/CUDA_CheckError.h
+++ include/CUDA_CheckError.h
@@ -20,4 +20,20 @@ inline void CUDA_Check_Error( cudaError Return, const char *File, const int Line
 
 
 
+// in CUDA_CHECK_MALLOC(), we must use "Call; cudaError_t Return = cudaGetLastError();" instead of "cudaError_t Return = Call;"
+// since cudaGetLastError() will reset the last error to cudaSuccess
+// --> otherwise CUDA_CHECK_ERROR( cudaGetLastError() ) in, for example, CUAPI_Asyn_FluidSolver(),
+//     will fail since the last error has not been reset!
+#define CUDA_CHECK_MALLOC( Call )                                                                        \
+{                                                                                                        \
+   Call;                                                                                                 \
+   const cudaError_t Return = cudaGetLastError();                                                        \
+   if      ( Return == cudaErrorMemoryAllocation )                                                       \
+      return GAMER_FAILED;                                                                               \
+   else if ( Return != cudaSuccess )                                                                     \
+      Aux_Error( ERROR_INFO, "CUDA ERROR in memory allocation : %s !!\n", cudaGetErrorString(Return) );  \
+}
+
+
+
 #endif // #ifndef __CUDA_CHECK_ERROR_H__
