f4636262da72a2f516ec138d78cd066620f00d29
KunkelAlexander <kunkel.alexander@protonmail.com>
2023-05-12 10:09:19 +0800

    Improve error message

--- src/Auxiliary/Aux_Check_Parameter.cpp
+++ src/Auxiliary/Aux_Check_Parameter.cpp
@@ -466,11 +466,10 @@ void Aux_Check_Parameter()
 #     error : ERROR : currently GAMER only supports "LOAD_BALANCE == HILBERT" !!
 #  endif
 
-
-
 // for sending fluid data fixed by coarse-fine fluxes correctly
    if ( OPT__FIXUP_FLUX  &&  Flu_ParaBuf >= PATCH_SIZE )
-      Aux_Error( ERROR_INFO, "\"%s\" is required for \"%s\" in LOAD_BALANCE --> check LB_RecordExchangeFixUpDataPatchID() !!\n",
+      Aux_Error( ERROR_INFO, "\"%s\" is required for \"%s\" in LOAD_BALANCE --> check LB_RecordExchangeFixUpDataPatchID() !!\n \
+                  For ( MODEL == ELBDM && WAVE_SCHEME == WAVE_GRAMFE ) consider turning OPT__FIXUP_FLUX off or increasing the patch size!! \n",
                  "Flu_ParaBuf < PATCH_SIZE", "OPT__FIXUP_FLUX" );
 
 // ensure that the variable "PaddedCr1D" will not overflow
