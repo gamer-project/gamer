f387f228772f6d22f2179ceee9e8a01998ec2027
ChunYen-Chen <chunyenchen2019@gmail.com>
2023-06-09 15:04:57 +0800

    Extend the print space, and showing the disabled options.

--- src/configure.py
+++ src/configure.py
@@ -286,21 +286,19 @@ def color_print( string, color ):
 def add_option( opt_str, name, val ):
     # NOTE: every -Doption must have a trailing space
     if type(val) == type(True):
-        if val:
-            opt_str += "-D%s "%(name)
-            print("%-20s : %r"%(name, val))
+        if val: opt_str += "-D%s "%(name)
+        print("%-25s : %r"%(name, val))
     elif type(val) == type("str"):
         if val != "":
             opt_str += "-D%s=%s "%(name, val)
-            print("%-20s : %s"%(name, val))
+            print("%-25s : %s"%(name, val))
     elif type(val) == type(0):
-        if val != 0:
-            opt_str += "-D%s=%d "%(name, val)
-            print("%-20s : %d"%(name, val))
+        if val != 0: opt_str += "-D%s=%d "%(name, val)
+        print("%-25s : %d"%(name, val))
     elif type(val) == type(0.):
-        if val != 0.0:
+        if val < 0.0:   # The condition might need to be changed
             opt_str += "-D%s=%f "%(name, val)
-            print("%-20s : %f"%(name, val))
+            print("%-25s : %f"%(name, val))
     else:
         raise TypeError("Unknown type to add the simulation options.")
 
