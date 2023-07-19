b1f9d91e1c8ab1f4ded970c02fa2e885375d5ba9
KunkelAlexander <kunkel.alexander@protonmail.com>
2023-07-19 15:59:29 +0800

    Minor

--- example/test_problem/ELBDM/LSS_Hybrid/generate_make.sh
+++ example/test_problem/ELBDM/LSS_Hybrid/generate_make.sh
@@ -3,5 +3,5 @@
 PYTHON=python
 
 ${PYTHON} configure.py --machine=eureka_intel --mpi=true --hdf5=true --fftw=FFTW3 --gpu=true \
-                       --model=ELBDM --elbdm_scheme=ELBDM_HYBRID --wave_scheme=WAVE_GRAMFE --wave_scheme=GRAMFE_MATMUL \
-                       --gravity=true --comoving=true --gsl=true --support_spectral_int=true
+                       --model=ELBDM --elbdm_scheme=ELBDM_HYBRID --wave_scheme=WAVE_GRAMFE --gramfe_scheme=GRAMFE_MATMUL \
+                       --gravity=true --comoving=true --gsl=true --spectral_int=true
