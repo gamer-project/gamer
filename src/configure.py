4ad36ca615d60b79584d92f54f94b1df78561b67
ChunYen-Chen <chunyenchen2019@gmail.com>
2023-08-20 14:31:45 +0800

    Add cosmic rays related configure options.

--- src/configure.py
+++ src/configure.py
@@ -360,12 +360,12 @@ def load_arguments():
     parser.add_argument( "--cosmic_ray", type=str2bool, metavar="BOOLEAN", gamer_name="COSMIC_RAY",
                          default=False,
                          depend={"model":"HYDRO"},
-                         constraint={ True:{"dual":[NONE_STR]} },
-                         help="Cosmic rays (not supported yet).\n"
+                         constraint={ True:{"dual":[NONE_STR], "eos":"COSMIC_RAY"} },
+                         help="Enable cosmic rays physics. Must use <--eos=COSMIC_RAY>.\n"
                        )
 
     parser.add_argument( "--eos", type=str, metavar="TYPE", gamer_name="EOS",
-                         default="GAMMA", choices=["GAMMA", "ISOTHERMAL", "NUCLEAR", "TABULAR", "USER"],
+                         default="GAMMA", choices=["GAMMA", "ISOTHERMAL", "NUCLEAR", "TABULAR", "COSMIC_RAY", "USER"],
                          depend={"model":"HYDRO"},
                          constraint={ "ISOTHERMAL":{"barotropic":True} },
                          help="Equation of state. Must be set when <--model=HYDRO>. Must enable <--barotropic> for ISOTHERMAL.\n"
@@ -471,6 +471,19 @@ def load_arguments():
                          default=False,
                          constraint={ True:{"model":"HYDRO", "eos":"GAMMA"} },
                          help="Enable Grackle, a chemistry and radiative cooling library. Must set <--passive> according to the primordial chemistry network set by GRACKLE_PRIMORDIAL. Please enable OpenMP when compiling Grackle (by 'make omp-on').\n"
+                       )
+
+    # A.6 microphysics
+    parser.add_argument( "--microphysics", type=str2bool, metavar="BOOLEAN", gamer_name="MICROPHYSICS",
+                         default=False,
+                         help="Enable microphysics, including cosmic rays diffusion.\n"
+                       )
+
+    parser.add_argument( "--cr_diffusion", type=str2bool, metavar="BOOLEAN", gamer_name="CR_DIFFUSION",
+                         default=False,
+                         depend={"microphysics":True},
+                         constraint={ True:{"cosmic_ray":True, "MHD":True} },
+                         help="Enable cosmic rays diffusion. Must enable <--mhd> and <--cosmic_ray>.\n"
                        )
 
     # B. miscellaneous options
@@ -710,10 +723,6 @@ def validation( paths, depends, constraints, **kwargs ):
 
         if kwargs["dual"] not in [NONE_STR, "DE_ENPY"]:
             color_print("ERROR: This dual energy form is not supported yet. Current: %s"%kwargs["dual"], BCOLOR.FAIL)
-            success = False
-
-        if kwargs["cosmic_ray"]:
-            color_print("ERROR: <--cosmic_ray> is not supported yet.", BCOLOR.FAIL)
             success = False
 
     elif kwargs["model"] == "ELBDM":
