d5bc9a17f667bc4d72389a1195a2b3d51c2cc3fa
ChunYen-Chen <chunyenchen2019@gmail.com>
2023-08-23 17:58:50 +0800

    Remove comment.

--- example/test_problem/Hydro/Cosmic_Ray_Diffusion/yt_slice.py
+++ example/test_problem/Hydro/Cosmic_Ray_Diffusion/yt_slice.py
@@ -4,7 +4,7 @@ import yt
 N = 11
 ana_obj = 'CRay'
 for i in range(N):
-    ds = yt.load('./Data_%06d'%i)  #gamer AMR
+    ds = yt.load('./Data_%06d'%i)
     p = yt.SlicePlot(ds, 'y', ana_obj, center=[0.5, 0.5, 0.5])
     p.set_log(ana_obj, False)
     p.set_zlim(ana_obj, zmin=0.0, zmax=1.5)
