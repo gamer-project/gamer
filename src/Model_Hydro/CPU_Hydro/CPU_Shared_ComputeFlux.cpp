4e07ca0c26058b1ae4f6d7ad152cff6716d8de65
ChunYen-Chen <chunyenchen2019@gmail.com>
2023-08-24 11:13:23 +0800

    Minor update.

--- src/Model_Hydro/CPU_Hydro/CPU_Shared_ComputeFlux.cpp
+++ src/Model_Hydro/CPU_Hydro/CPU_Shared_ComputeFlux.cpp
@@ -395,44 +395,43 @@ void Hydro_StoreFixFlux( const real g_FC_Flux[][NCOMP_TOTAL_PLUS_MAG][ CUBE(N_FC
 
    CGPU_LOOP( idx, SQR(PS2) )
    {
-      // index of the 2 transverse directions
+//    index of the 2 transverse directions
       const int store_idx_0 = idx % PS2;
       const int store_idx_1 = idx / PS2;
       const int flux_idx_0  = store_idx_0 + 1 - NSkip_T;
       const int flux_idx_1  = store_idx_1 + 1 - NSkip_T;
 
-      // flux index
-      // x-face
+//    flux index
+//    x-face
       const int idx_flux_0  = IDX321(          0, flux_idx_0, flux_idx_1, NFlux, NFlux );
       const int idx_flux_1  = IDX321(        PS1, flux_idx_0, flux_idx_1, NFlux, NFlux );
       const int idx_flux_2  = IDX321(        PS2, flux_idx_0, flux_idx_1, NFlux, NFlux );
-      // y-face
+//    y-face
       const int idx_flux_3  = IDX321( flux_idx_0,          0, flux_idx_1, NFlux, NFlux );
       const int idx_flux_4  = IDX321( flux_idx_0,        PS1, flux_idx_1, NFlux, NFlux );
       const int idx_flux_5  = IDX321( flux_idx_0,        PS2, flux_idx_1, NFlux, NFlux );
-      // z-face
+//    z-face
       const int idx_flux_6  = IDX321( flux_idx_0, flux_idx_1,          0, NFlux, NFlux );
       const int idx_flux_7  = IDX321( flux_idx_0, flux_idx_1,        PS1, NFlux, NFlux );
       const int idx_flux_8  = IDX321( flux_idx_0, flux_idx_1,        PS2, NFlux, NFlux );
 
-      // Store the flux
+//    store the flux
       for (int v=0; v<NCOMP_TOTAL; v++) {
-         // x-face
+//       x-face
          g_IntFlux[0][v][idx] = g_FC_Flux[0][v][idx_flux_0];
          g_IntFlux[1][v][idx] = g_FC_Flux[0][v][idx_flux_1];
          g_IntFlux[2][v][idx] = g_FC_Flux[0][v][idx_flux_2];
-         // y-face
+//       y-face
          g_IntFlux[3][v][idx] = g_FC_Flux[1][v][idx_flux_3];
          g_IntFlux[4][v][idx] = g_FC_Flux[1][v][idx_flux_4];
          g_IntFlux[5][v][idx] = g_FC_Flux[1][v][idx_flux_5];
-         // z-face
+//       z-face
          g_IntFlux[6][v][idx] = g_FC_Flux[2][v][idx_flux_6];
          g_IntFlux[7][v][idx] = g_FC_Flux[2][v][idx_flux_7];
          g_IntFlux[8][v][idx] = g_FC_Flux[2][v][idx_flux_8];
       }
 
    } // CGPU_LOOP( idx, SQR(PS2) )
-
 
 #  ifdef __CUDACC__
    __syncthreads();
