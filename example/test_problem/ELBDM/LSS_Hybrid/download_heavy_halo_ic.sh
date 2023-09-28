c2152a57179e09a2aacbce4b6c9ea5dcce3d96f7
Alexander Kunkel <kunkel.alexander@protonmail.com>
2023-09-28 15:29:33 +0200

    Update LSS_Hybrid test problem

--- example/test_problem/ELBDM/LSS_Hybrid/download_heavy_halo_ic.sh
+++ example/test_problem/ELBDM/LSS_Hybrid/download_heavy_halo_ic.sh
@@ -2,6 +2,6 @@ filename=Music_InitCondition_z99_L2.8_N0256_HeavyHalo
 link=http://use.yt/upload/4587e2e6
 
 curl ${link} -o ${filename}
-ln -s ${filename} UM_IC_wave
-python ELBDM_WAVE2HYBRID_IC.py -input UM_IC_wave -output UM_IC_hybrid -resolution 256
-ln -s UM_IC_hybrid UM_IC
+ln -sf ${filename} UM_IC_wave_heavy
+python elbdm_wave_to_hybrid.py -input UM_IC_wave_heavy -output UM_IC_hybrid_heavy -resolution 256
+ln -sf UM_IC_hybrid_heavy UM_IC
--- example/test_problem/ELBDM/LSS_Hybrid/download_light_halo_ic.sh
+++ example/test_problem/ELBDM/LSS_Hybrid/download_light_halo_ic.sh
@@ -3,7 +3,7 @@ link=http://use.yt/upload/a67d8dd1
 
 rm UM_IC*
 curl ${link} -o ${filename}
-python3 ELBDM_RESCALE_PERIODIC_IC.py -input ${filename} -output Music_InitCondition_z99_L2.8_N0256_LightHalo -n_in 64 -n_out 256
-ln -s Music_InitCondition_z99_L2.8_N0256_LightHalo UM_IC_wave
-python3 ELBDM_WAVE2HYBRID_IC.py -input UM_IC_wave -output UM_IC_hybrid -resolution 256
-ln -s UM_IC_hybrid UM_IC
+python3 elbdm_rescale_periodic_IC.py -input ${filename} -output Music_InitCondition_z99_L2.8_N0256_LightHalo -n_in 64 -n_out 256
+ln -sf Music_InitCondition_z99_L2.8_N0256_LightHalo UM_IC_wave_light
+python3 elbdm_wave_to_hybrid_IC.py -input UM_IC_wave_light -output UM_IC_hybrid_light -resolution 256
+ln -sf UM_IC_hybrid_light UM_IC
