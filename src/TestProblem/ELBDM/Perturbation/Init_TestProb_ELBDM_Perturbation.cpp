c36da55568757be5ebe022afe395982f50063c66
Alexander Kunkel <kunkel.alexander@protonmail.com>
2023-09-29 14:33:26 +0200

    Update default parameters

--- src/TestProblem/ELBDM/Perturbation/Init_TestProb_ELBDM_Perturbation.cpp
+++ src/TestProblem/ELBDM/Perturbation/Init_TestProb_ELBDM_Perturbation.cpp
@@ -132,7 +132,7 @@ void SetParameter()
 // (4) reset other general-purpose parameters
 //     --> a helper macro PRINT_WARNING is defined in TestProb.h
    const long   End_Step_Default = __INT_MAX__;
-   const double End_T_Default    = 1.0;
+   const double End_T_Default    = 2.0;
 
    if ( END_STEP < 0 ) {
       END_STEP = End_Step_Default;
