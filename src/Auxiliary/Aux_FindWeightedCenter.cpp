f3edb8a7eda7b33bcebe53292d2088514a6fa91b
Hsinhao <hsinaohuang@gmail.com>
2023-10-04 17:31:03 +0800

    Support iterating to find the weighted center in Aux_FindWeightedCenter()

--- src/Auxiliary/Aux_FindWeightedCenter.cpp
+++ src/Auxiliary/Aux_FindWeightedCenter.cpp
@@ -12,15 +12,20 @@
 //                where w is the weighting density field and r is a vector of the position coordinate.
 //                If weighting density field is the mass density, the weighted center is the center of mass.
 //
-// Parameter   :  WeightedCenter         : the coordinate of the weighted center to be returned
-//                Center_ref[]           : the coordinate of center of reference
+// Parameter   :  WeightedCenter         : Coordinate of the weighted center to be returned
+//                Center_ref[]           : Coordinate of center of reference
 //                MaxR                   : Maximum radius to specify the region to compute the weighted center
 //                MinWD                  : Minimum weighting density to specify the region to compute the weighted center
-//                WeightingDensityField  : the weighting density field used for compuation as the w(x,y,z) in the above Note
+//                WeightingDensityField  : Weighting density field used for compuation as the w(x,y,z) in the above Note
+//                TolErrR                : Maximum error of distance to tolerate during the iteration 
+//                NIterMax               : Maximum number of iterations to find the center of mass
+//                FinaldR                : Record of the dR in the last time of iteration
+//                FinalNIter             : Record of the total numer of iterations
 //
-// Return      :  WeightedCenter[]
+// Return      :  WeightedCenter[], FinaldR, FinalNIter
 //-------------------------------------------------------------------------------------------------------
-void Aux_FindWeightedCenter( double WeightedCenter[], const double Center_ref[], const double MaxR, const double MinWD, const long WeightingDensityField )
+void Aux_FindWeightedCenter( double WeightedCenter[], const double Center_ref[], const double MaxR, const double MinWD,
+                             const long WeightingDensityField, const double TolErrR, const int NIterMax, double *FinaldR, int *FinalNIter )
 {
 
 // check
@@ -44,27 +49,10 @@ void Aux_FindWeightedCenter( double WeightedCenter[], const double Center_ref[],
    }
 #  endif // #ifdef GAMER_DEBUG
 
-   const double MaxR2             = SQR( MaxR );
    const double HalfBox[3]        = { 0.5*amr->BoxSize[0], 0.5*amr->BoxSize[1], 0.5*amr->BoxSize[2] };
    const bool   Periodic[3]       = { OPT__BC_FLU[0] == BC_FLU_PERIODIC,
                                       OPT__BC_FLU[2] == BC_FLU_PERIODIC,
                                       OPT__BC_FLU[4] == BC_FLU_PERIODIC };
-
-// set the target region that takes into account periodic BC for excluding distant patches
-   double Center_Img[3], RegMax[3], RegMin[3], RegMax_Img[3], RegMin_Img[3];
-
-   for (int d=0; d<3; d++)
-   {
-      if ( Periodic[d] )
-      Center_Img[d] = ( Center_ref[d] > amr->BoxCenter[d] ) ? Center_ref[d]-amr->BoxSize[d] : Center_ref[d]+amr->BoxSize[d];
-      else
-      Center_Img[d] = Center_ref[d];
-
-      RegMax    [d] = Center_ref[d] + MaxR;
-      RegMin    [d] = Center_ref[d] - MaxR;
-      RegMax_Img[d] = Center_Img[d] + MaxR;
-      RegMin_Img[d] = Center_Img[d] - MaxR;
-   }
 
    const bool   IntPhase_No       = false;
    const real   MinDens_No        = -1.0;
@@ -85,138 +73,200 @@ void Aux_FindWeightedCenter( double WeightedCenter[], const double Center_ref[],
 #  endif
 #  endif // #ifdef PARTICLE
 
-   real (*WeightingDensity)[PS1][PS1][PS1];
-   int   *PID0List = NULL;
-   double W_ThisRank, WR_ThisRank[3], W_AllRank, WR_AllRank[3];
-   double WX_ThisRank, WY_ThisRank, WZ_ThisRank;
-
-   W_ThisRank  = 0.0;
-   WX_ThisRank = 0.0;
-   WY_ThisRank = 0.0;
-   WZ_ThisRank = 0.0;
+// initial the referenced center in the first iteration as the input Center_ref
+   const double MaxR2             = SQR( MaxR );
+   const double TolErrR2          = SQR( TolErrR );
+   double dR2, Center_ref_OldIter[3];
+   for (int d=0; d<3; d++) Center_ref_OldIter[d] = Center_ref[d];
+   int NIter = 0;
 
 
-// loop over all levels
-   for (int lv=0; lv<NLEVEL; lv++)
+// start the iteration to find the center until convergence
+   while ( true )
    {
-//    initialize the particle density array (rho_ext) and collect particles to the target level
-#     ifdef PARTICLE
-      Prepare_PatchData_InitParticleDensityArray( lv );
+//    set the target region that takes into account periodic BC for excluding distant patches
+      double Center_Img[3], RegMax[3], RegMin[3], RegMax_Img[3], RegMin_Img[3];
 
-      Par_CollectParticle2OneLevel( lv, _PAR_MASS|_PAR_POSX|_PAR_POSY|_PAR_POSZ|_PAR_TYPE, PredictParPos_No, NULL_REAL,
-                                    SibBufPatch, FaSibBufPatch, JustCountNPar_No, TimingSendPar_No );
-#     endif
-
-//    get the weighting density on grids
-      WeightingDensity = new real [ amr->NPatchComma[lv][1] ][PS1][PS1][PS1];
-      PID0List         = new int  [ amr->NPatchComma[lv][1]/8 ];
-
-      for (int PID0=0, t=0; PID0<amr->NPatchComma[lv][1]; PID0+=8, t++)    PID0List[t] = PID0;
-
-      Prepare_PatchData( lv, Time[lv], WeightingDensity[0][0][0], NULL, 0, amr->NPatchComma[lv][1]/8, PID0List, WeightingDensityField, _NONE,
-                         OPT__RHO_INT_SCHEME, INT_NONE, UNIT_PATCH, NSIDE_00, IntPhase_No, OPT__BC_FLU, BC_POT_NONE,
-                         MinDens_No, MinPres_No, MinTemp_No, MinEntr_No, DE_Consistency_No );
-
-      delete [] PID0List;
-
-
-//    free memory for collecting particles from other ranks and levels, and free density arrays with ghost zones (rho_ext)
-#     ifdef PARTICLE
-      Par_CollectParticle2OneLevel_FreeMemory( lv, SibBufPatch, FaSibBufPatch );
-
-      Prepare_PatchData_FreeParticleDensityArray( lv );
-#     endif
-
-
-//    calculate the weighted-average center
-      const double dh = amr->dh[lv];
-      const double dv = CUBE( dh );
-
-#     pragma omp parallel for reduction ( +:W_ThisRank, WX_ThisRank, WY_ThisRank, WZ_ThisRank ) schedule( runtime )
-      for (int PID=0; PID<amr->NPatchComma[lv][1]; PID++)
+      for (int d=0; d<3; d++)
       {
-//       skip non-leaf patches
-         if ( amr->patch[0][lv][PID]->son != -1 )  continue;
+         if ( Periodic[d] )
+         Center_Img[d] = ( Center_ref_OldIter[d] > amr->BoxCenter[d] ) ? Center_ref_OldIter[d]-amr->BoxSize[d] : Center_ref_OldIter[d]+amr->BoxSize[d];
+         else
+         Center_Img[d] = Center_ref_OldIter[d];
 
-//       skip distant patches
-         const double *EdgeL = amr->patch[0][lv][PID]->EdgeL;
-         const double *EdgeR = amr->patch[0][lv][PID]->EdgeR;
+         RegMax    [d] = Center_ref_OldIter[d] + MaxR;
+         RegMin    [d] = Center_ref_OldIter[d] - MaxR;
+         RegMax_Img[d] = Center_Img        [d] + MaxR;
+         RegMin_Img[d] = Center_Img        [d] - MaxR;
+      }
 
-         if (   (  ( EdgeL[0]>RegMax[0] || EdgeR[0]<RegMin[0] )  &&  ( EdgeL[0]>RegMax_Img[0] || EdgeR[0]<RegMin_Img[0] )  )   ||
-                (  ( EdgeL[1]>RegMax[1] || EdgeR[1]<RegMin[1] )  &&  ( EdgeL[1]>RegMax_Img[1] || EdgeR[1]<RegMin_Img[1] )  )   ||
-                (  ( EdgeL[2]>RegMax[2] || EdgeR[2]<RegMin[2] )  &&  ( EdgeL[2]>RegMax_Img[2] || EdgeR[2]<RegMin_Img[2] )  )    )
-            continue;
+      real (*WeightingDensity)[PS1][PS1][PS1];
+      int   *PID0List = NULL;
+      double W_ThisRank, WR_ThisRank[3], W_AllRank, WR_AllRank[3];
+      double WX_ThisRank, WY_ThisRank, WZ_ThisRank;
 
-
-//       loop over all cells
-         const double x0 = amr->patch[0][lv][PID]->EdgeL[0] + 0.5*dh;
-         const double y0 = amr->patch[0][lv][PID]->EdgeL[1] + 0.5*dh;
-         const double z0 = amr->patch[0][lv][PID]->EdgeL[2] + 0.5*dh;
-
-         for (int k=0; k<PS1; k++)  {  double z = z0 + k*dh;
-                                       double dz = z - Center_ref[2];
-                                       if ( Periodic[2] ) {
-                                          if      ( dz > +HalfBox[2] )  {  z -= amr->BoxSize[2];  dz -= amr->BoxSize[2];  }
-                                          else if ( dz < -HalfBox[2] )  {  z += amr->BoxSize[2];  dz += amr->BoxSize[2];  }
-                                       }
-         for (int j=0; j<PS1; j++)  {  double y = y0 + j*dh;
-                                       double dy = y - Center_ref[1];
-                                       if ( Periodic[1] ) {
-                                          if      ( dy > +HalfBox[1] )  {  y -= amr->BoxSize[1];  dy -= amr->BoxSize[1];  }
-                                          else if ( dy < -HalfBox[1] )  {  y += amr->BoxSize[1];  dy += amr->BoxSize[1];  }
-                                       }
-         for (int i=0; i<PS1; i++)  {  double x = x0 + i*dh;
-                                       double dx = x - Center_ref[0];
-                                       if ( Periodic[0] ) {
-                                          if      ( dx > +HalfBox[0] )  {  x -= amr->BoxSize[0];  dx -= amr->BoxSize[0];  }
-                                          else if ( dx < -HalfBox[0] )  {  x += amr->BoxSize[0];  dx += amr->BoxSize[0];  }
-                                       }
-
-            const double R2 = SQR(dx) + SQR(dy) + SQR(dz);
-            const double WD = WeightingDensity[PID][k][j][i];
-//          only include cells that are
-//          within a sphere with radius MaxR and with the weighting density larger than MinWD
-            if ( R2 < MaxR2  &&  WD > MinWD )
-            {
-               const double dw = WeightingDensity[PID][k][j][i]*dv; // weighting
-
-               W_ThisRank  += dw;
-               WX_ThisRank += dw*x;
-               WY_ThisRank += dw*y;
-               WZ_ThisRank += dw*z;
-            }
-         }}} // i,j,k
-      } // for (int PID=0; PID<amr->NPatchComma[lv][1]; PID++)
-
-      delete [] WeightingDensity;
-
-   } // for (int lv=0; lv<NLEVEL; lv++)
-
-   WR_ThisRank[0] = WX_ThisRank;
-   WR_ThisRank[1] = WY_ThisRank;
-   WR_ThisRank[2] = WZ_ThisRank;
+      W_ThisRank  = 0.0;
+      WX_ThisRank = 0.0;
+      WY_ThisRank = 0.0;
+      WZ_ThisRank = 0.0;
 
 
-// collect data from all ranks to calculate the weighted center
-// --> note that all ranks will get WeightedCenter[]
-   MPI_Allreduce( &W_ThisRank, &W_AllRank, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
-   MPI_Allreduce( WR_ThisRank, WR_AllRank, 3, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
-
-   for (int d=0; d<3; d++)    WeightedCenter[d] = WR_AllRank[d] / W_AllRank;
-
-// map the new center back to the simulation domain
-   for (int d=0; d<3; d++)
-   {
-      if ( Periodic[d] )
+//    loop over all levels
+      for (int lv=0; lv<NLEVEL; lv++)
       {
-         if      ( WeightedCenter[d] >= amr->BoxSize[d] )  WeightedCenter[d] -= amr->BoxSize[d];
-         else if ( WeightedCenter[d] < 0.0              )  WeightedCenter[d] += amr->BoxSize[d];
+//       initialize the particle density array (rho_ext) and collect particles to the target level
+#        ifdef PARTICLE
+         Prepare_PatchData_InitParticleDensityArray( lv );
+
+         Par_CollectParticle2OneLevel( lv, _PAR_MASS|_PAR_POSX|_PAR_POSY|_PAR_POSZ|_PAR_TYPE, PredictParPos_No, NULL_REAL,
+                                       SibBufPatch, FaSibBufPatch, JustCountNPar_No, TimingSendPar_No );
+#        endif
+
+//       get the weighting density on grids
+         WeightingDensity = new real [ amr->NPatchComma[lv][1] ][PS1][PS1][PS1];
+         PID0List         = new int  [ amr->NPatchComma[lv][1]/8 ];
+
+         for (int PID0=0, t=0; PID0<amr->NPatchComma[lv][1]; PID0+=8, t++)    PID0List[t] = PID0;
+
+         Prepare_PatchData( lv, Time[lv], WeightingDensity[0][0][0], NULL, 0, amr->NPatchComma[lv][1]/8, PID0List, WeightingDensityField, _NONE,
+                            OPT__RHO_INT_SCHEME, INT_NONE, UNIT_PATCH, NSIDE_00, IntPhase_No, OPT__BC_FLU, BC_POT_NONE,
+                            MinDens_No, MinPres_No, MinTemp_No, MinEntr_No, DE_Consistency_No );
+
+         delete [] PID0List;
+
+
+//       free memory for collecting particles from other ranks and levels, and free density arrays with ghost zones (rho_ext)
+#        ifdef PARTICLE
+         Par_CollectParticle2OneLevel_FreeMemory( lv, SibBufPatch, FaSibBufPatch );
+
+         Prepare_PatchData_FreeParticleDensityArray( lv );
+#        endif
+
+
+//       calculate the weighted-average center
+         const double dh = amr->dh[lv];
+         const double dv = CUBE( dh );
+
+#        pragma omp parallel for reduction ( +:W_ThisRank, WX_ThisRank, WY_ThisRank, WZ_ThisRank ) schedule( runtime )
+         for (int PID=0; PID<amr->NPatchComma[lv][1]; PID++)
+         {
+//          skip non-leaf patches
+            if ( amr->patch[0][lv][PID]->son != -1 )  continue;
+
+//          skip distant patches
+            const double *EdgeL = amr->patch[0][lv][PID]->EdgeL;
+            const double *EdgeR = amr->patch[0][lv][PID]->EdgeR;
+
+            if (   (  ( EdgeL[0]>RegMax[0] || EdgeR[0]<RegMin[0] )  &&  ( EdgeL[0]>RegMax_Img[0] || EdgeR[0]<RegMin_Img[0] )  )   ||
+                   (  ( EdgeL[1]>RegMax[1] || EdgeR[1]<RegMin[1] )  &&  ( EdgeL[1]>RegMax_Img[1] || EdgeR[1]<RegMin_Img[1] )  )   ||
+                   (  ( EdgeL[2]>RegMax[2] || EdgeR[2]<RegMin[2] )  &&  ( EdgeL[2]>RegMax_Img[2] || EdgeR[2]<RegMin_Img[2] )  )    )
+               continue;
+
+
+//          loop over all cells
+            const double x0 = amr->patch[0][lv][PID]->EdgeL[0] + 0.5*dh;
+            const double y0 = amr->patch[0][lv][PID]->EdgeL[1] + 0.5*dh;
+            const double z0 = amr->patch[0][lv][PID]->EdgeL[2] + 0.5*dh;
+
+            for (int k=0; k<PS1; k++)  {  double z = z0 + k*dh;
+                                          double dz = z - Center_ref_OldIter[2];
+                                          if ( Periodic[2] ) {
+                                             if      ( dz > +HalfBox[2] )  {  z -= amr->BoxSize[2];  dz -= amr->BoxSize[2];  }
+                                             else if ( dz < -HalfBox[2] )  {  z += amr->BoxSize[2];  dz += amr->BoxSize[2];  }
+                                          }
+            for (int j=0; j<PS1; j++)  {  double y = y0 + j*dh;
+                                          double dy = y - Center_ref_OldIter[1];
+                                          if ( Periodic[1] ) {
+                                             if      ( dy > +HalfBox[1] )  {  y -= amr->BoxSize[1];  dy -= amr->BoxSize[1];  }
+                                             else if ( dy < -HalfBox[1] )  {  y += amr->BoxSize[1];  dy += amr->BoxSize[1];  }
+                                          }
+            for (int i=0; i<PS1; i++)  {  double x = x0 + i*dh;
+                                          double dx = x - Center_ref_OldIter[0];
+                                          if ( Periodic[0] ) {
+                                             if      ( dx > +HalfBox[0] )  {  x -= amr->BoxSize[0];  dx -= amr->BoxSize[0];  }
+                                             else if ( dx < -HalfBox[0] )  {  x += amr->BoxSize[0];  dx += amr->BoxSize[0];  }
+                                          }
+
+               const double R2 = SQR(dx) + SQR(dy) + SQR(dz);
+               const double WD = WeightingDensity[PID][k][j][i];
+//             only include cells that are
+//             within a sphere with radius MaxR and with the weighting density larger than MinWD
+               if ( R2 < MaxR2  &&  WD > MinWD )
+               {
+                  const double dw = WeightingDensity[PID][k][j][i]*dv; // weighting
+
+                  W_ThisRank  += dw;
+                  WX_ThisRank += dw*x;
+                  WY_ThisRank += dw*y;
+                  WZ_ThisRank += dw*z;
+               }
+            }}} // i,j,k
+         } // for (int PID=0; PID<amr->NPatchComma[lv][1]; PID++)
+
+         delete [] WeightingDensity;
+
+      } // for (int lv=0; lv<NLEVEL; lv++)
+
+      WR_ThisRank[0] = WX_ThisRank;
+      WR_ThisRank[1] = WY_ThisRank;
+      WR_ThisRank[2] = WZ_ThisRank;
+
+
+//    collect data from all ranks to calculate the weighted center
+//    --> note that all ranks will get WeightedCenter[]
+      MPI_Allreduce( &W_ThisRank, &W_AllRank, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
+      MPI_Allreduce( WR_ThisRank, WR_AllRank, 3, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
+
+      for (int d=0; d<3; d++) WeightedCenter[d] = WR_AllRank[d] / W_AllRank;
+
+//    map the new center back to the simulation domain
+      for (int d=0; d<3; d++)
+      {
+         if ( Periodic[d] )
+         {
+            if      ( WeightedCenter[d] >= amr->BoxSize[d] ) WeightedCenter[d] -= amr->BoxSize[d];
+            else if ( WeightedCenter[d] < 0.0              ) WeightedCenter[d] += amr->BoxSize[d];
+         }
+
+      }
+
+      for (int d=0; d<3; d++)
+         if ( WeightedCenter[d] >= amr->BoxSize[d]  ||  WeightedCenter[d] < 0.0 )
+            Aux_Error( ERROR_INFO, "WeightedCenter[%d] = %14.7e lies outside the domain !!\n", d, WeightedCenter[d] );
+
+
+      dR2 = SQR( Center_ref_OldIter[0] - WeightedCenter[0] )
+          + SQR( Center_ref_OldIter[1] - WeightedCenter[1] )
+          + SQR( Center_ref_OldIter[2] - WeightedCenter[2] );
+      NIter++;
+
+//    check the convergence and number of iteration to decide whether to end the iteration
+      if ( dR2 <= TolErrR2  ||  NIter >= NIterMax )
+         break;
+      else
+//       use the weighted center as the refereced center in the next iteration
+         memcpy( Center_ref_OldIter, WeightedCenter, sizeof(double)*3 );
+
+
+      if ( W_AllRank == 0.0 )
+      {
+         if ( MPI_Rank == 0 )
+            Aux_Message( stderr, "WARNING : Weighted center cannot be found because the total weightging (W_AllRank) = %14.7e !!\n", W_AllRank );
+
+         break;
       }
 
    }
 
-   for (int d=0; d<3; d++)
-      if ( WeightedCenter[d] >= amr->BoxSize[d]  ||  WeightedCenter[d] < 0.0 )
-         Aux_Error( ERROR_INFO, "WeightedCenter[%d] = %14.7e lies outside the domain !!\n", d, WeightedCenter[d] );
+
+   if ( MPI_Rank == 0 )
+   {
+      if ( dR2 > TolErrR2 )
+         Aux_Message( stderr, "WARNING : dR (%13.7e) > TolErrR (%13.7e), the weighted center may not have converged yet when the iteration stopped !!\n", sqrt(dR2), TolErrR );
+   }
+
+// return the information about the iteration
+   *FinaldR    = sqrt(dR2);
+   *FinalNIter = NIter;
 
 } // FUNCTION : Aux_FindWeightedCenter
--- src/Auxiliary/Aux_Record_Center.cpp
+++ src/Auxiliary/Aux_Record_Center.cpp
@@ -21,9 +21,10 @@
 void Aux_Record_Center()
 {
 
-   const char filename_center  [] = "Record__Center";
+   const char FileName[] = "Record__Center";
+   static bool FirstTime = true;
 
-   // Maximum fluid density in HYDRO/ELBDM
+// 1. Maximum fluid density in HYDRO/ELBDM
    Extrema_t Max_Dens;
    Max_Dens.Field     = _DENS;
    Max_Dens.Radius    = HUGE_NUMBER; // entire domain
@@ -33,8 +34,9 @@ void Aux_Record_Center()
 
    Aux_FindExtrema( &Max_Dens, EXTREMA_MAX, 0, TOP_LEVEL, PATCH_LEAF );
 
+
 #  ifdef PARTICLE
-   // Maximum particle density
+// 2. Maximum particle density
    Extrema_t Max_ParDens;
    Max_ParDens.Field     = _PAR_DENS;
    Max_ParDens.Radius    = HUGE_NUMBER; // entire domain
@@ -44,7 +46,8 @@ void Aux_Record_Center()
 
    Aux_FindExtrema_ParDens( &Max_ParDens, EXTREMA_MAX, 0, TOP_LEVEL, PATCH_LEAF );
 
-   // Maximun total density including fluid density and particle density
+
+// 3. Maximun total density including fluid density and particle density
    Extrema_t Max_TotDens;
    Max_TotDens.Field     = _TOTAL_DENS;
    Max_TotDens.Radius    = HUGE_NUMBER; // entire domain
@@ -55,7 +58,8 @@ void Aux_Record_Center()
    Aux_FindExtrema_ParDens( &Max_TotDens, EXTREMA_MAX, 0, TOP_LEVEL, PATCH_LEAF );
 #  endif
 
-   // Minimun gravitational potential
+
+// 4. Minimun gravitational potential
    Extrema_t Min_Pote;
    Min_Pote.Field     = _POTE;
    Min_Pote.Radius    = HUGE_NUMBER; // entire domain
@@ -66,104 +70,78 @@ void Aux_Record_Center()
    Aux_FindExtrema_ParDens( &Min_Pote, EXTREMA_MIN, 0, TOP_LEVEL, PATCH_LEAF );
 
 
-// compute the center of mass until convergence
-   const double TolErrR2 = SQR( COM_TOL_ERR_R );
-
-   double dR2, CoM_Old[3], CoM_New[3];
-   int NIter = 0;
-
+// 5. Center of mass for the total density field
 // set an initial guess by the peak density position or the user-specified center
-   if ( MPI_Rank == 0 )
+   double CoM_ref[3];
+   if ( COM_CEN_X < 0.0  ||  COM_CEN_Y < 0.0  ||  COM_CEN_Z < 0.0 )
    {
-      if ( COM_CEN_X < 0  ||  COM_CEN_Y < 0  ||  COM_CEN_Z < 0 )
-      {
-#        ifdef PARTICLE
-         for (int d=0; d<3; d++) CoM_Old[d] = Max_TotDens.Coord[d];
-#        else
-         for (int d=0; d<3; d++) CoM_Old[d] = Max_Dens.Coord[d];
-#        endif
-      }
-      else
-      {
-         CoM_Old[0] = COM_CEN_X;
-         CoM_Old[1] = COM_CEN_Y;
-         CoM_Old[2] = COM_CEN_Z;
-      }
+#     ifdef PARTICLE
+      for (int d=0; d<3; d++) CoM_ref[d] = Max_TotDens.Coord[d];
+#     else
+      for (int d=0; d<3; d++) CoM_ref[d] = Max_Dens.Coord[d];
+#     endif
+   }
+   else
+   {
+      CoM_ref[0] = COM_CEN_X;
+      CoM_ref[1] = COM_CEN_Y;
+      CoM_ref[2] = COM_CEN_Z;
    }
 
-   MPI_Bcast( CoM_Old, 3, MPI_DOUBLE, 0, MPI_COMM_WORLD );
+// find the center of mass
+   double CoM_Coord[3];
+   double FinaldR;
+   int    FinalNIter;
+   Aux_FindWeightedCenter( CoM_Coord, CoM_ref, COM_MAX_R, COM_MIN_RHO, _TOTAL_DENS, COM_TOL_ERR_R, COM_N_ITER_MAX, &FinaldR, &FinalNIter );
 
-   while ( true )
-   {
-      Aux_FindWeightedCenter( CoM_New, CoM_Old, COM_MAX_R, COM_MIN_RHO, _TOTAL_DENS );
 
-      dR2 = SQR( CoM_Old[0] - CoM_New[0] )
-          + SQR( CoM_Old[1] - CoM_New[1] )
-          + SQR( CoM_Old[2] - CoM_New[2] );
-      NIter++;
-
-      if ( dR2 <= TolErrR2  ||  NIter >= COM_N_ITER_MAX )
-         break;
-      else
-         memcpy( CoM_Old, CoM_New, sizeof(double)*3 );
-   }
-
+// Output the center to file
    if ( MPI_Rank == 0 )
    {
-      if ( dR2 > TolErrR2 )
-         Aux_Message( stderr, "WARNING : dR (%13.7e) > COM_TOL_ERR_R (%13.7e) !!\n", sqrt(dR2), COM_TOL_ERR_R );
-   }
-
-
-   // Output the center to file
-   if ( MPI_Rank == 0 )
-   {
-
-      static bool FirstTime = true;
-
+//    Output the header
       if ( FirstTime )
       {
-         if ( Aux_CheckFileExist(filename_center) )
-            Aux_Message( stderr, "WARNING : file \"%s\" already exists !!\n", filename_center );
+         if ( Aux_CheckFileExist(FileName) )
+            Aux_Message( stderr, "WARNING : file \"%s\" already exists !!\n", FileName );
          else
          {
-            FILE *file_center = fopen( filename_center, "w" );
-            fprintf( file_center, "#%19s  %10s", "Time", "Step" );
-            fprintf( file_center, "  %14s  %14s  %14s  %14s",
-                     "MaxDens", "MaxDens_x", "MaxDens_y", "MaxDens_z" );
+            FILE *File = fopen( FileName, "w" );
+            fprintf( File, "#%19s  %10s", "Time", "Step" );
+            fprintf( File, "  %14s  %14s  %14s  %14s",
+                           "MaxDens", "MaxDens_x", "MaxDens_y", "MaxDens_z" );
 #           ifdef PARTICLE
-            fprintf( file_center, "  %14s  %14s  %14s  %14s",
-                     "MaxParDens", "MaxParDens_x", "MaxParDens_y", "MaxParDens_z" );
-            fprintf( file_center, "  %14s  %14s  %14s  %14s",
-                     "MaxTotalDens", "MaxTotalDens_x", "MaxTotalDens_y", "MaxTotalDens_z" );
+            fprintf( File, "  %14s  %14s  %14s  %14s",
+                           "MaxParDens", "MaxParDens_x", "MaxParDens_y", "MaxParDens_z" );
+            fprintf( File, "  %14s  %14s  %14s  %14s",
+                           "MaxTotalDens", "MaxTotalDens_x", "MaxTotalDens_y", "MaxTotalDens_z" );
 #           endif
-            fprintf( file_center, "  %14s  %14s  %14s  %14s",
-                     "MinPote", "MinPote_x", "MinPote_y", "MinPote_z" );
-            fprintf( file_center, "  %10s  %14s  %14s  %14s",
-                     "NIter", "CoM_x", "CoM_y", "CoM_z" );
-            fprintf( file_center, "\n" );
-            fclose( file_center );
+            fprintf( File, "  %14s  %14s  %14s  %14s",
+                           "MinPote", "MinPote_x", "MinPote_y", "MinPote_z" );
+            fprintf( File, "  %14s  %14s  %14s  %14s  %14s",
+                           "Final_NIter", "Final_dR", "CoM_x", "CoM_y", "CoM_z" );
+            fprintf( File, "\n" );
+            fclose( File );
          }
 
          FirstTime = false;
       }
 
-      FILE *file_center = fopen( filename_center, "a" );
-      fprintf( file_center, "%20.14e  %10ld", Time[0], Step );
-      fprintf( file_center, "  %14.7e  %14.7e  %14.7e  %14.7e",
-               Max_Dens.Value, Max_Dens.Coord[0], Max_Dens.Coord[1], Max_Dens.Coord[2] );
+      FILE *File = fopen( FileName, "a" );
+      fprintf( File, "%20.14e  %10ld", Time[0], Step );
+      fprintf( File, "  %14.7e  %14.7e  %14.7e  %14.7e",
+                     Max_Dens.Value, Max_Dens.Coord[0], Max_Dens.Coord[1], Max_Dens.Coord[2] );
 #     ifdef PARTICLE
-      fprintf( file_center, "  %14.7e  %14.7e  %14.7e  %14.7e",
-               Max_ParDens.Value, Max_ParDens.Coord[0], Max_ParDens.Coord[1], Max_ParDens.Coord[2] );
-      fprintf( file_center, "  %14.7e  %14.7e  %14.7e  %14.7e",
-               Max_TotDens.Value, Max_TotDens.Coord[0], Max_TotDens.Coord[1], Max_TotDens.Coord[2] );
+      fprintf( File, "  %14.7e  %14.7e  %14.7e  %14.7e",
+                     Max_ParDens.Value, Max_ParDens.Coord[0], Max_ParDens.Coord[1], Max_ParDens.Coord[2] );
+      fprintf( File, "  %14.7e  %14.7e  %14.7e  %14.7e",
+                     Max_TotDens.Value, Max_TotDens.Coord[0], Max_TotDens.Coord[1], Max_TotDens.Coord[2] );
 #     endif
-      fprintf( file_center, "  %14.7e  %14.7e  %14.7e  %14.7e",
-               Min_Pote.Value, Min_Pote.Coord[0], Min_Pote.Coord[1], Min_Pote.Coord[2] );
-      fprintf( file_center, "  %10d  %14.7e  %14.7e  %14.7e",
-               NIter, CoM_New[0], CoM_New[1], CoM_New[2] );
-      fprintf( file_center, "\n" );
-      fclose( file_center );
+      fprintf( File, "  %14.7e  %14.7e  %14.7e  %14.7e",
+                     Min_Pote.Value, Min_Pote.Coord[0], Min_Pote.Coord[1], Min_Pote.Coord[2] );
+      fprintf( File, "  %14d  %14.7e  %14.7e  %14.7e  %14.7e",
+                     FinalNIter, FinaldR, CoM_Coord[0], CoM_Coord[1], CoM_Coord[2] );
+      fprintf( File, "\n" );
+      fclose( File );
    }
 
 } // FUNCTION : Aux_Record_Center
