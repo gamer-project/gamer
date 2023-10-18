e45ac06021d55c73e4eddb1d5c7eb8ec9d8c806f
Hsi-Yu Schive <hyschive@gmail.com>
2023-10-18 10:50:45 +0800

    Minor

--- src/Output/Output_DumpData_Total_HDF5.cpp
+++ src/Output/Output_DumpData_Total_HDF5.cpp
@@ -242,7 +242,7 @@ Procedure for outputting new variables:
 //                2467 : 2023/05/18 --> replace OPT__INIT_BFIELD_BYFILE by OPT__INIT_BFIELD_BYVECPOT
 //                2468 : 2023/06/24 --> output OPT__SORT_PATCH_BY_LBIDX
 //                2469 : 2023/09/09 --> output MHM_CHECK_PREDICT
-//                2470 : 2023/10/** --> output OPT__OUTPUT_TEXT_FORMAT_FLT 
+//                2470 : 2023/10/16 --> output OPT__OUTPUT_TEXT_FORMAT_FLT
 //-------------------------------------------------------------------------------------------------------
 void Output_DumpData_Total_HDF5( const char *FileName )
 {
