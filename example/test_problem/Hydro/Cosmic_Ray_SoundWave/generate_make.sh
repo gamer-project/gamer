933d63cfa50eed72997bfd09ea891edbaad48239
ChunYen-Chen <chunyenchen2019@gmail.com>
2023-10-25 09:12:33 +0800

    Fix the options.

--- example/test_problem/Hydro/Cosmic_Ray_SoundWave/generate_make.sh
+++ example/test_problem/Hydro/Cosmic_Ray_SoundWave/generate_make.sh
@@ -2,5 +2,7 @@
 
 PYTHON=python
 
-${PYTHON} configure.py --machine=eureka_intel --model=HYDRO --flu_scheme=MHM_RP --flux=HLLD --double=true \
-                       --cosmic_ray=true --eos=COSMIC_RAY
+${PYTHON} configure.py --machine=eureka_intel --model=HYDRO --flu_scheme=MHM_RP --mhd=false --flux=HLLE \
+                       --double=true --cosmic_ray=true --eos=COSMIC_RAY
+#${PYTHON} configure.py --machine=eureka_intel --model=HYDRO --flu_scheme=MHM_RP --mhd=true --flux=HLLD \
+#                       --double=true --cosmic_ray=true --eos=COSMIC_RAY
