8b3bdf484bd1d60f69948469c26fee2317293e22
ChunYen-Chen <chunyenchen2019@gmail.com>
2023-10-25 13:41:59 +0800

    Minor

--- example/test_problem/Hydro/Cosmic_Ray_Diffusion/generate_make.sh
+++ example/test_problem/Hydro/Cosmic_Ray_Diffusion/generate_make.sh
@@ -3,4 +3,4 @@
 PYTHON=python
 
 ${PYTHON} configure.py --machine=eureka_intel --model=HYDRO --flu_scheme=MHM_RP --flux=HLLD --mhd=true \
-                       --microphysics=true --cosmic_ray=true --eos=COSMIC_RAY --cr_diffusion=true
+                       --microphysics=true --cosmic_ray=true --eos=COSMIC_RAY --cr_diffusion=true --hdf5=true
