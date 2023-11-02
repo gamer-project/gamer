a39c811f3df807b08a6ed9a617a035d20d087e37
ChunYen-Chen <chunyenchen2019@gmail.com>
2023-11-02 09:42:32 +0800

    Update Microphysics.h

--- include/Microphysics.h
+++ include/Microphysics.h
@@ -12,9 +12,9 @@
 // Structure   :  MicroPhy_t
 // Description :  Data structure storing the microphysics variables (e.g. diffusion, heat condution) to be passed to the CPU/GPU solvers
 //
-// Data Member :  safety          :
-//                diff_coeff_para : Diffusion coefficient of cosmic ray along magnetic field.
-//                diff_coeff_perp : Diffusion coefficient of cosmic ray perpendicular to magnetic field.
+// Data Member :  CR_safety          : The CFL safety factor of cosmic-ray
+//                CR_diff_coeff_para : Diffusion coefficient of cosmic-ray parallel to the magnetic field direction.
+//                CR_diff_coeff_perp : Diffusion coefficient of cosmic-ray perpendicular to the magnetic field direction.
 //
 // Method      :  None --> It seems that CUDA does not support functions in a struct
 //-------------------------------------------------------------------------------------------------------
@@ -22,9 +22,9 @@ struct MicroPhy_t
 {
 
 #  ifdef CR_DIFFUSION
-   double CR_safety;
-   double CR_diff_coeff_para;
-   double CR_diff_coeff_perp;
+   real CR_safety;
+   real CR_diff_coeff_para;
+   real CR_diff_coeff_perp;
 #  endif
 
 }; // struct MicroPhy_t
