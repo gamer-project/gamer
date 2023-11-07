23f4f328bf18dcba4a18b8ed69bfcba7d0a094d9
Alexander Kunkel <kunkel.alexander@protonmail.com>
2023-11-07 15:22:50 +0100

    Replace python by python3

--- example/test_problem/ELBDM/Perturbation/generate_make_BaseSpectral.sh
+++ example/test_problem/ELBDM/Perturbation/generate_make_BaseSpectral.sh
@@ -1,6 +1,6 @@
 # This script should run in the same directory as configure.py
 
-PYTHON=python
+PYTHON=python3
 
 ${PYTHON} configure.py --machine=eureka_intel --mpi=true --hdf5=true --fftw=FFTW3 \
                        --model=ELBDM --elbdm_scheme=ELBDM_WAVE \
