8cdf009c050b3db044d12cf70b3e25c0ac9df103
Hsi-Yu Schive <hyschive@gmail.com>
2023-11-11 20:37:49 +0800

    Fix minor issues reported by #hfhsieh

--- src/Output/Output_DumpData_Total_HDF5.cpp
+++ src/Output/Output_DumpData_Total_HDF5.cpp
@@ -69,7 +69,7 @@ Procedure for outputting new variables:
 
 
 //-------------------------------------------------------------------------------------------------------
-// Function    :  Output_DumpData_Total_HDF5 (FormatVersion = 2470)
+// Function    :  Output_DumpData_Total_HDF5 (FormatVersion = 2471)
 // Description :  Output all simulation data in the HDF5 format, which can be used as a restart file
 //                or loaded by YT
 //
@@ -1417,7 +1417,7 @@ void FillIn_KeyInfo( KeyInfo_t &KeyInfo, const int NFieldStored )
 
    const time_t CalTime = time( NULL );   // calendar time
 
-   KeyInfo.FormatVersion        = 2470;
+   KeyInfo.FormatVersion        = 2471;
    KeyInfo.Model                = MODEL;
    KeyInfo.NLevel               = NLEVEL;
    KeyInfo.NCompFluid           = NCOMP_FLUID;
