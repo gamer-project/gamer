b00b2e86e50a11b171ae4af143b882c8fedca737
Hsi-Yu Schive <hyschive@gmail.com>
2024-01-31 15:47:47 +0800

    Remove the warning of performing temporal interpolation in Prepare_PatchData()

--- src/Main/Prepare_PatchData.cpp
+++ src/Main/Prepare_PatchData.cpp
@@ -480,13 +480,6 @@ void Prepare_PatchData( const int lv, const double PrepTime, real *OutputCC, rea
       const int Sg0 = amr->FluSg[lv];
       SetTempIntPara( lv, Sg0, PrepTime, amr->FluSgTime[lv][Sg0], amr->FluSgTime[lv][1-Sg0],
                       FluIntTime, FluSg, FluSg_IntT, FluWeighting, FluWeighting_IntT );
-
-//    check: although temporal interpolation is allowed, currently PrepTime is expected to be equal to either
-//           amr->FluSgTime[lv][0] or amr->FluSgTime[lv][1]
-      if ( FluIntTime  &&  MPI_Rank == 0 )
-         Aux_Message( stderr, "WARNING : cannot determine FluSg "
-                              "(lv %d, PrepTime %20.14e, SgTime[0] %20.14e, SgTime[1] %20.14e) !!\n",
-                      lv, PrepTime, amr->FluSgTime[lv][0], amr->FluSgTime[lv][1] );
    }
 
 // magnetic field
@@ -501,13 +494,6 @@ void Prepare_PatchData( const int lv, const double PrepTime, real *OutputCC, rea
       const int Sg0 = amr->MagSg[lv];
       SetTempIntPara( lv, Sg0, PrepTime, amr->MagSgTime[lv][Sg0], amr->MagSgTime[lv][1-Sg0],
                       MagIntTime, MagSg, MagSg_IntT, MagWeighting, MagWeighting_IntT );
-
-//    check: although temporal interpolation is allowed, currently PrepTime is expected to be equal to either
-//           amr->MagSgTime[lv][0] or amr->MagSgTime[lv][1]
-      if ( MagIntTime  &&  MPI_Rank == 0 )
-         Aux_Message( stderr, "WARNING : cannot determine MagSg "
-                              "(lv %d, PrepTime %20.14e, SgTime[0] %20.14e, SgTime[1] %20.14e) !!\n",
-                      lv, PrepTime, amr->MagSgTime[lv][0], amr->MagSgTime[lv][1] );
    }
 #  endif // #ifdef MHD
 
@@ -522,18 +508,6 @@ void Prepare_PatchData( const int lv, const double PrepTime, real *OutputCC, rea
       const int Sg0 = amr->PotSg[lv];
       SetTempIntPara( lv, Sg0, PrepTime, amr->PotSgTime[lv][Sg0], amr->PotSgTime[lv][1-Sg0],
                       PotIntTime, PotSg, PotSg_IntT, PotWeighting, PotWeighting_IntT );
-
-//    check: although temporal interpolation is allowed, currently PrepTime is expected to be equal to either
-//           amr->PotSgTime[lv][0] or amr->PotSgTime[lv][1]
-//           --> the only exception is when calling Par_UpdateParticle() to prepare the coarse-grid potential
-//               for correcting the velocity of particles just crossing from fine to coarse grids
-#     ifdef PARTICLE
-      if ( amr->Par->ImproveAcc )
-#     endif
-      if ( PotIntTime  &&  MPI_Rank == 0 )
-         Aux_Message( stderr, "WARNING : cannot determine PotSg "
-                              "(lv %d, PrepTime %20.14e, SgTime[0] %20.14e, SgTime[1] %20.14e) !!\n",
-                      lv, PrepTime, amr->PotSgTime[lv][0], amr->PotSgTime[lv][1] );
    }
 #  endif // #ifdef GRAVITY
 
