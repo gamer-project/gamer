7f29804369bfcf682e10b3720e7770d27f712e70
koarakawaii <arahaguojnestore@gmail.com>
2024-04-06 10:46:33 +0800

    resume public version of configure.py

--- src/configure.py
+++ src/configure.py
@@ -27,8 +27,7 @@ PRINT_WIDTH = 100
 
 GAMER_CONFIG_DIR  = os.path.join("..", "configs")
 GAMER_MAKE_BASE   = "Makefile_base"
-#GAMER_MAKE_OUT    = "Makefile"
-GAMER_MAKE_OUT    = "Makefile_hydro_with_particle"
+GAMER_MAKE_OUT    = "Makefile"
 GAMER_DESCRIPTION = "Prepare a customized Makefile for GAMER.\nDefault values are marked by '*'.\nUse -lh to show a detailed help message.\n"
 GAMER_EPILOG      = "2023 Computational Astrophysics Lab, NTU. All rights reserved.\n"
 
