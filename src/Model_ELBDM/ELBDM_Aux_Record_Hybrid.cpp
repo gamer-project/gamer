4aa6f9b8e66715d2f786767a4a0e0d9d507f68d7
Hsi-Yu Schive <hyschive@gmail.com>
2024-04-22 14:50:24 +0800

    Polish Record__Hybrid

--- src/Model_ELBDM/ELBDM_Aux_Record_Hybrid.cpp
+++ src/Model_ELBDM/ELBDM_Aux_Record_Hybrid.cpp
@@ -7,7 +7,7 @@
 
 //-------------------------------------------------------------------------------------------------------
 // Function    :  ELBDM_Aux_Record_Hybrid
-// Description :  Record ratio of number of wave patches to total patches and fraction of simulation volume
+// Description :  Record the ratio of number of wave patches to total patches and the fraction of simulation volume
 //                using wave solver
 //
 // Return      :  Log file "Record__Hybrid"
@@ -15,32 +15,35 @@
 void ELBDM_Aux_Record_Hybrid()
 {
 
-   if ( MPI_Rank != 0 ) return;
+   if ( MPI_Rank != 0 )    return;
+
 
    static bool FirstTime = true;
    const char *FileName  = "Record__Hybrid";
-
+   FILE *File = NULL;
 
    if ( FirstTime )
    {
       if ( Aux_CheckFileExist(FileName) )
          Aux_Message( stderr, "WARNING : file \"%s\" already exists !!\n", FileName );
+
+      File = fopen( FileName, "a" );
+      fprintf( File, "#%13s  %8s  %10s  %9s  %17s  %17s\n", "Time", "Step", "NPatch", "WaveLevel", "NWavePatch/NPatch", "WaveVolume/Volume" );
+      fclose( File );
    }
 
 
-// compute number of wave patches on all levels
+// compute the number of wave patches on all levels
    long WavePatchCount  = 0;
    long TotalPatchCount = 0;
    int  WaveLevel       = -1;
 
    for (int lv=0; lv<NLEVEL; lv++)
    {
-      const real dv     = CUBE( amr->dh[lv] );
       const long NPatch = NPatchTotal[lv];
 
       if ( amr->use_wave_flag[lv] ) {
-//       store first level using wave scheme in WaveLevel
-         if ( WaveLevel == -1 ) WaveLevel = lv;
+         if ( WaveLevel == -1 )  WaveLevel = lv;   // store the first level using wave scheme
          WavePatchCount += NPatch;
       }
 
@@ -48,18 +51,15 @@ void ELBDM_Aux_Record_Hybrid()
    }
 
 
-   const real TotalVolume = NPatchTotal[0] * CUBE( amr->dh[0] );
-   const real WaveVolume  = (WaveLevel == -1) ? 0.0 : NPatchTotal[WaveLevel] * CUBE( amr->dh[WaveLevel] );
+// output to file
+   const double TotalVolume = NPatchTotal[0] * CUBE( amr->dh[0] );
+   const double WaveVolume  = ( WaveLevel == -1 ) ? 0.0 : NPatchTotal[WaveLevel] * CUBE( amr->dh[WaveLevel] );
 
-   // output to file
-   FILE *File = fopen( FileName, "a" );
-   if ( WaveLevel == -1 )
-      fprintf( File, "Time = %13.7e,  Step = %7ld,  NPatch = %10ld,  No wave scheme used\n",
-               Time[0], Step, TotalPatchCount );
-   else
-      fprintf( File, "Time = %13.7e,  Step = %7ld,  NPatch = %10ld,  Wave scheme on lv %2d,  NWavePatch/NPatch = %6.2f,  WaveVolume/Volume = %6.2f\n",
-               Time[0], Step, TotalPatchCount, WaveLevel, (real)WavePatchCount / (real)TotalPatchCount, WaveVolume / TotalVolume );
+   File = fopen( FileName, "a" );
+   fprintf( File, "%14.7e  %8ld  %10ld  %9d  %17.4f  %17.4f\n",
+            Time[0], Step, TotalPatchCount, WaveLevel, (double)WavePatchCount/(double)TotalPatchCount, WaveVolume/TotalVolume );
    fclose( File );
+
 
    if ( FirstTime )  FirstTime = false;
 
