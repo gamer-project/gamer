443ad10c180eb3c85de11a97f0e60a13bcc902ba
ChunYen-Chen <chunyenchen2019@gmail.com>
2024-05-13 15:48:00 +0800

    Fix typo, rename function and parameters.

--- tool/simulation/change_parameters.py
+++ tool/simulation/change_parameters.py
@@ -1,8 +1,9 @@
+#!/bin/python3
 """
-This is the small script of changing the parameters of Input__* files.
+This is a script of changing the parameters of Input__* files.
 
 How to use it:
-  1. Set up the files and the parameters you want ot change.
+  1. Set up the files and the parameters you want to change.
     a. The file is set to be a `File()` class. The `File` takes four inputs: file name, constant
        parameters, changing parameters, and flag file or not. Please check out the `Classes` section
        for the detail of inputs.
@@ -12,15 +13,14 @@ How to use it:
       Parameter files:
         The first column specifies the option's name and the second column should be the value of the option.
       Flag files:
-        The first line(row) of the flag files should always start with `# ` and the following column
+        The first line of a flag file should always start with `# ` and the following column
         names in the header specify the options' names.
 
   2. Overwrite the `execution()` function to your execution.
-     For instance, `subprocess.called(['./gamer']).
+     For instance, `subprocess.called([kwargs["exe"]]).
 
-  3. You might also want to pass some arguments to the `execution()` from the command line. We already
-     pass all the arguments, so all you need to do is to add you own `parser.add_argument` at `Main`
-     section.
+  3. [Optional] To pass extra arguments to `execution()` from the command line,
+     add your own `parser.add_argument` in the `Main` section.
 
 An example of the script (from Alexander Kunkel):
 -----------------------------------------------------------------------------------------------------
@@ -116,9 +116,9 @@ import subprocess
 #====================================================================================================
 # Global variables
 #====================================================================================================
-RECURSION_LIMIT = 1000  # Reset the recursion limit. (default in python is 1000)
-RETURN_FAIL    = False
-RETURN_SUCCESS = True
+RECURSION_LIMIT = 1000  # the recursion depth limit (default in Python is 1000)
+RETURN_FAIL     = False
+RETURN_SUCCESS  = True
 
 
 
@@ -126,76 +126,86 @@ RETURN_SUCCESS = True
 # Classes
 #====================================================================================================
 class File():
-    def __init__( self, file_name, consts, paras, flag ):
+    def __init__( self, file_name, consts, paras, flag_file ):
         """
         file_name : string. The file name.
-        consts    : dict. The const parameter want to be specified.
-        paras     : dict with list element. The parameter want to be chenged.
-        flag      : bool. The flag file or not.
+        consts    : dict. The const parameter to be specified.
+        paras     : dict with list element. The parameter to be chenged.
+        flag_file : bool. The flag file or not.
         """
         self.name   = file_name
         self.paras  = paras
         self.consts = consts
-        self.flag   = flag
+        self.flag   = flag_file
+
+        self.set_constants()
+
+    def set_constants( self ):
+        replace_func = replace_parameter_flag if self.flag else replace_parameter
+        for key, val in self.consts.items():
+            replace_func( self.name, key, val )
+        return
+
 
 
 #====================================================================================================
 # Functions
 #====================================================================================================
 def iter_files( files, record_changed={}, **kwargs ):
-    # copy_files = files.copy() # work in python3 only
-    copy_files = list(files)
+    if len(files) == 0: return execution( record=record_changed, **kwargs )
 
-    if copy_files == []:        # End of changing files
-        return execution( record=record_changed, **kwargs )
+    files_copy = list(files)
+    f_class    = files_copy.pop(0)
+    return iter_file_parameters( f_class.name, f_class.paras, f_class.flag, record_changed, files_copy, **kwargs )
 
-    replace_file_class = copy_files.pop(0)
-    replace_file  = replace_file_class.name
-    replace_paras = replace_file_class.paras
-    replace_flag  = replace_file_class.flag
-    return iter_paras( replace_file, replace_paras, record_changed, replace_flag, rest_files=copy_files, **kwargs )
+def iter_file_parameters( file_name, paras, flag_file, record, rest_files=[], **kwargs ):
+    if len(paras) == 0: return iter_files( rest_files, **kwargs )
 
-def iter_paras( file_name, paras, record, flag, **kwargs ):
-    copy_paras  = paras.copy()
-    copy_kwargs = kwargs.copy()
-    copy_kwargs.pop("rest_files")
-    if copy_paras == {}:        # End of the changes
-        return iter_files( kwargs["rest_files"], **copy_kwargs )
+    kwargs_copy  = kwargs.copy()
+    paras_copy   = paras.copy()
+    replace_key  = next( iter(paras_copy) ) # take the first key to replace
+    replace_func = replace_parameter_flag if flag_file else replace_parameter
+    vals         = paras_copy.pop(replace_key)
 
-    replace_key = next(iter(copy_paras)) # Take the first key to replace
-    vals = copy_paras.pop(replace_key)
     for val in vals:
-        if not kwargs["quite"]: print("File <%s> changing %s --> %s"%(file_name, replace_key, str(val)))
-        replace_parameter( file_name, replace_key, val, flag )
-        record[replace_key]=val
-        iter_paras( file_name, copy_paras, record, flag, **kwargs )  # repalce the next parameter
+        if not kwargs["quite"]: print("File %-25s changing: %-20s --> %-20s"%(file_name, replace_key, str(val)))
+        replace_func( file_name, replace_key, val )
+        record[replace_key] = val
+        iter_file_parameters( file_name, paras_copy, flag_file, record, rest_files, **kwargs ) # replace the next parameter
     return
 
+def replace_parameter( file_name, para_name, val ):
+    with open( file_name, 'r' ) as f:
+        content = f.read()
 
-def replace_parameter( file_name, name, val, flag ):
-    if flag:
-        with open( file_name, 'r' ) as f:
-            header = f.readline()
-        index = { v:i for i, v in enumerate(header.split()[1:]) }       # The first element assume to be "#"
-        data = np.loadtxt(file_name)
-        if len(index) != data.shape[1]: raise BaseException("ERROR: The number of columns in <%s> does not match to the header."%(file_name))
-        data[:, index[name]] = val
-        np.savetxt(file_name, data, fmt="%7g"+"%20.16g"*(len(index)-1), header=header[2:-1])
-    else:
-        with open( file_name, 'r' ) as f:
-            content = f.read()
+    content_new, Nsub = re.subn( r"^(%s\s+)([^\s]+)"%para_name, r"\g<1>%s"%str(val), content, flags=re.M )
+    if Nsub == 0: raise BaseException("ERROR: Cannot find <%s> in <%s>."%(para_name, file_name))
 
-        content_new, n_sub = re.subn( r"^(%s\s+)([^\s]+)"%name, r"\g<1>%s"%str(val), content, flags=re.M )
-        if n_sub == 0: raise BaseException("ERROR: Can not find <%s> in <%s>."%(name, file_name))
+    with open( file_name, 'w' ) as f:
+        f.write( content_new )
+    return
 
-        with open( file_name, 'w' ) as f:
-            f.write( content_new )
+def replace_parameter_flag( file_name, para_name, val ):
+    with open( file_name, 'r' ) as f:
+        header = f.readline()
+
+    index = { v:i for i, v in enumerate(header.split()[1:]) } # the first element is assumed to be "#"
+    data  = np.loadtxt(file_name)
+
+    if len(index) != data.shape[1]: raise BaseException("ERROR: The number of columns in <%s> does not match the header."%(file_name))
+
+    data[:, index[para_name]] = val
+
+    np.savetxt( file_name, data, fmt="%7g"+"%20.16g"*(len(index)-1), header=header[2:-1] )
     return
 
 def execution( **kwargs ):
+    """
+    Main execution after iterating all the parameters.
+    """
     record_parameters = kwargs["record"]
-    print(record_parameters)
-    print("GO GO GAMER GO!")
+    print( record_parameters )
+    print( "GO GO GAMER GO!" )
     return RETURN_SUCCESS
 
 
@@ -203,62 +213,57 @@ def execution( **kwargs ):
 #====================================================================================================
 # Main
 #====================================================================================================
-sys.setrecursionlimit(RECURSION_LIMIT)                                          # Reset the recursion depth limit
+if __name__ == "__main__":
+    sys.setrecursionlimit( RECURSION_LIMIT ) # reset the recursion depth limit
 
-# 1. Set up the files and the parameters to be changed
-file_name1   = "Input__Parameter"                                               # file name
-const_paras1 = {"END_T":-1, "END_STEP":50}                                      # the parameters change only once
-loop_paras1  = {"NX0_TOT_X":[16, 32], "NX0_TOT_Y":[16, 32]}                     # the parameters change as given list
-# set the `File` class, the file changing method is decided by the last parameter. If the file is a flag file, please set it to `True`.
-file1        = File( file_name1, const_paras1, loop_paras1, flag=False )
+    # 1. Set up the files and the parameters to be changed
+    file_name1   = "Input__Parameter"                                             # file name
+    const_paras1 = { "END_T":-1, "END_STEP":50 }                                  # the constant parameters
+    iter_paras1  = { "NX0_TOT_X":[16, 32], "NX0_TOT_Y":[16, 32] }                 # the parameters iterated as the given list
+    file1        = File( file_name1, const_paras1, iter_paras1, flag_file=False ) # set the `File` class
 
-file_name2   = "Input__TestProb"
-const_paras2 = {"Const_1":-1, "Const_2":50}
-loop_paras2  = {"Parameter_1":[0.01, 0.02], "Parameter_2":[16, 32]}
-file2        = File( file_name2, const_paras2, loop_paras2, flag=False )
+    file_name2   = "Input__TestProb"
+    const_paras2 = { "Const_1":-1, "Const_2":50 }
+    iter_paras2  = { "Parameter_1":[0.01, 0.02], "Parameter_2":[16, 32] }
+    #file2        = File( file_name2, const_paras2, iter_paras2, flag_file=False )
 
-# this will change the single column to the same value
-file_name3   = "Input__Flag"
-const_paras3 = {"derefine":0.1}
-loop_paras3  = {"Soften":[0.01, 0.02], "refine":[16, 32]}
-file3        = File( file_name3, const_paras3, loop_paras3, flag=True )
+    # this will change the single column to the same value
+    file_name3   = "Input__Flag"
+    const_paras3 = { "derefine":0.1 }
+    iter_paras3  = { "Soften":[0.01, 0.02], "refine":[16, 32] }
+    #file3        = File( file_name3, const_paras3, iter_paras3, flag_file=True )
 
-# this will change the column to the assigned value
-file_name4   = "Input__Flag_Rho"
-const_paras4 = {}
-loop_paras4  = { "Density":[( 0, 2, 4, 8,10,12,14,16,18,20,22,24,26),
-                            ( 1, 3, 5, 7, 9,11,13,15,17,19,21,23,25)] }
-file4        = File( file_name4, const_paras4, loop_paras4, flag=True )
+    # this will change the column to the assigned value
+    file_name4   = "Input__Flag_Rho"
+    const_paras4 = {}
+    iter_paras4  = { "Density":[( 0, 2, 4, 8,10,12,14,16,18,20,22,24 ),
+                                ( 1, 3, 5, 7, 9,11,13,15,17,19,21,23 )] }
+    file4        = File( file_name4, const_paras4, iter_paras4, flag_file=True )
 
-# Wrap all the `File` classes.
-files = [file1, file2, file3, file4]
+    # files = [file1, file2, file3, file4]        # wrap all the `File` classes.
+    files = [file1, file4]        # wrap all the `File` classes.
 
-# 2. Taking the input arguments
-parser = argparse.ArgumentParser( description = "A small script of changing the parameters of Input__* files.",
-                                  formatter_class = argparse.RawTextHelpFormatter,
-                                  add_help=False)
+    # 2. Taking the input arguments
+    parser = argparse.ArgumentParser( description = "A script of changing the parameters for Input__* files.",
+                                      formatter_class = argparse.RawTextHelpFormatter,
+                                      add_help=False)
 
-parser.add_argument( "-h", "--help",
-                     action="help", default=argparse.SUPPRESS,
-                     help="Show this help message and exit.\n"
-                   )
+    parser.add_argument( "-h", "--help",
+                         action="help", default=argparse.SUPPRESS,
+                         help="Show this help message and exit.\n"
+                       )
 
-parser.add_argument( "-q", "--quite",
-                     action="store_true",
-                     help="Enable slient mode.\n"
-                   )
+    parser.add_argument( "-q", "--quite",
+                         action="store_true",
+                         help="Enable silent mode.\n"
+                       )
 
-parser.add_argument( "-e", "--exe", type=str,
-                     default="./gamer",
-                     help="The file you want to execute.\n"
-                   )
+    parser.add_argument( "-e", "--exe", type=str,
+                         default="./gamer",
+                         help="The file you want to execute.\n"
+                       )
 
-args = vars( parser.parse_args() )
+    args = vars( parser.parse_args() )
 
-# 3. Set the constant parameters
-for f in files:
-    for key, val in f.consts.items():
-        replace_parameter( f.name, key, val, f.flag )
-
-# 4. Start changing parameters and running
-iter_files( files, **args )
+    # 3. Start changing parameters and running
+    iter_files( files, **args )
