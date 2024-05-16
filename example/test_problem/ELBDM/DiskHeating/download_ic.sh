6a9931e290e19a62f827059316eb007b6f0a73f0
HsunYeong <jason88697@gmail.com>
2024-05-16 20:53:23 +0800

    update link in download_ic.sh

--- example/test_problem/ELBDM/DiskHeating/download_ic.sh
+++ example/test_problem/ELBDM/DiskHeating/download_ic.sh
@@ -1,13 +1,9 @@
-filename=ic_files
+filename=disk-heating-ic
 
-curl http://use.yt/upload/edc16152 -o ${filename}.tgz
+curl https://girder.hub.yt/api/v1/item/6645cffcff473673ea91b24d/download -o ${filename}.tgz
 tar -zxvf ${filename}.tgz
 rm ${filename}.tgz
 ln -s ${filename}/UM_IC_0.4_M7 UM_IC
-
-curl http://use.yt/upload/3dde6b5c -o ${filename}.tgz
-tar -zxvf ${filename}.tgz -C ${filename}/
-rm ${filename}.tgz
 ln -s ${filename}/PAR_IC_0.4_M7_low_res PAR_IC
 
 
