42e82d6cf96d19c681da7c415cd74009c21b8f77
Alexander Kunkel <kunkel.alexander@protonmail.com>
2024-06-02 12:02:36 +0200

    Change default precision of precomputed Gram-Fourier interpolation to double

--- include/GramFE_Interpolation.h
+++ include/GramFE_Interpolation.h
@@ -25,7 +25,9 @@ namespace gfei_gsl = gsl_single_precision;
 #endif
 
 // accuracy for matrix multiplication in precomputed interpolation (PI)
-#ifdef FLOAT8
+// also requires double precision since interpolation matrices have high condition numbers
+#define PI_GSL_FLOAT8
+#ifdef PI_GSL_FLOAT8
 namespace pi_gsl   = gsl_double_precision;
 #else
 namespace pi_gsl   = gsl_single_precision;
