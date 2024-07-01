fa61796b1a76e7752d0affd95c0a3120b18abc65
ChunYen-Chen <chunyenchen2019@gmail.com>
2024-07-01 16:37:48 +0800

    Update generate_make.sh

--- example/test_problem/Hydro/JetICMWall/generate_make.sh
+++ example/test_problem/Hydro/JetICMWall/generate_make.sh
@@ -2,6 +2,6 @@
 
 PYTHON=python3
 
-${PYTHON} configure.py --machine=eureka_intel --mpi=true --hdf5=true --gpu=true --model=HYDRO \
+${PYTHON} configure.py --mpi=true --hdf5=true --gpu=true --model=HYDRO \
                        --srhd=true --eos=TAUBMATHEWS --flux=HLLC --flu_scheme=MHM \
-                       --passive=4
+                       --passive=4 "$@"
