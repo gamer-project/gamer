c6678c34b7ab0d6138b765608040f81d21e2d09a
Hsi-Yu Schive <hyschive@gmail.com>
2024-07-06 14:58:33 +0800

    Enable --barotropic automatically for --eos=ISOTHERMAL in configure.py

--- src/configure.py
+++ src/configure.py
@@ -447,7 +447,7 @@ def load_arguments():
                        )
 
     parser.add_argument( "--barotropic", type=str2bool, metavar="BOOLEAN", gamer_name="BAROTROPIC_EOS",
-                         default=False,
+                         default=None,
                          depend={"model":"HYDRO"},
                          constraint={ True:{"eos":["ISOTHERMAL", "TABULAR", "USER"]} },
                          help="Whether or not the equation of state set by <--eos> is barotropic. Mandatory for <--eos=ISOTHEMAL>. Optional for <--eos=TABULAR> and <--eos=USER>.\n"
@@ -744,6 +744,10 @@ def set_conditional_defaults( args ):
         if   args["cosmic_ray"]: args["eos"] = "COSMIC_RAY"
         elif args["srhd"]      : args["eos"] = "TAUBMATHEWS"
         else                   : args["eos"] = "GAMMA"
+
+    if args["barotropic"] is None:
+        if   args["eos"] == "ISOTHERMAL": args["barotropic"] = True
+        else                            : args["barotropic"] = False 
     return args
 
 def set_gpu( gpus, flags, args ):
