1fa6fe6997ab609992cbaecfeaf35572ad852b96
Barry T. Chiang <87967685+BarryTC@users.noreply.github.com>
2024-08-26 22:30:15 +0800

    Add files via upload

--- example/test_problem/Hydro/AGORA_IsolatedGalaxy/download_ic.sh
+++ example/test_problem/Hydro/AGORA_IsolatedGalaxy/download_ic.sh
@@ -1,1 +1,5 @@
-wget https://github.com/typelevel/grackle_data_files/blob/main/input/CloudyData_UVB%3DHM2012.h5
+wget --no-check-certificate -O ./LOW.tar.gz https://www.dropbox.com/sh/1xzt1rysy9v3a9l/AAAMlJBQG1OQFW4cjhp11Ex6a/LOW.tar.gz?dl=1
+wget --no-check-certificate  -O ./CloudyData_UVB=HM2012.h5 https://github.com/grackle-project/grackle_data_files/raw/main/input/CloudyData_UVB=HM2012.h5
+tar xzvf LOW.tar.gz
+mv LOW/*.dat ./
+rmdir LOW
