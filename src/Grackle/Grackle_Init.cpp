be45d08bcb662581e5cc386c0412c6c15df296f7
ChunYen-Chen <chunyenchen2019@gmail.com>
2024-10-17 13:47:17 +0800

    Add warning message

--- src/Grackle/Grackle_Init.cpp
+++ src/Grackle/Grackle_Init.cpp
@@ -34,6 +34,9 @@ void Grackle_Init()
       Aux_Error( ERROR_INFO, "inconsistent floating-point type (GAMER(real_che): %d, Grackle: %d) !!\n",
                  sizeof(real_che), sizeof(gr_float) );
 */
+   if ( MPI_Rank == 0  &&  sizeof(gr_float) != 8 )
+       Aux_Message( stderr, "WARNING : Grackle suggests floating-point number to be 8 bytes (GRACKLE_FLOAT_8) !!\n" );
+
    if ( MPI_Rank == 0 )   Aux_Message( stdout, "Grackle floating-point number uses %d bytes\n", sizeof(gr_float) );
 
 // comoving frame is not supported yet
