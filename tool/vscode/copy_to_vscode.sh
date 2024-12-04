93a3c28f7be411f3e7ff7199f924d927ea68e178
Yuan-Ming Hsu <technic960183@gmail.com>
2024-12-04 16:08:38 +0800

    Support dynamic working dir for copy_to_vscode
    And creating .vscode directory if not exists

--- tool/vscode/copy_to_vscode.sh
+++ tool/vscode/copy_to_vscode.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Change to the directory where the script is located
+cd "$(dirname "$0")"
+
 # Set source and destination paths relative to the script's location
 SOURCE_DIR="."
 TARGET_DIR="../../.vscode"
@@ -7,11 +10,15 @@ TARGET_DIR="../../.vscode"
 # Specify files to exclude (space-separated list, e.g., "file1 file2")
 EXCLUDE_FILES="copy_to_vscode.sh"
 
-if [ ! -d "$TARGET_DIR" ]; then
-  echo "Target directory $TARGET_DIR does not exist."
-  echo "Please create the .vscode directory or setup TARGET_DIR."
-  exit 1
+if [ ! -d "$(realpath "$TARGET_DIR")" ]; then
+  echo "Target directory $(realpath "$TARGET_DIR") does not exist."
+  mkdir -p "$(realpath "$TARGET_DIR")"
+  echo "Target directory $(realpath "$TARGET_DIR") created."
 fi
+
+# Prompt the user for the working directory under bin
+echo "Enter the working directory under bin/: (e.g. shocktube)"
+read -r WORKING_DIR
 
 # Function to check if a file is in the exclude list
 is_excluded() {
@@ -30,9 +37,7 @@ for file in "$SOURCE_DIR"/*; do
     continue
   fi
 
-  # Check if the target file already exists
   if [ -e "$TARGET_DIR/$filename" ]; then
-    # Prompt the user for overwriting the file
     echo "File $filename already exists in $TARGET_DIR. Overwrite? (y/n)"
     read -r answer
 
@@ -46,4 +51,6 @@ for file in "$SOURCE_DIR"/*; do
     cp "$file" "$TARGET_DIR/$filename"
     echo "$filename copied."
   fi
+
+  sed -i "s/Gamer_Working_SubDir/$WORKING_DIR/g" "$TARGET_DIR/$filename"
 done
