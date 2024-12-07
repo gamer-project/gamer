fae76383ae7452b6547f9d9df85c2588e43d4161
ChunYen-Chen <chunyenchen2019@gmail.com>
2024-12-07 22:15:05 +0800

    [Bugfix] Missing Output_HDF5_TestProb

--- src/TestProblem/Hydro/ExactCooling/Init_TestProb_ExactCooling.cpp
+++ src/TestProblem/Hydro/ExactCooling/Init_TestProb_ExactCooling.cpp
@@ -173,6 +173,31 @@ void SetGridIC( real fluid[], const double x, const double y, const double z, co
 
 
 
+#ifdef SUPPORT_HDF5
+//-------------------------------------------------------------------------------------------------------
+// Function    :  Output_HDF5_TestProb
+// Description :  Store the problem specific parameter in HDF5 outputs (Data_*)
+//
+// Note         : 1. This function only works in MPI_RANK == 0
+//                2. We supports int, uint, long, ulong, bool, float, double, and string datatype
+//                3. There MUST be more than one parameter to be stored
+//                4. The pointer of the data MUST still exist outside the function, e.g. global variables
+//
+// Parameter   :  HDF5_InputTest : the structure storing the parameters
+//
+// Return      :  None
+//-------------------------------------------------------------------------------------------------------
+void Output_HDF5_TestProb( HDF5_Output_t *HDF5_InputTest )
+{
+
+   HDF5_InputTest->Add( "EC_Temp", &EC_Temp );
+   HDF5_InputTest->Add( "EC_Dens", &EC_Dens );
+
+} // FUNCTION : Output_HDF5_TestProb
+#endif // #ifdef SUPPORT_HDF5
+
+
+
 //-------------------------------------------------------------------------------------------------------
 // Function    :  OutputExactCooling
 // Description :  Output the temperature relative error in the exact cooling problem
@@ -296,6 +321,9 @@ void Init_TestProb_Hydro_ExactCooling()
 
    Init_Function_User_Ptr         = SetGridIC;
    Output_User_Ptr                = Output_ExactCooling;
+#  ifdef SUPPORT_HDF5
+   Output_HDF5_TestProb_Ptr       = Output_HDF5_TestProb;
+#  endif
 //   End_User_Ptr                   = End_ClusterMerger;
 //   Aux_Record_User_Ptr            = Aux_Record_ClusterMerger;
 
