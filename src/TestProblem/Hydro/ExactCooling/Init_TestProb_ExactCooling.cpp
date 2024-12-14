74fc76fd2cd179e2700415fc6b22987dbfe2c07a
ChunYen-Chen <chunyenchen2019@gmail.com>
2024-12-14 16:45:23 +0800

    Review comments

--- src/TestProblem/Hydro/ExactCooling/Init_TestProb_ExactCooling.cpp
+++ src/TestProblem/Hydro/ExactCooling/Init_TestProb_ExactCooling.cpp
@@ -179,8 +179,8 @@ void SetGridIC( real fluid[], const double x, const double y, const double z, co
 // Description :  Store the problem specific parameter in HDF5 outputs (Data_*)
 //
 // Note         : 1. This function only works in MPI_RANK == 0
-//                2. We supports int, uint, long, ulong, bool, float, double, and string datatype
-//                3. There MUST be more than one parameter to be stored
+//                2. We support int, uint, long, ulong, bool, float, double, and string datatypes
+//                3. There MUST be at least one parameter to be stored
 //                4. The pointer of the data MUST still exist outside the function, e.g. global variables
 //
 // Parameter   :  HDF5_InputTest : the structure storing the parameters
