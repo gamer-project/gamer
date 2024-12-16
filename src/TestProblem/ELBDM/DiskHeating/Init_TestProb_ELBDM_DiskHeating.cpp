5916244367e96655bf89ec90c34dcae65d779016
ChunYen-Chen <chunyenchen2019@gmail.com>
2024-12-16 11:55:36 +0800

    Update according to merge

--- src/TestProblem/ELBDM/DiskHeating/Init_TestProb_ELBDM_DiskHeating.cpp
+++ src/TestProblem/ELBDM/DiskHeating/Init_TestProb_ELBDM_DiskHeating.cpp
@@ -47,7 +47,8 @@ static int     DispTable_Nbin;            // number of bins of velocity dispersi
 void Par_Init_ByFunction_DiskHeating( const long NPar_ThisRank, const long NPar_AllRank,
                                       real_par *ParMass, real_par *ParPosX, real_par *ParPosY, real_par *ParPosZ,
                                       real_par *ParVelX, real_par *ParVelY, real_par *ParVelZ, real_par *ParTime,
-                                      long_par *ParType, real_par *AllAttributeFlt[PAR_NATT_FLT_TOTAL],
+                                      long_par *ParType, long_par *ParPUid,
+                                      real_par *AllAttributeFlt[PAR_NATT_FLT_TOTAL],
                                       long_par *AllAttributeInt[PAR_NATT_INT_TOTAL] );
 static void Init_NewDiskRestart();
 static void Init_NewDiskVelocity();
