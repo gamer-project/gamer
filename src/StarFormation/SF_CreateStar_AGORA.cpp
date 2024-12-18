b345f7959ffba3cb9b5a568ff68a51f61d4fb0cb
Anchewei <10302leo@gmail.com>
2024-12-18 15:25:46 +0800

    minor fix

--- src/StarFormation/SF_CreateStar_AGORA.cpp
+++ src/StarFormation/SF_CreateStar_AGORA.cpp
@@ -406,7 +406,7 @@ void SF_CreateStar_AGORA( const int lv, const real TimeNew, const real dt, Rando
 
 // //       Gravitational minimum check inside the control volume
 // //       ===========================================================================================================
-//          real phi000 = Pot_Array_USG_F[t]; // the potential of the current cell
+         real phi000 = Pot_Array_USG_F[t]; // the potential of the current cell
 //          real phiijk = (real)0.0;
 //          bool NotMiniPot          = false;
 //          for (int vk=pk-AccCellNum; vk<=pk+AccCellNum; vk++)
