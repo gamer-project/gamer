958c189375735c41013ed245d043fa59e70ff3db
ChunYen-Chen <chunyenchen2019@gmail.com>
2024-12-28 20:54:51 +0800

    Support `python configure.py` autocomplete

--- tool/bash/config_autocomplete.sh
+++ tool/bash/config_autocomplete.sh
@@ -1,36 +1,75 @@
 #!/bin/bash
 
+__gamer_check_gamer_info() {
+    # Check if the current directory is GAMER and the `configure.py` for generating `Makefile`
+    # $1 : configure.py filename
+
+    local isGitDir=`git rev-parse --is-inside-work-tree`
+    [ ! "$isGitDir" = true ] && return 1
+
+    local firstGAMERCommit=`git rev-list --max-parents=0 HEAD`
+    [ ! "$firstGAMERCommit" = "2705887745ac1fd254774c512af4a05b0f7f7fb6" ] && return 1
+
+    local configurePyFirstCommit=`git log --diff-filter=A -- $1 | head -n 1 | awk '{print $2}'`
+    [ ! "$configurePyFirstCommit" = "a93978a39b4388d169a0f45d1987f55486578fab" ] && return 1
+
+    return 0
+
+} # __gamer_check_gamer_info()
+
+
 __gamer_configure_autocomplete() {
 
-    local options option_array option_array2
+    local all_options sub_options all_option_array sub_option_array configure_filename
     local not_set=true
+    local first=${COMP_WORDS[0]}
+    local second=${COMP_WORDS[1]}
     local subsub="${COMP_WORDS[COMP_CWORD-2]}"
     local sub="${COMP_WORDS[COMP_CWORD-1]}"
     local cur="${COMP_WORDS[COMP_CWORD]}"
-    local all_options=$(./configure.py --autocomplete_info=all)
-    IFS=' ' read -r -a option_array <<< "${all_options}"
+
+    if [[ "$first" == "python"* ]]; then
+        configure_filename=$second
+    else
+        configure_filename=$first
+    fi
+
+    __gamer_check_gamer_info $configure_filename
+
+    # Not the GAEMR `configure.py`
+    if [ $? -ne 0 ]; then
+       COMPREPLY=( $( compgen -o default -- ${COMP_WORDS[COMP_CWORD]} ) )
+       # add a trailing space since `-o nospace`
+       for i in "${!COMPREPLY[@]}"; do
+           COMPREPLY[$i]="${COMPREPLY[$i]} "
+       done
+       return 0
+    fi
+
+    all_options=$(./configure.py --autocomplete_info=all)
+    IFS=' ' read -r -a all_option_array <<< "${all_options}"
 
     COMPREPLY=() # NOTE: please add a space when ending the option
 
     # option choices
-    for opt in "${option_array[@]}"
+    for opt in "${all_option_array[@]}"
     do
         # --option=xx
         if [[ "$opt" == "$subsub=" && "=" == "$sub" ]]; then
-            options=$(./configure.py --autocomplete_info="$opt")
-            IFS=' ' read -r -a option_array2 <<< "${options}"
-            for opt2 in "${option_array2[@]}"
+            sub_options=$(./configure.py --autocomplete_info="$opt")
+            IFS=' ' read -r -a sub_option_array <<< "${sub_options}"
+            for opt2 in "${sub_option_array[@]}"
             do
-                if [[ "$opt2" != "$cur"* ]]; then continue;fi
+                if [[ "$opt2" != "$cur"* ]]; then continue; fi
                 COMPREPLY+=("$opt2 ")
             done
             not_set=false
             break
         # --option, --option xxx, or --option=
         elif [[ "$opt" == "$sub=" ]]; then
-            options=$(./configure.py --autocomplete_info="$opt")
-            IFS=' ' read -r -a option_array2 <<< "${options}"
-            for opt2 in "${option_array2[@]}"
+            sub_options=$(./configure.py --autocomplete_info="$opt")
+            IFS=' ' read -r -a sub_option_array <<< "${sub_options}"
+            for opt2 in "${sub_option_array[@]}"
             do
                 # --option= or --option
                 if [[ "$cur" == "" || "$cur" == "=" ]]; then
@@ -57,3 +96,5 @@ __gamer_configure_autocomplete() {
 } # __gamer_configure_autocomplete()
 
 complete -o nospace -F __gamer_configure_autocomplete ./configure.py
+complete -o nospace -F __gamer_configure_autocomplete python
+complete -o nospace -F __gamer_configure_autocomplete python3
