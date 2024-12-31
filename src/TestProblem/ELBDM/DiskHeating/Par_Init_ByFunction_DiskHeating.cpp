59d68076ef508cd1eb3c4e7a9623a3b60bb446f8
ChunYen-Chen <chunyenchen2019@gmail.com>
2025-01-01 00:28:07 +0800

    Bugfix prototype

--- src/TestProblem/ELBDM/DiskHeating/Par_Init_ByFunction_DiskHeating.cpp
+++ src/TestProblem/ELBDM/DiskHeating/Par_Init_ByFunction_DiskHeating.cpp
@@ -48,7 +48,7 @@
 void Par_Init_ByFunction_DiskHeating( const long NPar_ThisRank, const long NPar_AllRank,
                                       real_par *ParMass, real_par *ParPosX, real_par *ParPosY, real_par *ParPosZ,
                                       real_par *ParVelX, real_par *ParVelY, real_par *ParVelZ, real_par *ParTime,
-                                      long_par *ParType, real_par *AllAttribute[PAR_NATT_FLT_TOTAL],
+                                      long_par *ParType, real_par *AllAttributeFlt[PAR_NATT_FLT_TOTAL],
                                       long_par *AllAttributeInt[PAR_NATT_INT_TOTAL] )
 {
 
