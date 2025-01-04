ff6015a4c449aba7bee34ecc5b4087fd02e7a1fe
John ZuHone <jzuhone@gmail.com>
2025-01-03 21:58:13 -0500

    Increase end time for this test

--- src/TestProblem/Hydro/ConductionStepFunction/Init_TestProb_Hydro_ConductionStepFunction.cpp
+++ src/TestProblem/Hydro/ConductionStepFunction/Init_TestProb_Hydro_ConductionStepFunction.cpp
@@ -108,7 +108,7 @@ void SetParameter()
 
 // (3) reset other general-purpose parameters
 //     --> a helper macro PRINT_RESET_PARA is defined in Macro.h
-   const double End_T_Default    = 1.0;
+   const double End_T_Default    = 5.0;
    const long   End_Step_Default = __INT_MAX__;
 
    if ( END_STEP < 0 ) {
