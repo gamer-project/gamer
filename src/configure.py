530bcf21ce6483a5daf442ac0bc61a6736eafd85
John ZuHone <jzuhone@gmail.com>
2025-01-06 07:54:55 -0500

    Require MHM/MHM_RP for conduction and/or viscosity

--- src/configure.py
+++ src/configure.py
@@ -590,13 +590,15 @@ def load_arguments():
 
     parser.add_argument( "--viscosity", type=str2bool, metavar="BOOLEAN", gamer_name="VISCOSITY",
                          default=False,
-                         constraint={ True:{"comoving":False} },
+                         constraint={ True:{"comoving":False, "flu_scheme":["MHM", "MHM_RP"]},
+                                      False:{"flu_scheme":["CTU"]}},
                          help="Enable viscosity. Must enable <--hydro\n"
 	               )
 
     parser.add_argument( "--conduction", type=str2bool, metavar="BOOLEAN", gamer_name="CONDUCTION",
                          default=False,
-                         constraint={ True:{"comoving":False} },
+                         constraint={ True:{"comoving":False, "flu_scheme":["MHM", "MHM_RP"]},
+                                      False:{"flu_scheme":["CTU"]}},
 		                 help="Enable conduction. Must enable <--hydro.\n"
                        )
 
