4eecd486238badb5fca833256a5a2568c0997b3b
ChunYen-Chen <chunyenchen2019@gmail.com>
2025-01-07 11:56:43 +0800

    Update

--- example/test_problem/Hydro/CDM_LSS/download_ic.sh
+++ example/test_problem/Hydro/CDM_LSS/download_ic.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 LOCAL_FILENAME="PAR_IC"
+FILE_ID="677c92db999605c485c8de69"
 
 # 1. Download through `curl`
-curl https://hub.yt/api/v1/item/677c92db999605c485c8de69/download -o ${LOCAL_FILENAME}
+curl https://hub.yt/api/v1/item/${FILE_ID}/download -o ${LOCAL_FILENAME}
 
 # 2. download through `girder`
 API_URL="https://girder.hub.yt/api/v1"
-FILE_ID="677ba3e4999605c485c8de5a"
 
-# girder-cli --api-url ${API_URL} download --parent-type item ${FILE_ID} ${LOCAL_FILENAME}
-
+# girder-cli --api-url ${API_URL} download --parent-type item ${FILE_ID} temp
+# mv temp/${LOCAL_FILENAME} ./
+# rmdir temp
