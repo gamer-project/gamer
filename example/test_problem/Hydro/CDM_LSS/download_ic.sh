261cf2a334004269ff0cb56504c935f2de88c8bd
ChunYen-Chen <chunyenchen2019@gmail.com>
2025-01-07 10:44:26 +0800

    Update PAR_IC file

--- example/test_problem/Hydro/CDM_LSS/download_ic.sh
+++ example/test_problem/Hydro/CDM_LSS/download_ic.sh
@@ -1,1 +1,13 @@
-curl -LJO https://use.yt/upload/c2b0f76b
+#!/bin/bash
+
+LOCAL_FILENAME="PAR_IC"
+
+# 1. Download through `curl`
+curl https://hub.yt/api/v1/item/677c92db999605c485c8de69/download -o ${LOCAL_FILENAME}
+
+# 2. download through `girder`
+API_URL="https://girder.hub.yt/api/v1"
+FILE_ID="677ba3e4999605c485c8de5a"
+
+# girder-cli --api-url ${API_URL} download --parent-type item ${FILE_ID} ${LOCAL_FILENAME}
+
