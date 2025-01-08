5c05b8640cdea38504e50c975144cb6017e8f822
ChunYen-Chen <chunyenchen2019@gmail.com>
2025-01-08 17:43:58 +0800

    Uncomment the curl

--- example/test_problem/Hydro/AGORA_IsolatedGalaxy/download_ic.sh
+++ example/test_problem/Hydro/AGORA_IsolatedGalaxy/download_ic.sh
@@ -6,8 +6,8 @@ FILE_ID1="677ca225999605c485c8de6f"
 FILE_ID2="677ca211999605c485c8de6c"
 
 # 1. download
-#curl https://hub.yt/api/v1/item/${FILE_ID1}/download -o "${LOCAL_FILENAME1}.tar.gz"
-#curl https://hub.yt/api/v1/item/${FILE_ID2}/download -o "${LOCAL_FILENAME2}"
+curl https://hub.yt/api/v1/item/${FILE_ID1}/download -o "${LOCAL_FILENAME1}.tar.gz"
+curl https://hub.yt/api/v1/item/${FILE_ID2}/download -o "${LOCAL_FILENAME2}"
 
 # 2. unzip
 tar xzvf ${LOCAL_FILENAME1}.tar.gz
