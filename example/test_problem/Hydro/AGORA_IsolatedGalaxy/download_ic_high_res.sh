da7b96ff95b93992a8e226d74a3bfab4a0bc968b
BarryTC <barrytchiang@gmail.com>
2025-01-08 21:47:04 +0800

    IC download links updated

--- example/test_problem/Hydro/AGORA_IsolatedGalaxy/download_ic_high_res.sh
+++ example/test_problem/Hydro/AGORA_IsolatedGalaxy/download_ic_high_res.sh
@@ -1,10 +1,20 @@
-wget --no-check-certificate -O ./HI.tar.gz https://www.dropbox.com/scl/fi/m8a6iutr4uf5p2whjsydw/HI.tar.gz?rlkey=h3gz1z7usxzqhuud1kos4zzff
-wget --no-check-certificate  -O ./CloudyData_UVB=HM2012.h5 https://github.com/grackle-project/grackle_data_files/raw/main/input/CloudyData_UVB=HM2012.h5
-tar xzvf HI.tar.gz
-mv HI/*.dat ./
-rm -rf HI
-rm HI.tar.gz
+#!/bin/bash
+LOCAL_FILENAME1="HI"
+LOCAL_FILENAME2="CloudyData_UVB=HM2012.h5"
+FILE_ID1="677e4757999605c485c8de92"
+FILE_ID2="677ca211999605c485c8de6c"
 
+# file download
+curl https://hub.yt/api/v1/item/${FILE_ID1}/download -o "${LOCAL_FILENAME1}.tar.gz"
+curl https://hub.yt/api/v1/item/${FILE_ID2}/download -o "${LOCAL_FILENAME2}"
+
+# file unzip
+tar xzvf ${LOCAL_FILENAME1}.tar.gz
+mv ${LOCAL_FILENAME1}/*.dat ./
+rm -rf ${LOCAL_FILENAME1}
+rm ${LOCAL_FILENAME1}.tar.gz
+
+# Input_* soft links
 ln -s ./Input_Options/Input__Flag_Jeans.high-res Input__Flag_Jeans
 ln -s ./Input_Options/Input__Flag_ParMassCell.high-res Input__Flag_ParMassCell
 ln -s ./Input_Options/Input__Flag_Rho.high-res Input__Flag_Rho
