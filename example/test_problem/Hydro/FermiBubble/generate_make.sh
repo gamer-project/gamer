814da14825e3888e38c210b4ca10e0c62f2efbdd
ChunYen-Chen <chunyenchen2019@gmail.com>
2025-01-08 18:45:01 +0800

    Minor

--- example/test_problem/Hydro/FermiBubble/generate_make.sh
+++ example/test_problem/Hydro/FermiBubble/generate_make.sh
@@ -4,4 +4,4 @@ PYTHON=python3
 
 ${PYTHON} configure.py --flu_scheme=MHM_RP --gravity=true --pot_scheme=SOR --store_pot_ghost=true \
                        --unsplit_gravity=false --slope=PLM --flux=HLLC --srhd=true --cosmic_ray=true \
-                       --double=true --hdf5=true --nlevel=14 --fftw=FFTW2 --passive=3 --mpi=true --gpu=true
+                       --double=true --hdf5=true --nlevel=14 --fftw=FFTW2 --passive=3 --mpi=true --gpu=true "$@"
