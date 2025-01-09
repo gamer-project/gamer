c8c72a8b616ed9828e132b2ff28410fe9c1c0e61
Anchewei <10302leo@gmail.com>
2025-01-09 11:15:48 +0800

    Add SF_CreateStar_SinkParticle

--- include/Prototype.h
+++ include/Prototype.h
@@ -824,6 +824,9 @@ void SF_FreeRNG();
 void SF_CreateStar_AGORA( const int lv, const real TimeNew, const real dt, RandomNumber_t *RNG,
                           const real GasDensThres, const real Efficiency, const real MinStarMass, const real MaxStarMFrac,
                           const bool DetRandom, const bool UseMetal );
+void SF_CreateStar_SinkParticle( const int lv, const real TimeNew, const real dt, RandomNumber_t *RNG,
+                                 const real GasDensThres, const real Efficiency, const real MinStarMass, const real MaxStarMFrac,
+                                 const bool DetRandom, const bool UseMetal );
 #endif
 
 
