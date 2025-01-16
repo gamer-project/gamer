d8402471a68666ba98f0ba74991d1cdf8ea38984
ChunYen-Chen <chunyenchen2019@gmail.com>
2025-01-16 17:04:20 +0800

    Add file premission check

--- src/Auxiliary/Aux_CheckFileExist.cpp
+++ src/Auxiliary/Aux_CheckFileExist.cpp
@@ -1,5 +1,7 @@
 #include "GAMER.h"
 #include <sys/stat.h>
+#include <unistd.h>
+#include <grp.h>
 
 
 
@@ -40,7 +42,46 @@ bool Aux_CheckFolderExist( const char *FolderName )
    struct stat Buf;
 
    if ( stat(FolderName, &Buf) != 0 )    return false; // not exist
-   else if ( Buf.st_mode & S_IFDIR )     return true;  // is directory
-   else                                  return false; // not a diretory
+   else if ( !(Buf.st_mode & S_IFDIR) )  return false; // not a directoy
+
+   return true;
 
 } // FUNCTION : Aux_CheckFolderExist
+
+
+
+//-------------------------------------------------------------------------------------------------------
+// Function    :  Aux_CheckPermission
+// Description :  Check whether or not the target file has requested permission
+//
+// Note        :  Use the "stat" function to query the permission of the target file
+//
+// Parameter   :  FileName : Name of the target file
+//             :  perms    : permission of the file (4->read, 2->write, 1->execute)
+//
+// Return      :  true/false <-> you does/does not have the file permission
+//-------------------------------------------------------------------------------------------------------
+bool Aux_CheckPermission( const char *FileName, const int perms )
+{
+
+   if ( perms < 0  ||  perms > 7 )   Aux_Error( ERROR_INFO, "Incorrect file permission code %d (0~7)\n", perms);
+
+   const uid_t curUserId  = getuid(); // get user  id
+   const gid_t curGroupId = getgid(); // get group id
+
+   struct stat Buf;
+
+   if ( stat(FileName, &Buf) != 0 )    Aux_Error( ERROR_INFO, "%s does not exist!!\n", FileName );
+
+   int perm_r, perm_w, perm_x;
+   if      ( Buf.st_uid == curUserId  ) { perm_r = S_IRUSR; perm_w = S_IWUSR; perm_x = S_IXUSR; } // user
+   else if ( Buf.st_gid == curGroupId ) { perm_r = S_IRGRP; perm_w = S_IWGRP; perm_x = S_IXGRP; } // group
+   else                                 { perm_r = S_IROTH; perm_w = S_IWOTH; perm_x = S_IXOTH; } // other
+
+   if ( (perms & 4)  &&  !(Buf.st_mode & perm_r) )   return false;
+   if ( (perms & 2)  &&  !(Buf.st_mode & perm_w) )   return false;
+   if ( (perms & 1)  &&  !(Buf.st_mode & perm_x) )   return false;
+
+   return true;
+
+} // FUNCTION : Aux_CheckPermission
--- src/Auxiliary/Aux_Check_Parameter.cpp
+++ src/Auxiliary/Aux_Check_Parameter.cpp
@@ -334,6 +334,9 @@ void Aux_Check_Parameter()
    if (  ! Aux_CheckFolderExist( OUTPUT_DIR )  )
       Aux_Error( ERROR_INFO, "\"%s\" folder set by DUMP_DIR does not exist !!\n", OUTPUT_DIR );
 
+   if (  ! Aux_CheckPermission( OUTPUT_DIR, 2 )  )
+      Aux_Error( ERROR_INFO, "You do not have the write permission of \"%s\" folder !!\n", OUTPUT_DIR );
+
 
 
 // general warnings
