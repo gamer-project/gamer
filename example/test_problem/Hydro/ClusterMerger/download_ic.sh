97c77cb767e2f811dc28f211c32b1728189349fe
ChunYen-Chen <chunyenchen2019@gmail.com>
2025-01-21 17:39:01 +0800

    Add sha256sum check

--- example/test_problem/Hydro/ClusterMerger/download_ic.sh
+++ example/test_problem/Hydro/ClusterMerger/download_ic.sh
@@ -2,10 +2,14 @@
 
 LOCAL_FILENAME="gamer_ic_merging_cluster.tgz"
 FILE_ID="677caaec999605c485c8de74"
+FILE_SHA256="a233a892818504cf15e188bca862e22250bb1f3e09155740e45d272e4ab5f1c1"
 
 # 1. download
-curl https://hub.yt/api/v1/item/${FILE_ID}/download -o "${LOCAL_FILENAME}.tar.gz"
+curl https://hub.yt/api/v1/item/${FILE_ID}/download -o "${LOCAL_FILENAME}"
 
-# 2. unzip
+# 2. compare sha256sum
+! [ `sha256sum ${LOCAL_FILENAME} | awk '{print $1}'` = "${FILE_SHA256}" ] && echo "File broken: ${LOCAL_FILENAME}"
+
+# 3. unzip
 tar -zxvf ${LOCAL_FILENAME}
 rm ${LOCAL_FILENAME}
