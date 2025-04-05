07dadc760035ba7ad74a583be92287f1fbb8fbe5
Hsinhao <hsinaohuang@gmail.com>
2025-04-05 13:14:57 +0800

    Update to LoadInputTestProb

--- src/TestProblem/Hydro/StarFormationThreshold/Init_TestProb_StarFormationThreshold.cpp
+++ src/TestProblem/Hydro/StarFormationThreshold/Init_TestProb_StarFormationThreshold.cpp
@@ -111,6 +111,51 @@ void Validate()
 
 #if ( MODEL == HYDRO  &&  defined MASSIVE_PARTICLES )
 //-------------------------------------------------------------------------------------------------------
+// Function    :  LoadInputTestProb
+// Description :  Read problem-specific runtime parameters from Input__TestProb and store them in HDF5 snapshots (Data_*)
+//
+// Note        :  1. Invoked by SetParameter() to read parameters
+//                2. Invoked by Output_DumpData_Total_HDF5() using the function pointer Output_HDF5_InputTest_Ptr to store parameters
+//                3. If there is no problem-specific runtime parameter to load, add at least one parameter
+//                   to prevent an empty structure in HDF5_Output_t
+//                   --> Example:
+//                       LOAD_PARA( load_mode, "TestProb_ID", &TESTPROB_ID, TESTPROB_ID, TESTPROB_ID, TESTPROB_ID );
+//
+// Parameter   :  load_mode      : Mode for loading parameters
+//                                 --> LOAD_READPARA    : Read parameters from Input__TestProb
+//                                     LOAD_HDF5_OUTPUT : Store parameters in HDF5 snapshots
+//                ReadPara       : Data structure for reading parameters (used with LOAD_READPARA)
+//                HDF5_InputTest : Data structure for storing parameters in HDF5 snapshots (used with LOAD_HDF5_OUTPUT)
+//
+// Return      :  None
+//-------------------------------------------------------------------------------------------------------
+void LoadInputTestProb( const LoadParaMode_t load_mode, ReadPara_t *ReadPara, HDF5_Output_t *HDF5_InputTest )
+{
+
+#  ifndef SUPPORT_HDF5
+   if ( load_mode == LOAD_HDF5_OUTPUT )   Aux_Error( ERROR_INFO, "please turn on SUPPORT_HDF5 in the Makefile for load_mode == LOAD_HDF5_OUTPUT !!\n" );
+#  endif
+
+   if ( load_mode == LOAD_READPARA     &&  ReadPara       == NULL )   Aux_Error( ERROR_INFO, "load_mode == LOAD_READPARA and ReadPara == NULL !!\n" );
+   if ( load_mode == LOAD_HDF5_OUTPUT  &&  HDF5_InputTest == NULL )   Aux_Error( ERROR_INFO, "load_mode == LOAD_HDF5_OUTPUT and HDF5_InputTest == NULL !!\n" );
+
+// add parameters in the following format:
+// --> note that VARIABLE, DEFAULT, MIN, and MAX must have the same data type
+// --> some handy constants (e.g., NoMin_int, Eps_float, ...) are defined in "include/ReadPara.h"
+// --> LOAD_PARA() is defined in "include/TestProb.h"
+// ********************************************************************************************************************************
+// LOAD_PARA( load_mode, "KEY_IN_THE_FILE",                        &VARIABLE,                                     DEFAULT,       MIN,              MAX               );
+// ********************************************************************************************************************************
+   LOAD_PARA( load_mode, "StarFormationThreshold_MassDensity_Min", &StarFormationThreshold_MassDensity_Min,       1.0e-29,       Eps_double,       NoMax_double      );
+   LOAD_PARA( load_mode, "StarFormationThreshold_MassDensity_Max", &StarFormationThreshold_MassDensity_Max,       1.0e-21,       Eps_double,       NoMax_double      );
+   LOAD_PARA( load_mode, "StarFormationThreshold_Temperature_Min", &StarFormationThreshold_Temperature_Min,       1.0e+00,       Eps_double,       NoMax_double      );
+   LOAD_PARA( load_mode, "StarFormationThreshold_Temperature_Max", &StarFormationThreshold_Temperature_Max,       1.0e+08,       Eps_double,       NoMax_double      );
+
+} // FUNCITON : LoadInputTestProb
+
+
+
+//-------------------------------------------------------------------------------------------------------
 // Function    :  SetParameter
 // Description :  Load and set the problem-specific runtime parameters
 //
@@ -133,19 +178,11 @@ void SetParameter()
 
 
 // (1) load the problem-specific runtime parameters
+// (1-1) read parameters from Input__TestProb
    const char FileName[] = "Input__TestProb";
    ReadPara_t *ReadPara  = new ReadPara_t;
 
-// (1-1) add parameters in the following format:
-// --> note that VARIABLE, DEFAULT, MIN, and MAX must have the same data type
-// --> some handy constants (e.g., Useless_bool, Eps_double, NoMin_int, ...) are defined in "include/ReadPara.h"
-// ********************************************************************************************************************************
-// ReadPara->Add( "KEY_IN_THE_FILE",                               &VARIABLE,                                          DEFAULT,       MIN,              MAX               );
-// ********************************************************************************************************************************
-   ReadPara->Add( "StarFormationThreshold_MassDensity_Min",        &StarFormationThreshold_MassDensity_Min,            1.0e-29,       Eps_double,       NoMax_double      );
-   ReadPara->Add( "StarFormationThreshold_MassDensity_Max",        &StarFormationThreshold_MassDensity_Max,            1.0e-21,       Eps_double,       NoMax_double      );
-   ReadPara->Add( "StarFormationThreshold_Temperature_Min",        &StarFormationThreshold_Temperature_Min,            1.0e+00,       Eps_double,       NoMax_double      );
-   ReadPara->Add( "StarFormationThreshold_Temperature_Max",        &StarFormationThreshold_Temperature_Max,            1.0e+08,       Eps_double,       NoMax_double      );
+   LoadInputTestProb( LOAD_READPARA, ReadPara, NULL );
 
    ReadPara->Read( FileName );
 
@@ -380,7 +417,6 @@ void Init_TestProb_Hydro_StarFormationThreshold()
    Validate();
 
 
-// replace HYDRO by the target model (e.g., MHD/ELBDM) and also check other compilation flags if necessary (e.g., GRAVITY/PARTICLE)
 #  if ( MODEL == HYDRO  &&  defined MASSIVE_PARTICLES )
 // set the problem-specific runtime parameters
    SetParameter();
@@ -391,6 +427,9 @@ void Init_TestProb_Hydro_StarFormationThreshold()
    Par_Init_ByFunction_Ptr           = Par_Init_ByFunction_StarFormationThreshold;
    Flu_ResetByUser_Func_Ptr          = Flu_ResetByUser_StarFormationThreshold;
    Mis_GetTimeStep_User_Ptr          = Mis_GetTimeStep_StarFormationThreshold;
+#  ifdef SUPPORT_HDF5
+   Output_HDF5_InputTest_Ptr         = LoadInputTestProb;
+#  endif
 #  endif // #if ( MODEL == HYDRO  &&  defined MASSIVE_PARTICLES )
 
 
