9c4bd3b049d5192345eedec71c29da84a4b1ca36
Hsinhao <hsinaohuang@gmail.com>
2025-04-05 13:32:36 +0800

    Update to LoadInputTestProb

--- src/TestProblem/Hydro/SNFeedbackBlastWave/Init_TestProb_Hydro_SNFeedbackBlastWave.cpp
+++ src/TestProblem/Hydro/SNFeedbackBlastWave/Init_TestProb_Hydro_SNFeedbackBlastWave.cpp
@@ -100,6 +100,58 @@ void Validate()
 
 #if ( MODEL == HYDRO  &&  defined FEEDBACK )
 //-------------------------------------------------------------------------------------------------------
+// Function    :  LoadInputTestProb
+// Description :  Read problem-specific runtime parameters from Input__TestProb and store them in HDF5 snapshots (Data_*)
+//
+// Note        :  1. Invoked by SetParameter() to read parameters
+//                2. Invoked by Output_DumpData_Total_HDF5() using the function pointer Output_HDF5_InputTest_Ptr to store parameters
+//                3. If there is no problem-specific runtime parameter to load, add at least one parameter
+//                   to prevent an empty structure in HDF5_Output_t
+//                   --> Example:
+//                       LOAD_PARA( load_mode, "TestProb_ID", &TESTPROB_ID, TESTPROB_ID, TESTPROB_ID, TESTPROB_ID );
+//
+// Parameter   :  load_mode      : Mode for loading parameters
+//                                 --> LOAD_READPARA    : Read parameters from Input__TestProb
+//                                     LOAD_HDF5_OUTPUT : Store parameters in HDF5 snapshots
+//                ReadPara       : Data structure for reading parameters (used with LOAD_READPARA)
+//                HDF5_InputTest : Data structure for storing parameters in HDF5 snapshots (used with LOAD_HDF5_OUTPUT)
+//
+// Return      :  None
+//-------------------------------------------------------------------------------------------------------
+void LoadInputTestProb( const LoadParaMode_t load_mode, ReadPara_t *ReadPara, HDF5_Output_t *HDF5_InputTest )
+{
+
+#  ifndef SUPPORT_HDF5
+   if ( load_mode == LOAD_HDF5_OUTPUT )   Aux_Error( ERROR_INFO, "please turn on SUPPORT_HDF5 in the Makefile for load_mode == LOAD_HDF5_OUTPUT !!\n" );
+#  endif
+
+   if ( load_mode == LOAD_READPARA     &&  ReadPara       == NULL )   Aux_Error( ERROR_INFO, "load_mode == LOAD_READPARA and ReadPara == NULL !!\n" );
+   if ( load_mode == LOAD_HDF5_OUTPUT  &&  HDF5_InputTest == NULL )   Aux_Error( ERROR_INFO, "load_mode == LOAD_HDF5_OUTPUT and HDF5_InputTest == NULL !!\n" );
+
+// add parameters in the following format:
+// --> note that VARIABLE, DEFAULT, MIN, and MAX must have the same data type
+// --> some handy constants (e.g., NoMin_int, Eps_float, ...) are defined in "include/ReadPara.h"
+// --> LOAD_PARA() is defined in "include/TestProb.h"
+// ********************************************************************************************************************************
+// LOAD_PARA( load_mode, "KEY_IN_THE_FILE",          &VARIABLE,                  DEFAULT,      MIN,              MAX               );
+// ********************************************************************************************************************************
+   LOAD_PARA( load_mode, "SNBlast_Dens_Bg",          &SNBlast_Dens_Bg,           1.0e-22,      Eps_double,       NoMax_double      );
+   LOAD_PARA( load_mode, "SNBlast_Temp_Bg",          &SNBlast_Temp_Bg,           1.0e+01,      Eps_double,       NoMax_double      );
+   LOAD_PARA( load_mode, "SNBlast_MetalMassFrac_Bg", &SNBlast_MetalMassFrac_Bg,  0.0,          0.0,              1.0               );
+   LOAD_PARA( load_mode, "SNBlast_ParMass",          &SNBlast_ParMass,           1.0e+02,      Eps_double,       NoMax_double      );
+   LOAD_PARA( load_mode, "SNBlast_ParCenter_X",      &SNBlast_ParCenter[0],     -1.0,          NoMin_double,     amr->BoxSize[0]   );
+   LOAD_PARA( load_mode, "SNBlast_ParCenter_Y",      &SNBlast_ParCenter[1],     -1.0,          NoMin_double,     amr->BoxSize[1]   );
+   LOAD_PARA( load_mode, "SNBlast_ParCenter_Z",      &SNBlast_ParCenter[2],     -1.0,          NoMin_double,     amr->BoxSize[2]   );
+   LOAD_PARA( load_mode, "SNBlast_ParVelocity_X",    &SNBlast_ParVelocity[0],    0.0,          NoMin_double,     NoMax_double      );
+   LOAD_PARA( load_mode, "SNBlast_ParVelocity_Y",    &SNBlast_ParVelocity[1],    0.0,          NoMin_double,     NoMax_double      );
+   LOAD_PARA( load_mode, "SNBlast_ParVelocity_Z",    &SNBlast_ParVelocity[2],    0.0,          NoMin_double,     NoMax_double      );
+   LOAD_PARA( load_mode, "SNBlast_ParMetalMassFrac", &SNBlast_ParMetalMassFrac,  0.0,          0.0,              1.0               );
+
+} // FUNCITON : LoadInputTestProb
+
+
+
+//-------------------------------------------------------------------------------------------------------
 // Function    :  SetParameter
 // Description :  Load and set the problem-specific runtime parameters
 //
@@ -121,26 +173,11 @@ void SetParameter()
 
 
 // (1) load the problem-specific runtime parameters
+// (1-1) read parameters from Input__TestProb
    const char FileName[] = "Input__TestProb";
    ReadPara_t *ReadPara  = new ReadPara_t;
 
-// add parameters in the following format:
-// --> note that VARIABLE, DEFAULT, MIN, and MAX must have the same data type
-// --> some handy constants (e.g., NoMin_int, Eps_float, ...) are defined in "include/ReadPara.h"
-// ********************************************************************************************************************************
-// ReadPara->Add( "KEY_IN_THE_FILE",          &VARIABLE_ADDRESS,          DEFAULT,      MIN,              MAX               );
-// ********************************************************************************************************************************
-   ReadPara->Add( "SNBlast_Dens_Bg",          &SNBlast_Dens_Bg,           1.0e-22,      Eps_double,       NoMax_double      );
-   ReadPara->Add( "SNBlast_Temp_Bg",          &SNBlast_Temp_Bg,           1.0e+01,      Eps_double,       NoMax_double      );
-   ReadPara->Add( "SNBlast_MetalMassFrac_Bg", &SNBlast_MetalMassFrac_Bg,  0.0,          0.0,              1.0               );
-   ReadPara->Add( "SNBlast_ParMass",          &SNBlast_ParMass,           1.0e+02,      Eps_double,       NoMax_double      );
-   ReadPara->Add( "SNBlast_ParCenter_X",      &SNBlast_ParCenter[0],     -1.0,          NoMin_double,     amr->BoxSize[0]   );
-   ReadPara->Add( "SNBlast_ParCenter_Y",      &SNBlast_ParCenter[1],     -1.0,          NoMin_double,     amr->BoxSize[1]   );
-   ReadPara->Add( "SNBlast_ParCenter_Z",      &SNBlast_ParCenter[2],     -1.0,          NoMin_double,     amr->BoxSize[2]   );
-   ReadPara->Add( "SNBlast_ParVelocity_X",    &SNBlast_ParVelocity[0],    0.0,          NoMin_double,     NoMax_double      );
-   ReadPara->Add( "SNBlast_ParVelocity_Y",    &SNBlast_ParVelocity[1],    0.0,          NoMin_double,     NoMax_double      );
-   ReadPara->Add( "SNBlast_ParVelocity_Z",    &SNBlast_ParVelocity[2],    0.0,          NoMin_double,     NoMax_double      );
-   ReadPara->Add( "SNBlast_ParMetalMassFrac", &SNBlast_ParMetalMassFrac,  0.0,          0.0,              1.0               );
+   LoadInputTestProb( LOAD_READPARA, ReadPara, NULL );
 
    ReadPara->Read( FileName );
 
@@ -366,6 +403,9 @@ void Init_TestProb_Hydro_SNFeedbackBlastWave()
 #  ifdef MASSIVE_PARTICLES
    Par_Init_ByFunction_Ptr       = Par_Init_ByFunction_SNFeedbackBlastWave;
    Par_Init_Attribute_User_Ptr   = AddNewParticleAttribute_SNFeedbackBlastWave;
+#  ifdef SUPPORT_HDF5
+   Output_HDF5_InputTest_Ptr     = LoadInputTestProb;
+#  endif
 #  endif
 #  endif // #if ( MODEL == HYDRO  &&  defined FEEDBACK )
 
