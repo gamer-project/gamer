2631de96e94f7fb1ee014bd907252053f82a39f7
ChunYen-Chen <chunyenchen2019@gmail.com>
2025-07-16 16:23:51 +0800

    Separate Hypre_Free()

---
+++ src/Hypre/Hypre_Free.cpp
@@ -0,0 +1,23 @@
+#include "GAMER.h"
+
+
+
+#ifdef SUPPORT_HYPRE
+//-------------------------------------------------------------------------------------------------------
+// Function    :  Hypre_Free
+// Description :  Destory the Hypre arrays
+//
+// Parameter   :  None
+//-------------------------------------------------------------------------------------------------------
+void Hypre_Free()
+{
+
+   HYPRE_CHECK_FUNC(   HYPRE_SStructGridDestroy( Hypre_grid )   );
+   HYPRE_CHECK_FUNC(   HYPRE_SStructGraphDestroy( Hypre_graph )   );
+   HYPRE_CHECK_FUNC(   HYPRE_SStructStencilDestroy( Hypre_stencil )   );
+   HYPRE_CHECK_FUNC(   HYPRE_SStructMatrixDestroy( Hypre_A )   );
+   HYPRE_CHECK_FUNC(   HYPRE_SStructVectorDestroy( Hypre_x )   );
+   HYPRE_CHECK_FUNC(   HYPRE_SStructVectorDestroy( Hypre_b )   );
+
+} // FUNCITON : Hypre_Free
+#endif // #ifdef SUPPORT_HYPRE
--- src/Hypre/Hypre_Main.cpp
+++ src/Hypre/Hypre_Main.cpp
@@ -44,24 +44,4 @@ void Hypre_Solver( const Hypre_SolveType_t SolveType, const int lv, const double
    Hypre_Aux_Record( SolveType, lv, N_iter, final_residual );
 
 } // FUNCTION : Hypre_Solver
-
-
-
-//-------------------------------------------------------------------------------------------------------
-// Function    :  Hypre_Free
-// Description :  Destory the Hypre arrays
-//
-// Parameter   :  None
-//-------------------------------------------------------------------------------------------------------
-void Hypre_Free()
-{
-
-   HYPRE_CHECK_FUNC(   HYPRE_SStructGridDestroy( Hypre_grid )   );
-   HYPRE_CHECK_FUNC(   HYPRE_SStructGraphDestroy( Hypre_graph )   );
-   HYPRE_CHECK_FUNC(   HYPRE_SStructStencilDestroy( Hypre_stencil )   );
-   HYPRE_CHECK_FUNC(   HYPRE_SStructMatrixDestroy( Hypre_A )   );
-   HYPRE_CHECK_FUNC(   HYPRE_SStructVectorDestroy( Hypre_x )   );
-   HYPRE_CHECK_FUNC(   HYPRE_SStructVectorDestroy( Hypre_b )   );
-
-} // FUNCITON : Hypre_Free
 #endif // #ifdef SUPPORT_HYPRE
