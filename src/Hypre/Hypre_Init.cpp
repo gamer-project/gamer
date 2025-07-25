d08ba3f6d6b80a36269a93a03ad66eff34a96e1d
ChunYen-Chen <chunyenchen2019@gmail.com>
2025-07-25 09:54:29 +0800

    Add checks

--- src/Hypre/Hypre_Init.cpp
+++ src/Hypre/Hypre_Init.cpp
@@ -19,6 +19,34 @@ void Hypre_Init()
 
    if ( MPI_Rank == 0 )   Aux_Message( stdout, "%s ...\n", __FUNCTION__ );
 
+// checks
+#  if ( !defined SERIAL  &&  HYPRE_HAVE_MPI != 1 )
+   Aux_Error( ERROR_INFO, "GAMER MPI (enabled) != HYPRE MPI (disable) !!\n" );
+#  endif
+
+#  if ( defined SERIAL  &&  HYPRE_HAVE_MPI == 1 )
+   Aux_Error( ERROR_INFO, "GAMER MPI (disabled) != HYPRE MPI (enable) !!\n" );
+#  endif
+
+#  if ( defined GPU  &&  HYPRE_USING_CUDA != 1 )
+   Aux_Error( ERROR_INFO, "GAMER GPU (enabled) != HYPRE GPU (disable) !!\n" );
+#  endif
+
+#  if ( !defined GPU  &&  HYPRE_USING_CUDA == 1 )
+   Aux_Error( ERROR_INFO, "GAMER GPU (disabled) != HYPRE GPU (enable) !!\n" );
+#  endif
+
+
+// warning
+#  if ( defined OPENMP  &&  HYPRE_USING_OPENMP != 1 )
+   Aux_Message( stdout, "WARNING : GAMER OPENMP (enabled) != HYPRE OPENMP (disable) !!\n" );
+#  endif
+
+#  if ( !defined OPENMP  &&  HYPRE_USING_OPENMP == 1 )
+   Aux_Message( stdout, "WARNING : GAMER OPENMP (disabled) != HYPRE OPENMP (enable) !!\n" );
+#  endif
+
+
 // initialize Hypre
    HYPRE_CHECK_FUNC(   HYPRE_Initialize()   );
 
