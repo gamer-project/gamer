technic960183/gamer-fork-ymhsu

Add `FloorPassive_t` arg to header of `AddField`
with `Null`, `No` and `Yes` three possible values.
This only sets up the header, not the implementation.

src/TestProblem/ELBDM/PlaneWave/Init_TestProb_ELBDM_PlaneWave.cpp
@@ -284,7 +284,7 @@ void AddNewField_PlaneWave()
 {
 
    if ( PWave_Idx_Phase == Idx_Undefined )
-      PWave_Idx_Phase = AddField( "Phase", FIXUP_FLUX_NO, FIXUP_REST_NO, NORMALIZE_NO, INTERP_FRAC_NO );
+      PWave_Idx_Phase = AddField( "Phase", FIXUP_FLUX_NO, FIXUP_REST_NO, FLOOR_YES, NORMALIZE_NO, INTERP_FRAC_NO );
 
 } // FUNCTION : AddNewField_PlaneWave
 

gamer-project/gamer

src/TestProblem/ELBDM/PlaneWave/Init_TestProb_ELBDM_PlaneWave.cpp
@@ -357,7 +357,7 @@ void AddNewField_PlaneWave()
 {
 
    if ( PWave_Idx_WrappedPhase == Idx_Undefined )
-      PWave_Idx_WrappedPhase = AddField( "WrappedPhase", FIXUP_FLUX_NO, FIXUP_REST_NO, NORMALIZE_NO, INTERP_FRAC_NO );
+      PWave_Idx_WrappedPhase = AddField( "WrappedPhase", FIXUP_FLUX_NO, FIXUP_REST_NO, FLOOR_YES, NORMALIZE_NO, INTERP_FRAC_NO );
 
 } // FUNCTION : AddNewField_PlaneWave
 