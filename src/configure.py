d1ea17ca6561d733d494e7b65523b740d390c3dd
koarakawaii <arahaguojnestore@gmail.com>
2025-11-10 10:27:51 +0800

    include compute capability for GB10

--- src/configure.py
+++ src/configure.py
@@ -948,7 +948,7 @@ def set_gpu( gpus, flags, args ):
             gpu_opts["MAXRREGCOUNT_FLU"] = "--maxrregcount=128"
         else:
             gpu_opts["MAXRREGCOUNT_FLU"] = "--maxrregcount=70"
-    elif 500 <= compute_capability and compute_capability <= 1200:
+    elif 500 <= compute_capability and compute_capability <= 1210:
         if args["double"]:
             gpu_opts["MAXRREGCOUNT_FLU"] = "--maxrregcount=192"
         else:
