7b5c1035b4d822d9092160545a01290fc1743ef9
Anchewei <10302leo@gmail.com>
2026-01-10 19:42:39 +0800

    Update Analysis.py

--- example/test_problem/Hydro/SinkParTest/Analysis.py
+++ example/test_problem/Hydro/SinkParTest/Analysis.py
@@ -1,10 +1,10 @@
 #**************************************************************************#
-# Code:        GetColMap                                                   # 
+# Code:        Analysis                                                    # 
 #                                                                          #
 # Author:      Wei-An Chen                                                 #
 #                                                                          #
 # This scipt is useful for checking the evolution of simulation by         #
-# plotting the column density map.                                         #
+# plotting the column density map and mass growth of the particles.        #
 #                                                                          #
 # s_index:   the starting index of the snapshot,                           #
 #            e.g. s_index = 10 for Data_000010.                            #
@@ -13,8 +13,10 @@
 # ProjPlane: the projection plane [xy/xz].                                 #
 #**************************************************************************#
 
+import sys
+import argparse
 import yt
-from astropy.constants import m_p
+from yt.utilities.physical_constants import gravitational_constant_cgs, mp, solar_mass
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -30,12 +32,25 @@ mpl.rcParams['xtick.minor.width'] = 2
 mpl.rcParams['ytick.major.width'] = 2
 mpl.rcParams['ytick.minor.width'] = 2
 
-s_index   = 0
-e_index   = 300
-npixel    = 256
-ProjPlane = 'xy'
-Output    = "./ColMap"
-tff       = 16.560403596485347 # kyr
+# load the command-line parameters
+parser = argparse.ArgumentParser( description='Plot the gas column density and particle mass growth.' )
+
+parser.add_argument( '-s', action='store', required=True,  type=int, dest='s_index',
+                     help='first data index' )
+parser.add_argument( '-e', action='store', required=True,  type=int, dest='e_index',
+                     help='last data index' )
+parser.add_argument( '-npix', action='store', required=False, type=int, dest='npixel',
+                     help='image size [%(default)d]', default=256 )
+parser.add_argument( '-proj', action='store', required=False, type=str, dest='ProjPlane',
+                     help='projection plane [%(default)s]', default="xy" )
+
+args = parser.parse_args()
+
+# take note
+print( '\nCommand-line arguments:' )
+print( '-------------------------------------------------------------------' )
+print( ' '.join(map(str, sys.argv)) )
+print( '-------------------------------------------------------------------\n' )
 
 def ProjCol(ds, c, normal_vector, north_vector, width, npixel):
     field = ('gamer', 'Dens')
@@ -43,9 +58,25 @@ def ProjCol(ds, c, normal_vector, north_vector, width, npixel):
     Col = yt.off_axis_projection(ds, center=c,  normal_vector=normal_vector, north_vector=north_vector, width=width,
                           resolution=npixel, item=field, 
                           no_ghost=False).to('g/cm**2').value
-    Col /= 2.3*m_p.cgs.value
+    Col /= mu*mp.value
     
     return Col.T
+
+def free_fall_time(R, M):
+    rho = M/(4/3*np.pi*R**3)
+    
+    return np.sqrt(3*np.pi/(32*gravitational_constant_cgs*rho)).to("kyr")
+
+s_index   = args.s_index
+e_index   = args.e_index
+npixel    = args.npixel
+ProjPlane = args.ProjPlane
+Output    = "./ColMap"
+
+# Initial cloud parameters (from Input_TestProb)
+R0        = 3.0856776e+16 # cm
+Core_Mass = 1.0 # M_sun
+mu        = 2.3 # molecular mass
 
 fns = ['Data_{}'.format(format(index_id,'06')) for index_id in range(s_index, e_index+1)]
 index_list = [format(index_id,'06') for index_id in range(s_index, e_index+1)]
@@ -69,6 +100,9 @@ for fn, index_id in zip(fns, index_list):
         
     print("Current snapshot = %s"%fn)
     print('Simulation time = %.2f kyr'%(ds.current_time.to('kyr')))
+
+    # Get the free-fall time
+    tff = free_fall_time(R0*u.cm, Core_Mass*solar_mass)
     
     # Read the box dimensions
     box_size_lxy = ds.domain_right_edge.value[0] # cm, for x and y
@@ -89,6 +123,10 @@ for fn, index_id in zip(fns, index_list):
     elif ProjPlane == 'xz':
         normal_vector = np.array([0., -1., 0.])
         north_vector  = np.array([-1., 0., 0.])
+
+    elif ProjPlane == 'yz':
+        normal_vector = np.array([1., 0., 0.])
+        north_vector  = np.array([0., 0., 1.])
         
     img_extend = width.to('pc').value[0]/2 # the boundary for plotting
 
@@ -141,6 +179,8 @@ for fn, index_id in zip(fns, index_list):
         ax.scatter(Par_x, Par_y, color='b', s=1)
     elif ProjPlane == 'xz':
         ax.scatter(Par_z, -Par_x, color='b', s=1)
+    elif ProjPlane == 'yz':
+        ax.scatter(Par_y, Par_z, color='b', s=1)
 
     cbar = fig.colorbar(im, ax=ax, shrink=0.8, aspect=30)
     cbar.set_label(r'Column number density (cm$^{-2}$)', fontsize=13)
@@ -151,6 +191,9 @@ for fn, index_id in zip(fns, index_list):
     elif ProjPlane == 'xz':
         ax.set_xlabel(r'z (pc)')
         ax.set_ylabel(r'x (pc)')
+    elif ProjPlane == 'yz':
+        ax.set_xlabel(r'y (pc)')
+        ax.set_ylabel(r'z (pc)')
         
     ax.set_title(r'%.2f $t_{ff}$'%Time)
     
