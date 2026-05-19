gamer-project/gamer

commit a1735d44186301003c980c0dd9870af817807199

Minor updates for PR #351

src/Particle/Par_SetParUID.cpp
@@ -54,8 +54,9 @@ void Par_SetParUID()
       Aux_Error( ERROR_INFO, "NNewPar_AllRank (%ld) is negative !!\n", NNewPar_AllRank );
 
 // check whether the next PUID will overflow
-   if ( amr->Par->NextPUID > ( (sizeof(long_par) == sizeof(int)) ? __INT_MAX__ : __LONG_MAX__ ) - NNewPar_AllRank )
-      Aux_Error( ERROR_INFO, "amr->Par->PUID = amr->Par->NextPUID (%ld) + NNewPar_AllRank (%ld) - 1 will overflow !!\n", amr->Par->NextPUID, NNewPar_AllRank );
+   if ( amr->Par->NextPUID + NNewPar_AllRank - 1 > (sizeof(long_par) == sizeof(int)) ? __INT_MAX__ : __LONG_MAX__ )
+      Aux_Error( ERROR_INFO, "amr->Par->PUID = amr->Par->NextPUID (%ld) + NNewPar_AllRank (%ld) - 1 will overflow !!\n",
+                 amr->Par->NextPUID, NNewPar_AllRank );
 
 // safety check for the type conversion from long to int
    int NNewPar_ThisRank_int = NNewPar_ThisRank;

example/test_problem/ELBDM/LSS_Hybrid/README
@@ -53,7 +53,7 @@ Note:
 
    4. The simulation uses the fluid solver on levels 0 - 3 and switches to the wave solver in regions of interference on level 4.
       The corresponding runtime parameter "ELBDM_FIRST_WAVE_LEVEL" is set to 4.
-   
+
       The resolution for the first level using the wave solver is 2.8 Mpc/h /(64*2^4) ~ 2.75 kpc/h (comoving) with these settings.
       This resolution has shown to be a good compromise for the starting resolution of the wave solver.
       Note that increasing "ELBDM_FIRST_WAVE_LEVEL" will affect performance since it will likely lead to overrefinement. However, it should increase the accuracy of the hybrid scheme.

example/test_problem/Hydro/ParticleTest/Input__Parameter
@@ -187,7 +187,7 @@ INT_MONO_COEFF                2.0         # coefficient for ensuring the interpo
 OPT__OUTPUT_TOTAL             1           # output the simulation snapshot: (0=off, 1=HDF5, 2=C-binary) [1]
 OPT__OUTPUT_PART              0           # output a single line or slice: (0=off, 1=xy, 2=yz, 3=xz, 4=x, 5=y, 6=z, 7=diag, 8=entire box) [0]
 OPT__OUTPUT_USER              0           # output the user-specified data -> edit "Output_User.cpp" [0]
-OPT__OUTPUT_PAR_MODE          0           # output the particle data: (0=off, 1=text-file, 2=C-binary) [0] ##PARTICLE ONLY##
+OPT__OUTPUT_PAR_MODE          1           # output the particle data: (0=off, 1=text-file, 2=C-binary) [0] ##PARTICLE ONLY##
 OPT__OUTPUT_PAR_MESH          1           # output the attributes of tracer particles mapped from mesh quantities -> edit "Input__Par_Mesh" [1] ##PARTICLE ONLY##
 OPT__OUTPUT_BASEPS            0           # output the base-level power spectrum [0]
 OPT__OUTPUT_BASE              0           # only output the base-level data [0] ##OPT__OUTPUT_PART ONLY##

src/Init/Init_ByRestart_v2.cpp
@@ -148,7 +148,7 @@ void Init_ByRestart()
 
 #     ifdef PARTICLE
       if ( FormatVersion < 2310 )
-         Aux_Error( ERROR_INFO, "unsupported data format version for particle UID (only support version >= 2310)!!\n" );
+         Aux_Error( ERROR_INFO, "unsupported data format version for particle UID (only support version >= 2310) !!\n" );
 #     endif
    }
    MPI_Barrier( MPI_COMM_WORLD );

src/Output/Output_DumpData_Total.cpp
@@ -16,7 +16,7 @@ Procedure for outputting new variables:
 
 
 //-------------------------------------------------------------------------------------------------------
-// Function    :  Output_DumpData_Total (FormatVersion = 2220)
+// Function    :  Output_DumpData_Total (FormatVersion = 2310)
 // Description :  Output all simulation data in the binary form, which can be used as a restart file
 //
 // Note        :  1. This output format is deprecated and is mainly used for debugging only
@@ -39,6 +39,7 @@ Procedure for outputting new variables:
 //                2210 : 2019/06/07 --> support MHD
 //                2220 : 2020/08/25 --> output EOS
 //                2300 : 2024/08/25 --> output particle integer attributes
+//                2310 : 2026/03/26 --> output particle unique id
 //-------------------------------------------------------------------------------------------------------
 void Output_DumpData_Total( const char *FileName )
 {

src/Output/Output_DumpData_Total_HDF5.cpp
@@ -286,7 +286,7 @@ Procedure for outputting new variables:
 //                                             GRACKLE_HYDROGEN_MFRAC, OPT__UNFREEZE_GRACKLE,
 //                                             OPT__OUTPUT_GRACKLE_TEMP, OPT__OUTPUT_GRACKLE_MU, OPT__OUTPUT_GRACKLE_TCOOL,
 //                                             DT__GRACKLE_COOLING, OPT__FLAG_COOLING_LEN, FlagTable_CoolingLen
-//                2508 : 2026/03/26 --> output Particle unique id
+//                2508 : 2026/03/26 --> output particle unique id
 //-------------------------------------------------------------------------------------------------------
 void Output_DumpData_Total_HDF5( const char *FileName )
 {

src/Particle/Par_Aux_Check_Particle.cpp
@@ -9,7 +9,7 @@ static void Check_FindHomePatch      ( int &PassAll, int &PassOne, const char *c
 static void Check_InLeafPatch        ( int &PassAll, int &PassOne, const char *comment, const int lv,
                                        const int PID, const int NParThisPatch );
 static void Check_NActive            ( int &PassAll, int &PassOne, const char *comment, const long NParInLeaf );
-static void Check_InactiveMass       ( int &PassAll, int &PassOne, const char *comment, const int lv,
+static void Check_Inactive           ( int &PassAll, int &PassOne, const char *comment, const int lv,
                                        const int PID, const long ParUID );
 static void Check_OneHomePatch       ( int &PassAll, int &PassOne, const char *comment, const int lv,
                                        const int PID, const long ParUID, bool *ParHome );
@@ -101,7 +101,7 @@ void Par_Aux_Check_Particle( const char *comment )
 
                   Check_FindHomePatch( PassAll, PassCheck[0], comment, lv, PID, ParUID, EdgeL, EdgeR, ParPos );
 
-                  Check_InactiveMass( PassAll, PassCheck[3], comment, lv, PID, ParUID );
+                  Check_Inactive( PassAll, PassCheck[3], comment, lv, PID, ParUID );
 
                   Check_ValidType( PassAll, PassCheck[10], comment, lv, PID, ParUID );
 
@@ -266,7 +266,7 @@ void Check_NActive( int &PassAll, int &PassOne, const char *comment, const long 
 
 
 //-------------------------------------------------------------------------------------------------------
-// Function    :  Check_InactiveParticle
+// Function    :  Check_Inactive
 // Description :  Check if amr->patch[0][lv][PID]->ParList[] contains any inactive particle
 //
 // Note        :  None
@@ -278,8 +278,8 @@ void Check_NActive( int &PassAll, int &PassOne, const char *comment, const long 
 //                PID     : Target patch ID
 //                ParUID  : Target particle UID
 //-------------------------------------------------------------------------------------------------------
-void Check_InactiveMass( int &PassAll, int &PassOne, const char *comment, const int lv, const int PID,
-                         const long ParUID )
+void Check_Inactive( int &PassAll, int &PassOne, const char *comment, const int lv, const int PID,
+                     const long ParUID )
 {
 
    if ( amr->Par->Mass[ParUID] >= 0.0 )   return;
@@ -299,7 +299,7 @@ void Check_InactiveMass( int &PassAll, int &PassOne, const char *comment, const 
    PassAll = false;
    PassOne = false;
 
-} // FUNCTION : Check_InactiveMass
+} // FUNCTION : Check_Inactive
 
 
 

commit 5c508838f6547ac0a31027a62c67f4e907ac171c

Fix a bug in Par_SetParUID.cpp introduced by commit a1735d4

src/Particle/Par_SetParUID.cpp
@@ -54,7 +54,7 @@ void Par_SetParUID()
       Aux_Error( ERROR_INFO, "NNewPar_AllRank (%ld) is negative !!\n", NNewPar_AllRank );
 
 // check whether the next PUID will overflow
-   if ( amr->Par->NextPUID + NNewPar_AllRank - 1 > (sizeof(long_par) == sizeof(int)) ? __INT_MAX__ : __LONG_MAX__ )
+   if (  amr->Par->NextPUID + NNewPar_AllRank - 1 > ( (sizeof(long_par) == sizeof(int)) ? __INT_MAX__ : __LONG_MAX__ )  )
       Aux_Error( ERROR_INFO, "amr->Par->PUID = amr->Par->NextPUID (%ld) + NNewPar_AllRank (%ld) - 1 will overflow !!\n",
                  amr->Par->NextPUID, NNewPar_AllRank );
 